65c501f24d6ddf267e88f144a2fc643a05b8444b
Dave Nichols <dave_nichols@europe.pall.com>
2023-09-05 10:17:13 +0100

    Added new Accessibe script to load delayed

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,6 +1,9 @@
 // eslint-disable-next-line import/no-cycle
 import { sampleRUM } from './lib-franklin.js';
 
+// Accessibe code for footer accessible options
+import './lib-accessibe.js';
+
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
