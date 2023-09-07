57e79041dab71db14a5230618834789793506895
Duy Nguyen <duy.phnguyen@gmail.com>
2023-09-07 16:02:41 +0200

    fix lint error

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -42,10 +42,12 @@ function buildSearchBlock(headerBlock) {
   logoLinkBlock.className = 'w-44 md:w-28 lg:w-44 lg:h-8';
   logoLinkBlock.innerHTML = '';
   logoLinkBlock.append(logoPictureBlock);
-  const titleLinkBlock = div({ class: 'bg-danaherblue-900 w-full rounded-tr-lg overflow-hidden hidden md:block lg:hidden' },
+  const titleLinkBlock = div(
+    { class: 'bg-danaherblue-900 w-full rounded-tr-lg overflow-hidden hidden md:block lg:hidden' },
     a({ class: 'h-full flex pl-2 py-2 items-center text-sm text-white rounded-tr-lg overflow-hidden', href: '/' }, 'Life Sciences'),
   );
-  const logoGroupBlock = div({ class: 'flex flex-col lg:py-0 mx-auto md:mx-0 gap-2' },
+  const logoGroupBlock = div(
+    { class: 'flex flex-col lg:py-0 mx-auto md:mx-0 gap-2' },
     logoLinkBlock,
     titleLinkBlock,
   );
@@ -71,7 +73,7 @@ function buildSearchBlock(headerBlock) {
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" class="w-6 h-6 text-white rounded-full" data-di-rand="1693233993603">
       <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0zM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632z"/>
     </svg>
-  `
+  `;
   loginIcon.setAttribute('style', 'filter: brightness(0) invert(1);');
   const loginSpan = span({ class: 'w-12 pl-2 lg:block hidden lg:inline' }, loginLink.textContent);
   loginLink.textContent = '';
