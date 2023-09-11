e3efb57329cacf0eda9c2023943136bb035ca2bb
rgravitvl <ravitvl83@gmail.com>
2023-09-11 19:42:55 +0530

    lint issue addressed

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -8,5 +8,5 @@ sampleRUM('cwv');
 // add more delayed functionality here
 // google tag manager
 if (!window.location.hostname.includes('localhost') && !document.location.hostname.includes('.hlx.page')) {
-    loadGTM();
+  loadGTM();
 }
