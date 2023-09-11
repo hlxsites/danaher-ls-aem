62d9908733d492a050657573353a56f74b5d3806
Deepak Kumar <deepak.kumar@dhlifesciences.com>
2023-09-11 13:29:33 -0400

    Card block styling

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -3,14 +3,30 @@ import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
+
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
+    const heading = row.querySelector('h2');
+    heading.className = 'card-title';
+    heading.remove();
+    const link = row.querySelector('a');
+    link.className = 'card-link';
+    link.innerHTML = link.innerHTML + ' &rarr;';
+    link.remove();
     li.innerHTML = row.innerHTML;
     [...li.children].forEach((div) => {
-      if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
+      if (div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
     });
-    ul.append(li);
+    li.addEventListener('click',()=> {
+      link.click();
+    })
+    const cardWrapper = document.createElement('div');
+    cardWrapper.className = 'card-wrapper';
+    cardWrapper.append(heading);
+    cardWrapper.append(li);
+    ul.append(cardWrapper);
+    li.querySelector('div.cards-card-body').append(link);
   });
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
