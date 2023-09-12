d264380c1617b331b4e2605f5941b9673db522b5
rgravitvl <ravitvl83@gmail.com>
2023-09-12 17:17:19 +0530

    initial commit for coveo analytics

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,14 +1,15 @@
 // eslint-disable-next-line import/no-cycle
-import { sampleRUM } from './lib-franklin.js';
+import { sampleRUM } from "./lib-franklin.js";
 
 // Core Web Vitals RUM collection
-sampleRUM('cwv');
+sampleRUM("cwv");
 
 // add more delayed functionality here
-// google tag manager
+// google tag manager -start
 function loadGTM() {
-  const scriptTag = document.createElement('script');
+  const scriptTag = document.createElement("script");
   scriptTag.innerHTML = `
+      let gtmId = window.DanaherConfig !== undefined ? window.DanaherConfig.gtmID : 'GTM-THXPLCS';
       // googleTagManager
       (function (w, d, s, l, i) {
           w[l] = w[l] || [];
@@ -22,14 +23,90 @@ function loadGTM() {
           j.src =
               'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
           f.parentNode.insertBefore(j, f);
-      })(window, document, 'script', 'dataLayer', 'GTM-KCBGM2N');
+      })(window, document, 'script', 'dataLayer', gtmId);
       `;
   document.head.prepend(scriptTag);
 }
+// google tag manager -end
+
+//coveo analytics - start
+(function (c, o, v, e, O, u, a) {
+  a = "coveoua";
+  c[a] =
+    c[a] ||
+    function () {
+      (c[a].q = c[a].q || []).push(arguments);
+    };
+  c[a].t = Date.now();
+  u = o.createElement(v);
+  u.async = 1;
+  u.src = e;
+  O = o.getElementsByTagName(v)[0];
+  O.parentNode.insertBefore(u, O);
+})(
+  window,
+  document,
+  "script",
+  "https://static.cloud.coveo.com/coveo.analytics.js/2/coveoua.js"
+);
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+}
+
+let clientId = getCookie("coveo_visitorId");
+const isInternal = typeof getCookie("exclude-from-analytics") != "undefined";
+let pageName =
+  window.atPageParams !== undefined ? window.atPageParams.page : "";
+
+let accessToken =
+  window.DanaherConfig !== undefined
+    ? window.DanaherConfig.searchKey
+    : "xxf2f10385-5a54-4a18-bb48-fd8025d6b5d2";
+let organizationId =
+  window.DanaherConfig !== undefined
+    ? window.DanaherConfig.searchOrg
+    : "danaherproductionrfl96bkr";
+
+let customMetadata = {
+  contentIdKey: "contentpath",
+  contentIdValue: "/content/danaher/ls/us/en",
+  language: "en",
+  title: "https://lifesciences.danaher.com/us/en.html",
+  location: "Danaher Life Sciences | Drug Discovery & Development Solutions",
+  clientId: clientId,
+  anonymous: true,
+  customData: {
+    context_internal: isInternal,
+  },
+};
 
 if (
-  !window.location.hostname.includes('localhost')
-  && !document.location.hostname.includes('.hlx.page')
+  !window.location.hostname.includes("localhost") &&
+  !document.location.hostname.includes(".hlx.page")
 ) {
   loadGTM();
+  coveoua(
+    "init",
+    accessToken,
+    "https://" + organizationId + ".analytics.org.coveo.com"
+  );
+
+  if (pageName !== "" && !pageName.includes("products")) {
+    coveoua("send", "view", customMetadata);
+  }
+
+  coveoua("ec:setAction", "detail");
+  coveoua("send", "event");
 }
