f82d8d85f4616906a06b0161a36955b17a77d0ec
Deepak Kumar <deepak.kumar@dhlifesciences.com>
2023-09-12 10:14:13 -0400

    Removed remove function

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -8,25 +8,29 @@ export default function decorate(block) {
     const li = document.createElement('li');
     const heading = row.querySelector('h2');
     heading.className = 'card-title';
-    heading.remove();
     const link = row.querySelector('a');
     link.className = 'card-link';
     link.innerHTML += ' &rarr;';
-    link.remove();
+
+    const cardWrapper = document.createElement('div');
+    cardWrapper.className = 'card-wrapper';
+    cardWrapper.append(heading);
+    
     li.innerHTML = row.innerHTML;
+
     [...li.children].forEach((div) => {
       if (div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
     });
+
     li.addEventListener('click', () => {
       link.click();
     });
-    const cardWrapper = document.createElement('div');
-    cardWrapper.className = 'card-wrapper';
-    cardWrapper.append(heading);
+
     cardWrapper.append(li);
     ul.append(cardWrapper);
     li.querySelector('div.cards-card-body').append(link);
+    
   });
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
