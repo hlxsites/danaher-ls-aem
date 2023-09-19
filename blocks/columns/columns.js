b0ccbaa0f4693f00da6391dd585e667e7afb79ed
Alberto Di Cagno <adicagno@adobe.com>
2023-09-19 16:19:41 +0200

    fix: columns image load error breaks layout

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -2,9 +2,19 @@ export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
 
+  const imageAspectRatio = 1.7778;
+
   // setup image columns
   [...block.children].forEach((row) => {
     [...row.children].forEach((col) => {
+      const img = col.querySelector('img');
+      if (img) {
+        img.onerror = function () {
+          img.width = img.width;
+          img.height = `${Math.floor(img.width / imageAspectRatio)}`;
+        };
+      }
+
       const pic = col.querySelector('picture');
       if (pic) {
         const picWrapper = pic.closest('div');
