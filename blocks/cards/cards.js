200cadc1a1cea38ed24fecd0c5e099c849bc3ecc
Markus Haack <mhaack@adobe.com>
2023-09-20 10:17:46 +0200

    fix: card html structure

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,34 +1,29 @@
 import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
+import { a, li } from '../../scripts/dom-builder.js';
 
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
 
   [...block.children].forEach((row) => {
-    const li = document.createElement('li');
     const heading = row.querySelector('h2');
     heading.className = 'card-title';
-    const link = row.querySelector('a');
-    link.className = 'card-link';
-    link.innerHTML += ' &rarr;';
+    const cardLink = a({ class: 'card-wrapper' });
+    const card = li(heading, cardLink);
 
-    const cardWrapper = document.createElement('div');
-    cardWrapper.className = 'card-wrapper';
-    cardWrapper.append(heading);
-    li.innerHTML = row.innerHTML;
+    cardLink.innerHTML = row.innerHTML;
 
-    [...li.children].forEach((div) => {
+    [...cardLink.children].forEach((div) => {
       if (div.querySelector('picture, img')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
     });
 
-    li.addEventListener('click', () => {
-      link.click();
-    });
+    const readMoreLink = row.querySelector('a');
+    readMoreLink.innerHTML += ' &rarr;';
+    readMoreLink.className = 'card-link';
+    card.querySelector('div.cards-card-body').append(readMoreLink);
 
-    cardWrapper.append(li);
-    ul.append(cardWrapper);
-    li.querySelector('div.cards-card-body').append(link);
+    ul.append(card);
   });
   ul.querySelectorAll('img').forEach((img) => {
     const picture = img.closest('picture');
