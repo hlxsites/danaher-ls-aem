65cef28a5b64add078815ac6f8a3836d6202b1d2
rgravitvl <ravitvl83@gmail.com>
2023-09-29 12:56:14 +0530

    Updated for blog detail page

--- tools/importer/import.js
+++ tools/importer/import.js
@@ -208,33 +208,37 @@ const createEventCards = (main, document) => {
   });
 };
 
-const createColumn = (main, document) => {
-  main.querySelectorAll('fulllayout').forEach((fl) => {
-    fl.querySelectorAll('grid[columns="2"]').forEach((item) => {
-      const columns = [];
-      const templates = item.querySelectorAll('template');
-      if (templates.length > 2) {
-        const featureImage = templates[0].content.querySelector('div.featureimage');
-        const imageText = templates[1].content.querySelector('imagetext');
-        if (featureImage) {
-          columns.push(featureImage);
-        }
-        if (imageText) {
-          const img = document.createElement('img');
-          img.setAttribute('src', imageText.getAttribute('image'));
-          columns.push(img);
-        }
-        const cells = [
-          ['Columns'],
-          [...columns],
-        ];
-
-        if (columns.length > 0) {
-          const block = WebImporter.DOMUtils.createTable(cells, document);
-          fl.append(block);
-        }
+const createTwoColumn = (main, document) => {
+  main.querySelectorAll('grid[columns="2"]').forEach((item) => {
+    const columns = [];
+    const templates = item.querySelectorAll('template');
+    if (templates.length > 2) {
+      const featureImage = templates[0].content.querySelector('div.featureimage');
+      const imageText = templates[1].content.querySelector('imagetext');
+      if (featureImage.firstElementChild.getAttribute('btnhref')) {
+        const anc = document.createElement('a');
+        anc.href = featureImage.firstElementChild.getAttribute('btnhref');
+        anc.textContent = featureImage.firstElementChild.getAttribute('btntext');
+        featureImage.append(anc);
       }
-    });
+      if (featureImage) {
+        columns.push(featureImage);
+      }
+      if (imageText) {
+        const img = document.createElement('img');
+        img.setAttribute('src', imageText.getAttribute('image'));
+        columns.push(img);
+      }
+      const cells = [
+        ['Columns'],
+        [...columns],
+      ];
+
+      if (columns.length > 0) {
+        const block = WebImporter.DOMUtils.createTable(cells, document);
+        item.append(block);
+      }
+    }
   });
 };
 
@@ -576,7 +580,7 @@ export default {
     createEventCards(main, document);
     createLogoCloud(main, document);
     createWeSee(main, document);
-    createColumn(main, document);
+    createTwoColumn(main, document);
     createBlogDetail(main, document);
 
     // we only create the footer and header if not included via XF on a page
