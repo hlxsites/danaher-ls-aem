5636fb3f312099874b5bac1e698f655abb480303
rgravitvl <ravitvl83@gmail.com>
2023-10-04 18:51:17 +0530

    updated header navigaton

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -522,23 +522,17 @@ function buildNavBlock(headerBlock) {
   const navHtmlBlock = div({ class: 'bg-white hidden lg:block' });
 
   // home link
-  const homeLink = a({ class: 'flex items-center !text-black text-lg hover:text-black', href: '/' }, 'Life Sciences');
+  const homeLink = a({ class: 'btn !bg-transparent !text-black !font-medium !ring-0 !border-0 !ring-offset-0 group relative', href: '/' }, 'Life Sciences');
   const homeLinkImg = span({ class: 'inline-block w-5 ml-2', style: 'filter: brightness(0) invert(0.5);' });
   homeLinkImg.className = 'inline-block w-5 ml-2';
-  homeLinkImg.innerHTML = `
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" class="inline-block h-5 w-5 ml-3 text-gray-500">
-      <path d="M11.47 3.84a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.06l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 0 0 1.061 1.06l8.69-8.69z"/>
-      <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.43z"/>
-    </svg>
-  `;
   homeLink.append(homeLinkImg);
 
   // main nav
   const navWrapper = div({ class: 'megamenu mx-auto max-w-7xl bg-white' });
-  const pageNav = nav({ class: 'flex content-start' });
+  const pageNav = nav({ class: 'flex flex-wrap-reverse content-end' });
   pageNav.append(
     div(
-      { class: 'flex-none flex-grow-0 ml-1 mr-14 lg:flex lg:pl-8 xl:pl-4 items-center' },
+      { class: 'py-4 space-x-4 hoverable' },
       homeLink,
     ),
   );
