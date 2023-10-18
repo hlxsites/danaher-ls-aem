a331c78b2b11f77a3be597911b575a23ac32635c
Deepak Kumar <deepak.kumar@dhlifesciences.com>
2023-10-18 16:22:46 -0400

    Lint updates

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,4 +1,4 @@
 export default function decorate(block) {
-    const main = document.querySelector('main');
-    main.prepend(block);
+  const main = document.querySelector('main');
+  main.prepend(block);
 }
