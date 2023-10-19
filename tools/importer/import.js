e57af0144cced4d11be949ac9eae41f0bc52ee25
rgravitvl <ravitvl83@gmail.com>
2023-10-19 18:48:38 +0530

    updated options to pdf embed block

--- tools/importer/import.js
+++ tools/importer/import.js
@@ -127,10 +127,16 @@ const render = {
   },
   pdfembed: (embedEl, document) => {
     const pdfEl = embedEl?.querySelector('div.cmp-pdfviewer');
+    const data = JSON.parse(decodeURIComponent(pdfEl.getAttribute('data-cmp-viewer-config-json')));
+    let blockOptions = data.embedMode;
+    if (data.showFullScreen) blockOptions += ', showFullScreen';
+    if (data.showDownloadPDF) blockOptions += ', showDownload';
+    if (data.showPrintPDF) blockOptions += ', showPrint';
+    if (blockOptions.startsWith(', ')) blockOptions = blockOptions.slice(2);
     const anc = document.createElement('a');
     anc.href = pdfEl.getAttribute('data-cmp-document-path');
     anc.textContent = 'PDF Viewer';
-    const block = [['embed'], [anc]];
+    const block = [[`embed (${blockOptions})`], [anc]];
     const table = WebImporter.DOMUtils.createTable(block, document);
     embedEl.append(table);
   },
