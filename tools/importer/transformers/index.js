d5f1b7e3f6b000fda00a70a537c0959ea861aff6
rgravitvl <ravitvl83@gmail.com>
2023-10-26 19:50:51 +0530

    split of header and footer code importer

---
+++ tools/importer/transformers/index.js
@@ -0,0 +1,21 @@
+import header from './header.js';
+import footer from './footer.js';
+import stickyFooter from './stickyFooter.js';
+
+// eslint-disable-next-line import/prefer-default-export
+export const xfTransformers = [
+  footer,
+  stickyFooter,
+];
+
+export const xfAsyncTransformers = [
+  header,
+];
+
+export const preTransformers = [
+
+];
+
+export const postTransformers = [
+
+];
