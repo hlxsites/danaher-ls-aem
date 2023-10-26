2865309f8e2e8bbb883fc1b6b556ea8b7d429312
Amlan Sengupta <amlans3@gmail.com>
2023-10-26 21:32:44 +0530

    fetching & displaying content as fragment

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,6 +1,22 @@
-import { button, div, span } from '../../scripts/dom-builder.js';
+import { decorateIcons, loadScript } from '../../scripts/lib-franklin.js';
 
-import { readBlockConfig, decorateIcons, loadScript } from '../../scripts/lib-franklin.js';
+async function getFragmentFromFile(fragmentURL) {
+  const response = await fetch(fragmentURL);
+  if (!response.ok) {
+    // eslint-disable-next-line no-console
+    console.error('error loading fragment details', response);
+    return null;
+  }
+  const text = await response.text();
+  if (!text) {
+    // eslint-disable-next-line no-console
+    console.error('fragment details empty', fragmentURL);
+    return null;
+  }
+  const parser = new DOMParser();
+  const html = parser.parseFromString(text, 'text/html');
+  return html.body.children;
+}
 
 function loadAccessibe() {
   loadScript('../../scripts/lib-accessibe.js');
@@ -12,36 +28,14 @@ function scrollToTop() {
 
 function generateStickyFooter(stickyFooter) {
   const stickyFooterClone = stickyFooter[0].cloneNode(true);
-  const allNestedAnchorChilds = stickyFooterClone.querySelectorAll('a');
-  const container = div({ class: 'sticky-footer' });
-  const wrapper = div({ class: 'btn-group' });
-  allNestedAnchorChilds.forEach((child) => {
-    const childClone = child.cloneNode(true);
-    childClone.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" aria-label="${child.textContent}" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" class="w-5 h-5 users"><path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"></path></svg> <span class="hidden md:block">${child.textContent}</span>`;
-    childClone.className = 'btn';
-    const url = new URL(childClone.href);
-    url.searchParams.set('utm_previouspage', window.location.href.split('?')[0]);
-    childClone.href = url.href;
-    childClone.title = childClone.textContent.trim();
-    wrapper.appendChild(childClone);
-  });
-  const topFeature = button(
-    { class: 'btn', title: 'Top', 'aria-label': 'scroll to top of the page' },
-    span({ class: 'icon icon-chevron-up block w-5 h-5' }),
-    span({ class: 'hidden md:block' }, 'Top'),
-  );
-  topFeature.addEventListener('click', scrollToTop);
-  wrapper.appendChild(topFeature);
+  const accessibeBtn = stickyFooterClone.children[stickyFooterClone.children.length - 1];
+  const topBtn = stickyFooterClone.querySelector('button.scroll-top');
+  topBtn.addEventListener('click', scrollToTop);
   stickyFooter[0].remove();
-  container.append(wrapper);
-  const accessibe = button(
-    { class: 'accessibility', 'aria-label': 'users', 'data-acsb-custom-trigger': 'true' },
-    span({ class: 'icon icon-accessibe block w-5 md:w-6 h-5 md:h-6' }),
-  );
-  accessibe.addEventListener('click', loadAccessibe);
-  container.append(accessibe);
-  decorateIcons(container);
-  document.body.appendChild(container);
+  accessibeBtn.addEventListener('click', loadAccessibe);
+  stickyFooterClone.append(accessibeBtn);
+  decorateIcons(stickyFooterClone);
+  document.body.appendChild(stickyFooterClone);
 }
 
 /**
@@ -49,24 +43,21 @@ function generateStickyFooter(stickyFooter) {
  * @param {Element} block The footer block element
  */
 export default async function decorate(block) {
-  const cfg = readBlockConfig(block);
-  block.textContent = '';
-
-  // fetch footer content
-  const footerPath = cfg.footer || '/fragments/footer/';
-  const resp = await fetch(`${footerPath}master.plain.html`, window.location.pathname.endsWith('/master.plain.html') ? { cache: 'reload' } : {});
-
-  if (resp.ok) {
-    const html = await resp.text();
-
-    // decorate footer DOM
-    const footer = document.createElement('div');
-    footer.innerHTML = html;
-    generateStickyFooter(footer.getElementsByClassName('sticky-footer'));
-    const manageCookiesLink = footer.querySelector('ul>li>a[href="#manage-cookies"]');
-    manageCookiesLink.classList.add('ot-sdk-show-settings');
-    manageCookiesLink.href = '/#';
-    decorateIcons(footer);
-    block.append(footer);
+  try {
+    // get the content
+    const fragment = await getFragmentFromFile('/fragments/footer.html');
+    block.innerHTML = '';
+    if (fragment && fragment.length > 0) {
+      [...fragment].forEach((item) => {
+        if (item.className === 'sticky-footer') {
+          generateStickyFooter([item]);
+        } else block.append(item);
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    block.textContent = '';
+    // eslint-disable-next-line no-console
+    console.warn(`cannot load snippet at ${url}: ${e}`);
   }
 }
