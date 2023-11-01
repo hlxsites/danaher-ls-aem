978b63fca3fe7c8e3b195c6a12df4d51b5e37438
rgravitvl <ravitvl83@gmail.com>
2023-11-01 21:18:14 +0530

    Trying get from footer folder

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -45,15 +45,16 @@ function generateStickyFooter(stickyFooter) {
 export default async function decorate(block) {
   try {
     // get the content
-    const fragment = await getFragmentFromFile('/fragments/footer.plain.html');
+    const fragment = await getFragmentFromFile('/fragments/footer/master.plain.html');
     block.innerHTML = '';
+    const divEl = document.createElement('div');
     if (fragment && fragment.length > 0) {
       document.querySelector('.footer').parentElement.className += ' bg-danaherpurple-800';
       document.querySelector('.footer').className += ' max-w-7xl mx-auto text-white py-6 px-6 sm:px-6 lg:px-8 divide-y divide-danaherpurple-500 print:hidden';
       [...fragment].forEach((item) => {
         if (item.className === 'sticky-footer') {
           generateStickyFooter([item]);
-        } else block.append(item);
+        } else block.innerHTML = block.innerHTML + item.outerHTML;
       });
     }
   } catch (e) {
