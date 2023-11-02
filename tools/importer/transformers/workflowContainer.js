4e991fb742c4798de69ab462d96df16bc2f8a935
rgravitvl <ravitvl83@gmail.com>
2023-11-02 10:13:49 +0530

    addressed review comments

--- tools/importer/transformers/workflowContainer.js
+++ tools/importer/transformers/workflowContainer.js
@@ -2,7 +2,7 @@
 const createWorkflowContainerSection = (main, document) => {
   main.querySelectorAll('div.container-workflow').forEach((e, i, arr) => {
     if (i === arr.length - 1) {
-      const cells = [['Section Metadata'], ['style', 'workflow-container-two-col']];
+      const cells = [['Section Metadata'], ['style', 'container-two-col']];
       const table = WebImporter.DOMUtils.createTable(cells, document);
       e.append(table, document.createElement('hr'));
     }
