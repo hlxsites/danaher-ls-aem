312cf32224ef61f7aafeff1a3df683c4a35991a7
rgravitvl <ravitvl83@gmail.com>
2023-11-03 18:30:51 +0530

    updated review comments

--- blocks/related-articles/related-articles.js
+++ blocks/related-articles/related-articles.js
@@ -45,7 +45,7 @@ export default async function decorate(block) {
   const articleType = getMetadata('template').toLowerCase();
   const articleKeyword = getMetadata('keywords')?.toLowerCase();
   const url = new URL(getMetadata('og:url'));
-  let articles = await ffetch('/us/en/query-index.json')
+  let articles = await ffetch('/us/en/article-index.json')
     .filter(({ type }) => type.toLowerCase() === articleType)
     .filter(({ keywords }) => keywords.toLowerCase() === articleKeyword)
     .filter((article) => url.pathname !== article.path)
