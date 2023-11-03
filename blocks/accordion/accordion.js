914fdd0fd154555d95a9b76d36441df9793f34ce
rgravitvl <ravitvl83@gmail.com>
2023-11-03 15:46:23 +0530

    updated as per review comments

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -10,23 +10,21 @@ function toggleAccordion(activeButton, panel) {
   panel.classList.toggle('hidden', isOpen);
 }
 
-function createAccordionBlock(question, answer, index) {
-  const buttonId = `button-${index}`;
-  const panelId = `panel-${index}`;
+function createAccordionBlock(question, answer) {
   const divEl = div();
   const btn = dt(
-    { id: buttonId, class: 'close button' },
+    { class: 'close button' },
     button(
       { type: 'button', class: 'flex w-full items-start justify-between text-left text-gray-900' },
       h3({ class: 'text-base font-semibold leading-7' }, question),
       span({ class: 'ml-6 flex h-14 items-center pr-2' }),
     ),
   );
-  btn.querySelector('span').innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" class="chevy ml-2 h-5 w-5 transition"><path fill-rule="evenodd" d="M12.53 16.28a.75.75 0 01-1.06 0l-7.5-7.5a.75.75 0 011.06-1.06L12 14.69l6.97-6.97a.75.75 0 111.06 1.06l-7.5 7.5z"></path></svg>';
+  btn.querySelector('span').innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="ml-2 h-5 w-5 transition"><path fill-rule="evenodd" d="M12.53 16.28a.75.75 0 01-1.06 0l-7.5-7.5a.75.75 0 011.06-1.06L12 14.69l6.97-6.97a.75.75 0 111.06 1.06l-7.5 7.5z"></path></svg>';
 
   const panel = dd(
-    { class: 'hidden panal mt-2 pr-12', id: panelId },
-    p({ class: 'text-base leading-7 text-gray-600 href-text v-html' }),
+    { class: 'hidden panal mt-2 pr-12' },
+    p({ class: 'text-base leading-7 text-gray-600 href-text' }),
     p(answer),
   );
 
