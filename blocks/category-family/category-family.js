3933ba92f7abb5b0b01ecfb385f15073e17631f7
Deepak Kumar <deepak.kumar@dhlifesciences.com>
2023-11-08 15:09:41 -0500

    Grid style fixes

--- blocks/category-family/category-family.js
+++ blocks/category-family/category-family.js
@@ -29,122 +29,124 @@ const categoryFamily = `
             </div>
             <!-- GRID VIEW -->
             <atomic-did-you-mean></atomic-did-you-mean>
-            <atomic-result-list display="grid" image-size="medium" density="compact">
-              <atomic-result-template>
-                <template>
-                    <style>
-                        .mailbox{
-                            position: relative;
-                            z-index: 10;
-                            margin-left: 1px;
-                            margin-right: 1px;
-                            display: flex;
-                            height: 100%;
-                            min-height: 22rem;
-                            width: 100%;
-                            min-width: -moz-min-content;
-                            min-width: min-content;
-                            cursor: pointer;
-                            flex-direction: column;
-                            overflow: hidden;
-                            border-radius: 0.375rem;
-                            border-width: 1px;
-                            --tw-bg-opacity: 1;
-                            background-color: rgb(255 255 255 / var(--tw-bg-opacity));
-                            --tw-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
-                            --tw-shadow-colored: 0 4px 6px -1px var(--tw-shadow-color), 0 2px 4px -2px var(--tw-shadow-color);
-                            box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
-                            transition-property: color, background-color, border-color, text-decoration-color, fill, stroke, opacity, box-shadow, transform, filter, -webkit-backdrop-filter;
-                            transition-property: color, background-color, border-color, text-decoration-color, fill, stroke, opacity, box-shadow, transform, filter, backdrop-filter;
-                            transition-property: color, background-color, border-color, text-decoration-color, fill, stroke, opacity, box-shadow, transform, filter, backdrop-filter, -webkit-backdrop-filter;
-                            transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
-                            transition-duration: 150ms;
-                        }
-                        .mailbox:hover {
-                          --tw-scale-x: 1.05;
-                          --tw-scale-y: 1.05;
-                          transform: translate(var(--tw-translate-x), var(--tw-translate-y)) rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y));
-                        }
-                        .mailbox-image {
-                            padding-top: 1rem;
-                            padding-bottom: 1rem;
-                            height: 12rem;
-                        }
-                    </style>
-                    <div class="mailbox">
-                        <div class="mailbox-image">
-                            <atomic-result-image field="images" aria-hidden="true" class="category-image"></atomic-result-image>
-                        </div>
-                        <div class="mailbox-body">
-                            <style>
-                                .description {
-                                    overflow: hidden;
-                                    display: -webkit-box;
-                                    -webkit-box-orient: vertical;
-                                    -webkit-line-clamp: 3;
-                                    font-size: 0.875rem;
-                                    line-height: 1.25rem;
-                                    height: 4rem;
-                                    padding-left: 1rem;
-                                    padding-right: 1rem;
-                                    margin-bottom: 0.75rem;
-                                    --tw-text-opacity: 1;
-                                    color: rgb(107 114 128 / var(--tw-text-opacity));
-                                }
-                                .title{
-                                    overflow: hidden;
-                                    display: -webkit-box;
-                                    -webkit-box-orient: vertical;
-                                    overflow-wrap: break-word;
-                                    font-size: 1.25rem;
-                                    line-height: 1.75rem;
-                                    font-weight: 700;
-                                    line-height: 1.25;
-                                    height: 5rem;
-                                    letter-spacing: 0em;
-                                    padding-left: 1rem;
-                                    padding-right: 1rem;
-                                }
-                                .mailbox-footer{
-                                    display: flex;
-                                    width: 100%;
-                                    align-items: center;
-                                    gap: 0.125rem;
-                                    --tw-bg-opacity: 1;
-                                    background-color: rgb(243 244 246 / var(--tw-bg-opacity));
-                                    font-size: 1rem;
-                                    line-height: 1.5rem;
-                                    font-weight: 600;
-                                    --tw-text-opacity: 1;
-                                    height: 3.5rem;
-                                    color: rgb(13 49 114 / var(--tw-text-opacity));
-                                }
-                                .mailbox-action{
-                                    display: flex;
-                                    width: 100%;
-                                    padding-left: 1rem;
-                                }
-                            </style>
-                            <atomic-result-title class="title">
-                                <atomic-result-link field="clickUri"></atomic-result-link>
-                            </atomic-result-title>
-                            <div class="description">
-                                <atomic-result-text class="" field="description"></atomic-result-text>
-                            </div>
-                        </div>
-                        <div class="mailbox-footer">
-                            <span class="mailbox-action" aria-label="View Products">View Products
-                                <span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
-                                    </svg>
-                                </span>
-                            </span>
-                        </div>
-                    </div>
-                </template>
-              </atomic-result-template>
-            </atomic-result-list>
+            <atomic-layout-section section="pagination"">
+              <atomic-result-list display="grid" image-size="medium" density="compact">
+                <atomic-result-template>
+                  <template>
+                      <style>
+                          .mailbox{
+                              position: relative;
+                              z-index: 10;
+                              margin-left: 1px;
+                              margin-right: 1px;
+                              display: flex;
+                              height: 100%;
+                              min-height: 22rem;
+                              width: 100%;
+                              min-width: -moz-min-content;
+                              min-width: min-content;
+                              cursor: pointer;
+                              flex-direction: column;
+                              overflow: hidden;
+                              border-radius: 0.375rem;
+                              border-width: 1px;
+                              --tw-bg-opacity: 1;
+                              background-color: rgb(255 255 255 / var(--tw-bg-opacity));
+                              --tw-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
+                              --tw-shadow-colored: 0 4px 6px -1px var(--tw-shadow-color), 0 2px 4px -2px var(--tw-shadow-color);
+                              box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
+                              transition-property: color, background-color, border-color, text-decoration-color, fill, stroke, opacity, box-shadow, transform, filter, -webkit-backdrop-filter;
+                              transition-property: color, background-color, border-color, text-decoration-color, fill, stroke, opacity, box-shadow, transform, filter, backdrop-filter;
+                              transition-property: color, background-color, border-color, text-decoration-color, fill, stroke, opacity, box-shadow, transform, filter, backdrop-filter, -webkit-backdrop-filter;
+                              transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+                              transition-duration: 150ms;
+                          }
+                          .mailbox:hover {
+                            --tw-scale-x: 1.05;
+                            --tw-scale-y: 1.05;
+                            transform: translate(var(--tw-translate-x), var(--tw-translate-y)) rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y));
+                          }
+                          .mailbox-image {
+                              padding-top: 1rem;
+                              padding-bottom: 1rem;
+                              height: 12rem;
+                          }
+                      </style>
+                      <div class="mailbox">
+                          <div class="mailbox-image">
+                              <atomic-result-image field="images" aria-hidden="true" class="category-image"></atomic-result-image>
+                          </div>
+                          <div class="mailbox-body">
+                              <style>
+                                  .description {
+                                      overflow: hidden;
+                                      display: -webkit-box;
+                                      -webkit-box-orient: vertical;
+                                      -webkit-line-clamp: 3;
+                                      font-size: 0.875rem;
+                                      line-height: 1.25rem;
+                                      height: 4rem;
+                                      padding-left: 1rem;
+                                      padding-right: 1rem;
+                                      margin-bottom: 0.75rem;
+                                      --tw-text-opacity: 1;
+                                      color: rgb(107 114 128 / var(--tw-text-opacity));
+                                  }
+                                  .title{
+                                      overflow: hidden;
+                                      display: -webkit-box;
+                                      -webkit-box-orient: vertical;
+                                      overflow-wrap: break-word;
+                                      font-size: 1.25rem;
+                                      line-height: 1.75rem;
+                                      font-weight: 700;
+                                      line-height: 1.25;
+                                      height: 5rem;
+                                      letter-spacing: 0em;
+                                      padding-left: 1rem;
+                                      padding-right: 1rem;
+                                  }
+                                  .mailbox-footer{
+                                      display: flex;
+                                      width: 100%;
+                                      align-items: center;
+                                      gap: 0.125rem;
+                                      --tw-bg-opacity: 1;
+                                      background-color: rgb(243 244 246 / var(--tw-bg-opacity));
+                                      font-size: 1rem;
+                                      line-height: 1.5rem;
+                                      font-weight: 600;
+                                      --tw-text-opacity: 1;
+                                      height: 3.5rem;
+                                      color: rgb(13 49 114 / var(--tw-text-opacity));
+                                  }
+                                  .mailbox-action{
+                                      display: flex;
+                                      width: 100%;
+                                      padding-left: 1rem;
+                                  }
+                              </style>
+                              <atomic-result-title class="title">
+                                  <atomic-result-link field="clickUri"></atomic-result-link>
+                              </atomic-result-title>
+                              <div class="description">
+                                  <atomic-result-text class="" field="description"></atomic-result-text>
+                              </div>
+                          </div>
+                          <div class="mailbox-footer">
+                              <span class="mailbox-action" aria-label="View Products">View Products
+                                  <span>
+                                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                          <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+                                      </svg>
+                                  </span>
+                              </span>
+                          </div>
+                      </div>
+                  </template>
+                </atomic-result-template>
+              </atomic-result-list>
+            </atomic-layout-section>
             <atomic-layout-section section="pagination">
               <style>
                 .pagination {
