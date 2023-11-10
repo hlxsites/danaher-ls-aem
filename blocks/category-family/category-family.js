72ec092eb45e9011938cdf7f6e7cf0ab1cf62b4b
Markus Haack <mhaack@adobe.com>
2023-11-10 15:09:36 +0100

    fix: category URL extraction

--- blocks/category-family/category-family.js
+++ blocks/category-family/category-family.js
@@ -160,7 +160,8 @@ const isOTEnabled = () => {
 };
 
 export default async function decorate(block) {
-  const category = getMetadata('category');
+  const paths = window.location.pathname.replace('.html', '').split('/');
+  const category = paths.splice(4, paths.length).join('|');
   const host = (window.location.host === 'lifesciences.danaher.com') ? window.location.host : 'stage.lifesciences.danaher.com';
 
   block.classList.add('pt-10');
