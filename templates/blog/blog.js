0a55989ac934fc48c2efe39dcbd082ee56edc243
Markus Haack <mhaack@adobe.com>
2023-11-13 15:13:17 +0100

    feat: implement JSON-LD for articles

--- templates/blog/blog.js
+++ templates/blog/blog.js
@@ -1,4 +1,42 @@
-import { buildBlock } from '../../scripts/lib-franklin.js';
+import { buildBlock, getMetadata } from '../../scripts/lib-franklin.js';
+import { makePublicUrl, setJsonLd } from '../../scripts/scripts.js';
+
+function buildJsonLd() {
+  const data = {
+    '@context': 'http://schema.org',
+    '@type': 'Article',
+    '@id': `https://lifesciences.danaher.com${makePublicUrl(window.location.pathname)}`,
+    headline: getMetadata('og:title'),
+    image: getMetadata('og:image'),
+    publisher: {
+      '@type': 'Organization',
+      name: 'Danaher Life Sciences',
+      logo: {
+        '@type': 'ImageObject',
+        url: 'https://lifesciences.danaher.com/content/dam/danaher/brand-logos/danaher/Logo.svg',
+      },
+    },
+    description: getMetadata('description'),
+    mainEntityOfPage: {
+      '@type': 'WebPage',
+      '@id': `https://lifesciences.danaher.com${makePublicUrl(window.location.pathname)}`,
+    },
+  };
+
+  if (getMetadata('creationdate')) data.datePublished = getMetadata('creationdate');
+  if (getMetadata('updatedate')) data.dateModified = getMetadata('updatedate');
+  if (getMetadata('authorname')) {
+    data.author = {
+      '@type': 'Person',
+      name: getMetadata('authorname'),
+    };
+  }
+
+  setJsonLd(
+    data,
+    'article',
+  );
+}
 
 export default async function buildAutoBlocks() {
   const main = document.querySelector('main');
@@ -48,4 +86,6 @@ export default async function buildAutoBlocks() {
     buildBlock('social-media', { elems: [] }),
     buildBlock('related-articles', { elems: [] }),
   );
+
+  buildJsonLd();
 }
