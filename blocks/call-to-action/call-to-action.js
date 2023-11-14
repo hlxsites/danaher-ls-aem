a45e182390ff36b89158e2bc0660916a3135f995
Amlan Sengupta <amlans3@gmail.com>
2023-11-14 22:44:05 +0530

    CTA action-button color change

--- blocks/call-to-action/call-to-action.js
+++ blocks/call-to-action/call-to-action.js
@@ -3,7 +3,7 @@ import { decorateModals } from '../../scripts/scripts.js';
 export default function decorate(block) {
   block.classList.add(...'ctasection mx-auto max-w-7xl lg:py-8'.split(' '));
   block.querySelector(':scope div > div').classList.add(...'md:flex space-y-8 md:space-y-0 md:flex-row w-full py-8 md:py-16 md:px-12 px-6 items-center md:justify-between bg-danaherpurple-800'.split(' '));
-  block.querySelector('h2').classList.add(...'text-2xl p-0 m-0 tracking-tight sm:text-3xl text-white font-normal'.split(' '));
-  block.querySelector('p').classList.add(...'show-modal-btn btn btn-outline-trending-brand rounded-full px-6 py-3'.split(' '));
+  block.querySelector('h2').classList.add(...'text-2xl p-0 m-0 tracking-tight sm:text-3xl text-white font-normal tracking-wide'.split(' '));
+  block.querySelector('p').classList.add(...'show-modal-btn btn btn-outline-trending-brand text-lg rounded-full px-6 py-3'.split(' '));
   decorateModals(block);
 }
