cb4ba094e463f9dd4099221c34d6694eb254fda0
wz914876 <sharanagouda_patil@pall.com>
2023-11-16 14:54:57 +0530

    social-media was missing on top of topic detail page

--- templates/topic/topic.js
+++ templates/topic/topic.js
@@ -5,9 +5,17 @@ export default async function buildAutoBlocks(block) {
     element.classList.add('font-semibold', 'text-xl', 'text-danahergray-900');
   });
   const main = document.querySelector('main');
-  const firstWrapper = main.querySelector(':scope > div');
+  const firstWrapper = main.querySelector(':scope > div:nth-child(2)');
+  let topicH1 = '';
+  const firstChildren = Array.from(firstWrapper.children);
+  firstChildren.every((child) => {
+    if (child.tagName === 'H1') {
+      topicH1 = child;
+    }
+  });
   firstWrapper.prepend(
     buildBlock('social-media', { elems: [] }),
+    buildBlock('heading', { elems: [topicH1] }),
   );
   const lastWrapper = main.querySelector(':scope > div:last-of-type');
   lastWrapper.append(
