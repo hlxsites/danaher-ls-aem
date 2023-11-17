518d81e9523ff8fb4079b4b12a083fbef5b9b4dc
Duy Nguyen <duy.phnguyen@gmail.com>
2023-11-17 10:16:22 +0100

    chore: use block options instead of content for variants

--- tools/importer/transformers/sideNav.js
+++ tools/importer/transformers/sideNav.js
@@ -2,8 +2,12 @@
 const sideNav = (main, document) => {
   const sideNavEl = main.querySelector('#side-nav');
   if (sideNavEl) {
-    const sideNavContent = sideNavEl?.textContent?.trim() || '';
-    const block = [['Side Nav'], [sideNavContent]];
+    let blockName = 'Side Nav';
+    const sideNavContent = sideNavEl?.textContent?.trim();
+    if (sideNavContent && sideNavContent.length < 65) {
+      blockName += ` (${sideNavEl.textContent})`;
+    }
+    const block = [[blockName], ['']];
     const table = WebImporter.DOMUtils.createTable(block, document);
     sideNavEl.replaceWith(table);
   }
