76ce9aead04e2e8eb03160a0d8f215d2eeab244c
Duy Nguyen <duy.phnguyen@gmail.com>
2023-11-20 15:43:27 +0100

    feat: sidebar for topics hub

--- blocks/side-nav/side-nav.js
+++ blocks/side-nav/side-nav.js
@@ -4,12 +4,19 @@ import { getMetadata } from '../../scripts/lib-franklin.js';
 import { makePublicUrl } from '../../scripts/scripts.js';
 import { fetchTopicsForCategory } from '../topic-list/topic-list.js';
 
+async function fetchAllProductCategories() {
+  const products = await ffetch('/us/en/products-index.json')
+    .filter(({ type, path }) => type === 'Category' && path.endsWith('/topics'))
+    .all();
+  return products.sort((item1, item2) => item1.title.localeCompare(item2.title));
+}
+
 function renderSideNav(sideNavItems) {
   const sideNavElements = div({ class: 'flex flex-col items-start' });
   sideNavItems.forEach((sideNavItem) => {
     sideNavElements.append(div(
       {
-        class: 'w-full side-nav-item',
+        class: 'w-full side-nav-item hover:bg-danaherpurple-25 hover:rounded-md',
       },
       div(
         {
@@ -30,7 +37,7 @@ export default async function decorate(block) {
   let sideNavTitle = 'Side Navigation';
   let selectedNavItem = null;
   let sideNavElements = div();
-  if (block.classList.contains('topic-content')) {
+  if (block.classList.contains('topics')) {
     const category = getMetadata('fullcategory');
     sideNavItems = await fetchTopicsForCategory(category);
     const categoryObj = await ffetch('/us/en/products-index.json')
@@ -39,10 +46,19 @@ export default async function decorate(block) {
     sideNavTitle = categoryObj?.title || category || sideNavTitle;
     sideNavElements = renderSideNav(sideNavItems);
     selectedNavItem = sideNavElements.querySelector(`.side-nav-item a[href="${window.location.pathname}"]`)?.closest('.side-nav-item');
+  } else if (block.classList.contains('products')) {
+    sideNavItems = await fetchAllProductCategories();
+    sideNavTitle = 'Products';
+    sideNavElements = renderSideNav(sideNavItems);
   }
-
-  if (selectedNavItem) selectedNavItem.classList.add('font-bold', 'bg-danaherpurple-50', 'rounded-md');
+  selectedNavItem = sideNavElements.querySelector(`.side-nav-item a[href="${window.location.pathname}"]`)?.closest('.side-nav-item');
+  if (selectedNavItem) selectedNavItem.classList.add('font-bold', 'bg-danaherpurple-50', 'hover:bg-danaherpurple-50', 'rounded-md');
   block.append(div({ class: 'text-lg px-5 py-4' }, strong(sideNavTitle)), sideNavElements);
   block.classList.add('pt-6', 'pr-2');
+
+  const main = document.querySelector('main');
+  main.classList.add('grid', 'px-4', 'lg:px-0', 'max-w-7xl', 'mx-auto', 'auto-cols-auto');
+  main.querySelector(':scope > div.side-nav-container').classList.add('!col-span-1', '!col-start-1', 'row-span-6', 'hidden', 'lg:block');
+  main.querySelectorAll(':scope > div').forEach((div) => div.classList.add('lg:col-span-3', 'lg:col-start-2', 'space-y-4', 'mb-2', 'flex-1', 'p-0', 'pl-0', 'lg:pl-6'));
   return block;
 }
--- templates/topic/topic.js
+++ templates/topic/topic.js
@@ -4,7 +4,7 @@ import { buildBlock } from '../../scripts/lib-franklin.js';
 export default async function buildAutoBlocks() {
   const main = document.querySelector('main');
   const sideNavBlock = div(buildBlock('side-nav', { elems: [] }));
-  sideNavBlock.querySelector('.side-nav').classList.add('topic-content');
+  sideNavBlock.querySelector('.side-nav').classList.add('topics');
   main.firstElementChild.insertAdjacentElement('afterend', sideNavBlock);
   main.querySelector(':scope > div:nth-child(3)')?.prepend(buildBlock('social-media', { elems: [] }));
   main.lastElementChild.append(buildBlock('social-media', { elems: [] }));
