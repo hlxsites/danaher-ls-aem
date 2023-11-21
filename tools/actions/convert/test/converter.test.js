abf20fe494b4a89dc9f1a55c967b458c8ca30d48
Markus Haack <mhaack@adobe.com>
2023-11-21 18:06:11 +0100

    chore: update converter

--- tools/actions/convert/test/converter.test.js
+++ tools/actions/convert/test/converter.test.js
@@ -9,86 +9,99 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-/* eslint-env mocha */
 
-import assert from 'assert';
-import { readFile } from 'fs/promises';
-import { resolve } from 'path';
-import nock from 'nock';
-import { render } from '../src/index.js';
+import path from 'path';
+import { pipeline, toMocha } from 'crosswalk-converter';
+import converterCfg from '../../../../converter.yaml';
+import mappingCfg from '../../../../paths.yaml';
+import transform from '../../../importer/import.js';
 
-async function test(spec) {
-  const html = await readFile(resolve(__testdir, 'fixtures', `${spec}.html`), 'utf-8');
-  nock('http://www.example.com')
-    .get(`/${spec}.html`)
-    .reply(200, html);
-  const expected = await readFile(resolve(__testdir, 'fixtures', `${spec}-semantic.html`), 'utf-8');
-  const actual = await render(`/${spec}.html`, {}, {
-    env: {
-      publicURL: 'https://stage.lifesciences.danaher.com/',
-      aemURL: 'http://www.example.com',
-    },
-  });
-  assert.strictEqual(actual.html.trim(), expected.replaceAll('\r\n', '\n').trim());
-}
-
-describe('Converter Tests', () => {
-  before(() => {
-    nock.disableNetConnect();
+describe('Converter', async () => {
+  // eslint-disable-next-line no-undef
+  const fixturesFolder = path.resolve(__testdir, 'fixtures');
+  const testRunner = pipeline().wrap(toMocha, {
+    transform,
+    converterCfg,
+    mappingCfg,
+    fixturesFolder,
   });
 
-  after(() => {
-    nock.enableNetConnect();
-  });
-
-  afterEach(() => {
-    nock.cleanAll();
-  });
-
-  it('convert the footer html', async () => {
-    await test('footer');
-  });
-  it('convert the header html', async () => {
-    const json = await readFile(resolve(__testdir, 'fixtures', 'megamenu_items_us.json'), 'utf-8');
-    nock('https://stage.lifesciences.danaher.com')
-      .get('/content/dam/danaher/system/navigation/megamenu_items_us.json')
-      .reply(200, json);
-    await test('header');
-  });
-  it('convert the en html', async () => {
-    await test('en');
-  });
-  it('convert the blog html', async () => {
-    await test('blog');
-    await test('blog2');
-    await test('blog3');
-    await test('blog4');
-    await test('blog5');
-    await test('blog6');
-  });
-  it('convert the news html', async () => {
-    await test('news');
-  });
-  it('convert the product html', async () => {
-    await test('product');
-  });
-  it('convert the blog hub html', async () => {
-    await test('blog-hub');
-  });
-  it('convert the product category html', async () => {
-    await test('product-category');
-    await test('product-category1');
-  });
-  it('convert the product topic html', async () => {
-    await test('product-topic');
-  });
-  it('convert the topic hub html', async () => {
-    await test('topic-hub');
-  });
-  it('convert the library hub html', async () => {
-    await test('library-hub');
-  });
-  it('convert the application html', async () => {
-    await test('application');
-  });
+  await testRunner();
 });
+
+
+// async function test(spec) {
+//   const html = await readFile(resolve(__testdir, 'fixtures', `${spec}.html`), 'utf-8');
+//   nock('http://www.example.com')
+//     .get(`/${spec}.html`)
+//     .reply(200, html);
+//   const expected = await readFile(resolve(__testdir, 'fixtures', `${spec}-semantic.html`), 'utf-8');
+//   const actual = await render(`/${spec}.html`, {}, {
+//     env: {
+//       publicURL: 'https://stage.lifesciences.danaher.com/',
+//       aemURL: 'http://www.example.com',
+//     },
+//   });
+//   assert.strictEqual(actual.html.trim(), expected.replaceAll('\r\n', '\n').trim());
+// }
+
+// describe('Converter Tests', () => {
+//   before(() => {
+//     nock.disableNetConnect();
+//   });
+
+//   after(() => {
+//     nock.enableNetConnect();
+//   });
+
+//   afterEach(() => {
+//     nock.cleanAll();
+//   });
+
+//   it('convert the footer html', async () => {
+//     await test('footer');
+//   });
+//   it('convert the header html', async () => {
+//     const json = await readFile(resolve(__testdir, 'fixtures', 'megamenu_items_us.json'), 'utf-8');
+//     nock('https://stage.lifesciences.danaher.com')
+//       .get('/content/dam/danaher/system/navigation/megamenu_items_us.json')
+//       .reply(200, json);
+//     await test('header');
+//   });
+//   it('convert the en html', async () => {
+//     await test('en');
+//   });
+//   it('convert the blog html', async () => {
+//     await test('blog');
+//     await test('blog2');
+//     await test('blog3');
+//     await test('blog4');
+//     await test('blog5');
+//     await test('blog6');
+//   });
+//   it('convert the news html', async () => {
+//     await test('news');
+//   });
+//   it('convert the product html', async () => {
+//     await test('product');
+//   });
+//   it('convert the blog hub html', async () => {
+//     await test('blog-hub');
+//   });
+//   it('convert the product category html', async () => {
+//     await test('product-category');
+//     await test('product-category1');
+//   });
+//   it('convert the product topic html', async () => {
+//     await test('product-topic');
+//   });
+//   it('convert the topic hub html', async () => {
+//     await test('topic-hub');
+//   });
+//   it('convert the library hub html', async () => {
+//     await test('library-hub');
+//   });
+//   it('convert the application html', async () => {
+//     await test('application');
+//   });
+// });
--- tools/actions/convert/webpack.config.js
+++ tools/actions/convert/webpack.config.js
@@ -9,20 +9,27 @@
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
+const inspector = require('inspector');
 const path = require('path');
 const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+const ZipPlugin = require('zip-webpack-plugin');
 
-module.exports = {
-  mode: 'development',
-  devtool: 'source-map',
+const DEFAULT_CONFIG = {
   target: 'node',
   output: {
-    filename: 'main.js',
+    filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
     library: {
-      type: 'commonjs2',
+      name: 'main',
+      type: 'umd',
     },
   },
+  externals: [
+    'import node-fetch',
+    'openwhisk',
+    'express',
+  ],
   resolve: {
     modules: [
       'node_modules',
@@ -31,9 +38,9 @@ module.exports = {
       path.resolve(__dirname, 'node_modules'),
     ],
     alias: {
-      // alias for helix-m2docx as it has a dependency to adobe/fetch which did not build well with
-      // webpack: https://github.com/webpack/webpack/issues/16724. Even though its closed, still a
-      // problem with 5.88.2, but we don't need it anyway.
+      // alias for helix-m2docx as it has a dependency to adobe/fetch which did not build well
+      // with webpack: https://github.com/webpack/webpack/issues/16724. Even though its closed,
+      // still a problem with 5.88.2, but we don't need it anyway.
       '@adobe/helix-md2docx': false,
     },
   },
@@ -48,7 +55,7 @@ module.exports = {
     new webpack.DefinePlugin({ window: null }),
     // for those jsdom dependencies we want to throw a missing module error if they would be used
     // on the execution path
-    new webpack.IgnorePlugin({ resourceRegExp: /canvas/ }),
+    new webpack.IgnorePlugin({ resourceRegExp: /^canvas/ }),
     new webpack.IgnorePlugin({ resourceRegExp: /bufferutil/ }),
     new webpack.IgnorePlugin({ resourceRegExp: /utf-8-validate/ }),
   ],
@@ -58,6 +65,63 @@ module.exports = {
         test: /\.ya?ml$/,
         use: 'yaml-loader',
       },
+      {
+        test: /\.html?$/,
+        use: 'raw-loader',
+      },
     ],
   },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+          mangle: {
+            reserved: [
+              'fetchContent',
+              'html2md',
+              'parseMd',
+              'transformMdast',
+              'stringifyMdast',
+              'md2html',
+            ],
+          },
+        },
+      }),
+    ],
+  },
+};
+
+module.exports = (env, { mode = 'development' }) => {
+  let plugins = [...DEFAULT_CONFIG.plugins];
+  let output = { ...DEFAULT_CONFIG.output };
+  let devtool = 'source-map';
+
+  if (mode === 'production') {
+    // to zip the action
+    plugins = [
+      ...plugins,
+      new ZipPlugin({ include: [/\.js$/] }),
+    ];
+    devtool = false;
+  } else {
+    // for development:
+    // debugger connection open? (instant-mocha)
+    // eslint-disable-next-line no-lonely-if
+    if (typeof inspector?.url() !== 'undefined') {
+      devtool = 'eval-source-map';
+      output = {
+        ...output,
+        devtoolModuleFilenameTemplate: 'webpack:///[absolute-resource-path]',
+      };
+    }
+  }
+
+  return {
+    ...DEFAULT_CONFIG,
+    output,
+    plugins,
+    mode,
+    devtool,
+  };
 };
