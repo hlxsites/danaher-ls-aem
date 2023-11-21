b554012ed75cef1217347071072ca9f8e5aef5e0
Duy Nguyen <duy.phnguyen@gmail.com>
2023-11-21 12:46:55 +0100

    chore: update topic hub category

--- blocks/side-nav/side-nav.js
+++ blocks/side-nav/side-nav.js
@@ -5,10 +5,10 @@ import { makePublicUrl } from '../../scripts/scripts.js';
 import { fetchTopicsForCategory } from '../topic-list/topic-list.js';
 
 async function fetchAllProductCategories() {
-  const products = await ffetch('/us/en/products-index.json')
-    .filter(({ type, path }) => type === 'Category' && path.endsWith('/topics'))
+  const topicHubs = await ffetch('/us/en/products-index.json')
+    .filter(({ type, path }) => type === 'TopicHub' && path.endsWith('/topics'))
     .all();
-  return products.sort((item1, item2) => item1.title.localeCompare(item2.title));
+  return topicHubs.sort((item1, item2) => item1.title.localeCompare(item2.title));
 }
 
 function renderSideNav(sideNavItems) {
@@ -53,10 +53,5 @@ export default async function decorate(block) {
   if (selectedNavItem) selectedNavItem.classList.add('font-bold', 'bg-danaherpurple-50', 'hover:bg-danaherpurple-50', 'rounded-md');
   block.append(div({ class: 'text-lg px-5 py-4' }, strong(sideNavTitle)), sideNavElements);
   block.classList.add('pt-6', 'pr-2');
-
-  const main = document.querySelector('main');
-  main.classList.add('grid', 'px-4', 'lg:px-0', 'max-w-7xl', 'mx-auto', 'lg:grid-cols-4');
-  main.querySelector(':scope > div.side-nav-container').classList.add('lg:!col-span-1', 'lg:!col-start-1', 'row-span-6', 'hidden', 'lg:block');
-  main.querySelectorAll(':scope > div').forEach((child) => child.classList.add('lg:col-span-3', 'lg:col-start-2', 'space-y-4', 'mb-2', 'flex-1', 'p-0', 'pl-0', 'lg:pl-6'));
   return block;
 }
