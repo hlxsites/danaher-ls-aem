0c5a6428785eeaee1183f3dd09f8929ab99a0e0d
wz914876 <sharanagouda_patil@pall.com>
2023-11-22 17:51:08 +0530

    implement-template-for-library-pages

--- templates/library/library.js
+++ templates/library/library.js
@@ -1,13 +1,4 @@
-import { buildBlock } from '../../scripts/lib-franklin.js';
-
 export default async function buildAutoBlocks(block) {
   const main = document.querySelector('main');
-  const firstWrapper = main.querySelector(':scope > div');
-  firstWrapper.prepend(
-    buildBlock('social-media', { elems: [] }),
-  );
-  const lastWrapper = main.querySelector(':scope > div:last-of-type');
-  lastWrapper.append(
-    buildBlock('social-media', { elems: [] }),
-  );
+  main.prepend(block);
 }
