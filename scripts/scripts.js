9c5cdb5a574e82d5eeaa532a29839e7a3828baf3
Markus Haack <mhaack@adobe.com>
2023-11-23 14:19:07 +0100

    chore: disable decorateButtons as we are not using it and mostly work arround

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,7 +3,6 @@ import {
   buildBlock,
   loadHeader,
   loadFooter,
-  decorateButtons,
   decorateIcons,
   decorateSections,
   decorateBlocks,
@@ -318,8 +317,6 @@ function decorateTwoColumnSection(main) {
  */
 // eslint-disable-next-line import/prefer-default-export
 export function decorateMain(main) {
-  // hopefully forward compatible button decoration
-  decorateButtons(main);
   decorateIcons(main);
   buildAutoBlocks(main);
   decorateSections(main);
