7ebd0f9a8bb8a1cf2d7cd2a0bf896bcc6c743a67
rgravitvl <ravitvl83@gmail.com>
2023-11-27 14:26:43 +0530

    lint issue fixed

--- tools/importer/transformers/twoColumns.js
+++ tools/importer/transformers/twoColumns.js
@@ -7,22 +7,22 @@ const createTwoColumn = (main, document) => {
     const columns = [];
     const templates = item.querySelectorAll('template');
     [...templates].forEach((template) => {
-      if(template.content.children.length > 0){
+      if (template.content.children.length > 0) {
         const row = [];
-        [...template.content.children].forEach(element => {
-          if (element.className === 'featureimage') {  
+        [...template.content.children].forEach((element) => {
+          if (element.className === 'featureimage') {
             const featureImage = template.content.querySelector('div.featureimage');
             if (featureImage?.firstElementChild?.localName === 'feature-image') {
               featureimage(featureImage, document);
               WebImporter.DOMUtils.remove(featureImage, ['feature-image']);
             }
-  
+
             if (featureImage) {
               row.push(featureImage);
             }
           } else if (element.className === 'imagetext') {
             const imageText = template.content.querySelector('imagetext');
-  
+
             if (imageText) {
               const img = document.createElement('img');
               img.setAttribute('src', imageText.getAttribute('image'));
@@ -30,13 +30,13 @@ const createTwoColumn = (main, document) => {
             }
           } else if (element.className === 'heading-aem') {
             const heading = template.content.querySelector('div.heading-aem');
-            
+
             if (heading) {
               row.push(heading.firstElementChild);
             }
           } else if (element.className === 'heading') {
             const heading = template.content.querySelector('div.heading');
-            
+
             if (heading) {
               const headingEL = heading?.querySelector('heading');
               const hTag = headingEL?.getAttribute('headingtag') ? headingEL?.getAttribute('headingtag') : 'h1';
@@ -46,7 +46,7 @@ const createTwoColumn = (main, document) => {
             }
           } else if (element.className === 'script') {
             const featureImage = template.content.querySelector('div.featureimage');
-  
+
             if (featureImage) {
               row.push(featureImage);
             }
