38ebd75adb5ca2579af8b2904d0f5f7d5961f158
rgravitvl <ravitvl83@gmail.com>
2023-11-27 16:07:39 +0530

    updated as two col and multiple row

--- tools/importer/transformers/miniTeaser.js
+++ tools/importer/transformers/miniTeaser.js
@@ -4,56 +4,64 @@ const teaser = (xf, cards, document) => {
     const articleCard = tmp.content.querySelector('articlecard');
     if (articleCard) {
       const cardImg = articleCard.getAttribute('cardimg');
+      const altText = articleCard.getAttribute('imagealttext') ? articleCard.getAttribute('imagealttext') : 'Danaher Corporation';
       const cardTitle = articleCard.getAttribute('cardtitle');
       const cardHref = articleCard.getAttribute('cardhref');
       const cardLinkText = articleCard.getAttribute('linktext');
 
-      const divEl = document.createElement('div');
+      const leftDiv = document.createElement('div');
       const img = document.createElement('img');
       img.setAttribute('src', cardImg);
-      divEl.append(img);
+      img.setAttribute('alt', altText);
+      leftDiv.append(img);
+
+      const rightDiv = document.createElement('div');
       const h3 = document.createElement('h3');
       h3.textContent = cardTitle;
-      divEl.append(h3);
+      rightDiv.append(h3);
       const a = document.createElement('a');
       a.setAttribute('href', cardHref);
       a.textContent = cardLinkText;
-      divEl.append(a);
-      cards.push(divEl);
+      rightDiv.append(a);
+      cards.push([leftDiv, rightDiv]);
     }
     const featureimage = tmp.content.querySelector('feature-image');
     if (featureimage) {
       const cardImg = featureimage.getAttribute('img');
+      const cardImgAlt = featureimage?.getAttribute('imgalt') ? featureimage?.getAttribute('imgalt') : 'Danaher Corporation';
       const cardTitle = featureimage.getAttribute('title');
       const btnRfq = featureimage.getAttribute('btnrfq');
       const btnHref = featureimage.getAttribute('btnhref');
       const cardLinkText = featureimage.getAttribute('btntext');
 
-      const divEl = document.createElement('div');
+      const leftDiv = document.createElement('div');
       const img = document.createElement('img');
       img.setAttribute('src', cardImg);
-      divEl.append(img);
+      img.setAttribute('alt', cardImgAlt);
+      leftDiv.append(img);
+
+      const rightDiv = document.createElement('div');
       const h3 = document.createElement('h3');
       h3.textContent = cardTitle;
-      divEl.append(h3);
+      rightDiv.append(h3);
       const a = document.createElement('a');
       if (btnRfq) a.setAttribute('href', '#RequestAQuote');
       else a.setAttribute('href', btnHref);
       a.textContent = cardLinkText;
-      divEl.append(a);
-      cards.push(divEl);
+      rightDiv.append(a);
+      cards.push([leftDiv, rightDiv]);
     }
   });
 };
 
 const createMiniTeaser = (main, document) => {
-  const websiteLinks = main.querySelector('div#website-links');
+  const websiteLinks = main.querySelector('div#mini-teaser');
   if (websiteLinks) {
     const xf = websiteLinks.closest('div.experiencefragment');
     const cards = [];
     teaser(xf, cards, document);
     const cells = [['Mini Teasers'],
-      cards,
+      ...cards,
     ];
     if (cells.length > 0) {
       const block = WebImporter.DOMUtils.createTable(cells, document);
