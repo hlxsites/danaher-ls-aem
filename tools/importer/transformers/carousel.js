996b3d62b27bf100208840df6e6b4ab929b66dbd
wz914876 <sharanagouda_patil@pall.com>
2023-11-27 14:43:23 +0530

    Changed to '2 column' div structure.

--- tools/importer/transformers/carousel.js
+++ tools/importer/transformers/carousel.js
@@ -8,13 +8,13 @@ const createCarousel = (main, document) => {
     const carouselLists = JSON.parse(decodeHtmlEntities(carousel.getAttribute('slidedata')));
     cells.push(['carousel']);
     [...carouselLists].forEach((list) => {
-      const divEl = document.createElement('div');
+      const rightDiv = document.createElement('div');
       const title = document.createElement('p');
       title.innerHTML = list.title;
-      if (list.title) divEl.append(title);
+      if (list.title) rightDiv.append(title);
       const description = document.createElement('p');
       description.innerHTML = list.description;
-      if (list.description) divEl.append(description);
+      if (list.description) rightDiv.append(description);
       const btnText1 = list.btntitle1;
       const btnhref1 = list.btn1path;
       const rfqBtn1 = list.buttonRFQOne;
@@ -24,7 +24,7 @@ const createCarousel = (main, document) => {
         a1.setAttribute('href', rfqBtn1 ? '#request-quote' : btnhref1);
         a1.textContent = btnText1;
         p1.append(a1);
-        divEl.append(p1);
+        rightDiv.append(p1);
       }
       const btnText2 = list.btntitle2;
       const btnhref2 = list.btn2path;
@@ -35,8 +35,10 @@ const createCarousel = (main, document) => {
         a2.setAttribute('href', rfqBtn2 ? '#request-quote' : btnhref2);
         a2.textContent = btnText2;
         p2.append(a2);
-        divEl.append(p2);
+        rightDiv.append(p2);
       }
+
+      const leftDiv = document.createElement('div');
       const img = document.createElement('img');
       const altText = list.imgalt;
       img.setAttribute('src', list.image);
@@ -45,8 +47,9 @@ const createCarousel = (main, document) => {
       } else {
         img.setAttribute('alt', 'Danaher Corporation');
       }
-      divEl.append(img);
-      carousels.push(divEl);
+      leftDiv.append(img);
+
+      carousels.push(leftDiv, rightDiv);
     });
     cells.push(carousels);
     const block = WebImporter.DOMUtils.createTable(cells, document);
