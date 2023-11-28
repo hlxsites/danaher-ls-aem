703f16a95e9769a19bcc7233a1fb64ed5fdfed22
rgravitvl <ravitvl83@gmail.com>
2023-11-28 22:13:39 +0530

    updated for mobile view

--- blocks/mini-teasers/mini-teasers.js
+++ blocks/mini-teasers/mini-teasers.js
@@ -2,9 +2,10 @@ import { decorateModals } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
   block.parentNode.prepend(document.createElement('hr'));
-  block.classList.add(...'flex flex-row my-16'.split(' '));
+  // block.classList.add(...'flex flex-row my-16'.split(' '));
+  block.classList.add(...'grid max-w-7xl w-full mx-auto gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 px-4 sm:px-0 my-16'.split(' '));
   [...block.children].forEach((element) => {
-    element.classList.add(...'pr-60 w-full'.split(' '));
+    element.classList.add(...'items-start mr-20 lg:mr-56'.split(' '));
     element.querySelector('img')?.classList.add('py-1');
     element.querySelector('h3')?.classList.add(...'text-base p-0 my-1 sm:text-sm font-normal'.split(' '));
     const link = element.querySelector('a');
