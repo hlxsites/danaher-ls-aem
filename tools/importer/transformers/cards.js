facf292e3aac4b3f9523b5441a758dc5796d48bb
rgravitvl <ravitvl83@gmail.com>
2023-11-29 11:07:48 +0530

    updated for number of column

--- tools/importer/transformers/cards.js
+++ tools/importer/transformers/cards.js
@@ -1,48 +1,63 @@
 /* global WebImporter */
+const card = (tmp, cards, document) => {
+  const h2 = tmp.content.querySelector('h2');
+  const articleCard = tmp.content.querySelector('articlecard');
+  if (articleCard) {
+    const cardImg = articleCard.getAttribute('cardimg');
+    const altText = articleCard.getAttribute('imagealttext');
+    const cardTitle = articleCard.getAttribute('cardtitle');
+    const cardDescription = articleCard.getAttribute('carddescription');
+    const cardHref = articleCard.getAttribute('cardhref');
+    const cardLinkText = articleCard.getAttribute('linktext');
+
+    const leftDiv = document.createElement('div');
+    if (h2) {
+      leftDiv.append(h2);
+    }
+    const img = document.createElement('img');
+    img.setAttribute('src', cardImg);
+    if (altText) {
+      img.setAttribute('alt', altText);
+    } else {
+      img.setAttribute('alt', 'Danaher Corporation');
+    }
+    leftDiv.append(img);
+    const rightDiv = document.createElement('div');
+    const h3 = document.createElement('h3');
+    h3.textContent = cardTitle;
+    rightDiv.append(h3);
+    const p = document.createElement('p');
+    p.textContent = cardDescription;
+    const a = document.createElement('a');
+    a.setAttribute('href', cardHref);
+    a.textContent = cardLinkText;
+    p.append(a);
+    rightDiv.append(p);
+    cards.push([leftDiv, rightDiv]);
+  }
+};
+
 const createCards = (main, document) => {
   main.querySelectorAll('fulllayout').forEach((fl) => {
     const cards = [];
-    fl.querySelectorAll('grid[columns="3"] > template ,grid[columns="4"] > template').forEach((tmp) => {
-      const h2 = tmp.content.querySelector('h2');
-      const articleCard = tmp.content.querySelector('articlecard');
-      if (articleCard) {
-        const cardImg = articleCard.getAttribute('cardimg');
-        const altText = articleCard.getAttribute('imagealttext');
-        const cardTitle = articleCard.getAttribute('cardtitle');
-        const cardDescription = articleCard.getAttribute('carddescription');
-        const cardHref = articleCard.getAttribute('cardhref');
-        const cardLinkText = articleCard.getAttribute('linktext');
+    const cells = [];
+    const threeColumn = fl.querySelectorAll('grid[columns="3"] > template');
+    const fourColumn = fl.querySelectorAll('grid[columns="4"] > template');
+    if (threeColumn.length > 0) {
+      threeColumn.forEach((tmp) => {
+        card(tmp, cards, document);
+      });
+      cells.push(['Cards (articlecard) ']);
+    }
 
-        const leftDiv = document.createElement('div');
-        if (h2) {
-          leftDiv.append(h2);
-        }
-        const img = document.createElement('img');
-        img.setAttribute('src', cardImg);
-        if (altText) {
-          img.setAttribute('alt', altText);
-        } else {
-          img.setAttribute('alt', 'Danaher Corporation');
-        }
-        leftDiv.append(img);
-        const rightDiv = document.createElement('div');
-        const h3 = document.createElement('h3');
-        h3.textContent = cardTitle;
-        rightDiv.append(h3);
-        const p = document.createElement('p');
-        p.textContent = cardDescription;
-        const a = document.createElement('a');
-        a.setAttribute('href', cardHref);
-        a.textContent = cardLinkText;
-        p.append(a);
-        rightDiv.append(p);
-        cards.push([leftDiv, rightDiv]);
-      }
-    });
-    const cells = [
-      ['Cards (articlecard)'],
-      ...cards,
-    ];
+    if (fourColumn.length > 0) {
+      fourColumn.forEach((tmp) => {
+        card(tmp, cards, document);
+      });
+      cells.push(['Cards (articlecard-4) ']);
+    }
+
+    cells.push(...cards);
 
     if (cards.length > 0) {
       fl.before(document.createElement('hr'));
