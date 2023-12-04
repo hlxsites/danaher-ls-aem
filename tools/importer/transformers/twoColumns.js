c9b6ebde92a91a77eea03a40f393236cb3ae64d0
rgravitvl <ravitvl83@gmail.com>
2023-12-04 22:26:10 +0530

    Updated converter for column

--- tools/importer/transformers/twoColumns.js
+++ tools/importer/transformers/twoColumns.js
@@ -5,6 +5,10 @@ import {
 const createTwoColumn = (main, document) => {
   main.querySelectorAll('grid[columns="2"]').forEach((item) => {
     const columns = [];
+    const cells = [];
+    if (item?.getAttribute('itemscenter')) cells.push([['Columns (itemscenter)']]);
+    else cells.push([['Columns']]);
+
     const templates = item.querySelectorAll('template');
     [...templates].forEach((template) => {
       if (template.content.children.length > 0) {
@@ -30,19 +34,27 @@ const createTwoColumn = (main, document) => {
             }
           } else if (element.className === 'heading-aem') {
             const heading = template.content.querySelector('div.heading-aem');
-
             if (heading) {
-              row.push(heading.firstElementChild);
+              if (heading.nextElementSibling && [...heading.nextElementSibling.classList].includes('featureimage')) {
+                const text = document.createElement('strong');
+                text.textContent = heading.firstElementChild.textContent;
+                row.push(text);
+              } else row.push(heading.firstElementChild);
             }
           } else if (element.className === 'heading') {
             const heading = template.content.querySelector('div.heading');
-
             if (heading) {
               const headingEL = heading?.querySelector('heading');
-              const hTag = headingEL?.getAttribute('headingtag') ? headingEL?.getAttribute('headingtag') : 'h1';
-              const headEl = document.createElement(hTag);
-              headEl.textContent = headingEL?.getAttribute('heading');
-              row.push(headEl);
+              if (heading.nextElementSibling && [...heading.nextElementSibling.classList].includes('featureimage')) {
+                const text = document.createElement('strong');
+                text.textContent = headingEL?.getAttribute('heading');
+                row.push(text);
+              } else {
+                const hTag = headingEL?.getAttribute('headingtag') ? headingEL?.getAttribute('headingtag') : 'h2';
+                const headEl = document.createElement(hTag);
+                headEl.textContent = headingEL?.getAttribute('heading');
+                row.push(headEl);
+              }
             }
           } else if (element.className === 'script') {
             const featureImage = template.content.querySelector('div.featureimage');
@@ -55,10 +67,7 @@ const createTwoColumn = (main, document) => {
         columns.push(row);
       }
     });
-    const cells = [
-      ['Columns'],
-      [...columns],
-    ];
+    cells.push([...columns]);
 
     if (columns.length > 0) {
       const block = WebImporter.DOMUtils.createTable(cells, document);
