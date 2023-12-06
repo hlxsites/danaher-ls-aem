ce3c9820ac6f95df51fdccc3e0b1b4f219b94210
wz914876 <sharanagouda_patil@pall.com>
2023-12-06 12:25:59 +0530

    Converter update for opco home page for related-articles

--- tools/importer/transformers/relatedArticles.js
+++ tools/importer/transformers/relatedArticles.js
@@ -1,8 +1,11 @@
 /* global WebImporter */
 const createProductCategoryList = (main, document) => {
+  const url = document.querySelector('[property="og:url"]')?.content;
   const placeholder = main.querySelector('div#related-articles');
   if (placeholder) {
-    const block = [['Related Articles'], ['']];
+    let blockName = 'Related Articles';
+    if (url && url.includes('/us/en/products/brands/')) blockName = 'related-articles (opco-home)';
+    const block = [[blockName], ['']];
     const table = WebImporter.DOMUtils.createTable(block, document);
     placeholder.innerHTML = '';
     placeholder.append(table);
