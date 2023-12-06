dc55362db02995d2733d63e8d927227c933fc948
rgravitvl <ravitvl83@gmail.com>
2023-12-06 14:57:53 +0530

    updated product category list for opco home

--- blocks/product-category-list/product-category-list.js
+++ blocks/product-category-list/product-category-list.js
@@ -1,14 +1,14 @@
 import ffetch from '../../scripts/ffetch.js';
 import {
-  ul, li, a, h4, span,
+  ul, li, a, h4, span, div,
 } from '../../scripts/dom-builder.js';
 import { makePublicUrl, imageHelper } from '../../scripts/scripts.js';
-import { toClassName } from '../../scripts/lib-franklin.js';
+import { toClassName, getMetadata } from '../../scripts/lib-franklin.js';
 import createFilters from './filter.js';
 
 const getSelectionFromUrl = (field) => toClassName(new URLSearchParams(window.location.search).get(field)) || '';
 
-function createCard(product, firstCard = false) {
+function createOpcoCard(product, firstCard = false) {
   const cardWrapper = a(
     { href: makePublicUrl(product.path), title: product.title },
     imageHelper(product.image, product.title, firstCard),
@@ -22,7 +22,24 @@ function createCard(product, firstCard = false) {
   );
   const card = li({
     class:
-        'w-52 lg:w-40 h-52 flex flex-col col-span-1 relative mx-auto justify-center transform transition duration-500 border-l-0-5 border-black hover:scale-105 overflow-hidden bg-white max-w-xl pl-6 pr-6 lg:pr-0',
+        'h-52 flex flex-col col-span-1 relative mx-auto justify-center transform transition duration-500 hover:scale-105 overflow-hidden bg-white max-w-xl pl-6 pr-6 lg:pl-0 lg:pr-0',
+  }, cardWrapper);
+  card.querySelector('img').className = 'mb-2 h-24 w-full object-cover';
+  return card;
+}
+
+function createCard(product, firstCard = false) {
+  const cardWrapper = a(
+    { href: makePublicUrl(product.path), title: product.title },
+    imageHelper(product.image, product.title, firstCard),
+    h4({class: '!text-sm !font-normal !break-words !text-danahergray-900 !h-16',},
+      product.title,
+      span({ class: 'text-lg font-semibold text-danaherpurple-500' }, ' →'),
+    ),
+  );
+  const card = li({
+    class:
+        'w-64 lg:w-44 h-52 flex flex-col col-span-1 relative mx-auto justify-center transform transition duration-500 border-l-0-5 border-black hover:scale-105 overflow-hidden bg-white max-w-xl pl-6 pr-6 lg:pr-0',
   }, cardWrapper);
   card.querySelector('img').className = 'mb-2 h-24 w-full object-cover';
   return card;
@@ -30,13 +47,19 @@ function createCard(product, firstCard = false) {
 
 export default async function decorate(block) {
   block.parentElement.parentElement.classList.add('!pb-0');
-
+  let isbrandPage = false;
+  isbrandPage = (block.classList.contains('opco-home'));
+  const metaBrand = getMetadata('brand');
   let products = await ffetch('/us/en/products-index.json')
     .filter(({ fullCategory }) => fullCategory.split('|').length === 1)
     .filter(({ fullCategory }) => fullCategory !== '')
     .filter(({ path }) => !path.includes('/topics'))
     .filter(({ path }) => !path.includes('/product-coveo'))
     .filter(({ path }) => !path.includes('/brands'))
+    .filter(({ brand }) => {
+      if(isbrandPage) return brand.includes(metaBrand);
+      else return true;
+    })
     .all();
 
   const activeTagFilter = getSelectionFromUrl('tag');
@@ -52,11 +75,11 @@ export default async function decorate(block) {
     class:
           'container grid max-w-7xl w-full mx-auto gap-6 grid-cols-1 sm:grid-cols-3 lg:grid-cols-6 px-4 py-4 sm:px-0 justify-items-center mt-3 mb-3',
   });
-
+  
   filteredProducts.forEach((product, index) => {
-    cardList.append(createCard(product, index === 0));
+    cardList.append( isbrandPage ? createOpcoCard(product, index === 0) : createCard(product, index === 0));
   });
-  const filterTags = createFilters(products, activeTagFilter, 'brand');
+  const filterTags = isbrandPage ? '' : createFilters(products, activeTagFilter, 'brand');
   block.textContent = '';
   block.append(filterTags, cardList);
 }
