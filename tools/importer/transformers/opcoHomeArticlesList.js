c66cb686f5ad95ffbfbdcc3cc05e7c5d09bd4b8b
wz914876 <sharanagouda_patil@pall.com>
2023-12-06 18:50:50 +0530

    lint error fixes

--- tools/importer/transformers/opcoHomeArticlesList.js
+++ tools/importer/transformers/opcoHomeArticlesList.js
@@ -1,13 +1,12 @@
 /* global WebImporter */
 const createOpcoHomeArticlesList = (main, document) => {
-  const url = document.querySelector('[property="og:url"]')?.content;
   const placeholder = main.querySelector('div#articles-opco-home');
   if (placeholder) {
-    let blockName = 'Articles list (opco-home)';    
+    const blockName = 'Articles list (opco-home)';
     const block = [[blockName], ['']];
     const table = WebImporter.DOMUtils.createTable(block, document);
     placeholder.innerHTML = '';
     placeholder.append(table);
   }
 };
-export default createOpcoHomeArticlesList; 
+export default createOpcoHomeArticlesList;
