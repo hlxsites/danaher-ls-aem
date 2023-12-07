a844ae57276b9180168addb6212acad58f54937b
davenichols-DHLS <124819426+davenichols-DHLS@users.noreply.github.com>
2023-12-07 11:10:31 +0000

    Update carousel.js

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,7 +1,7 @@
 import { button, div, span } from '../../scripts/dom-builder.js';
 
 const SLIDE_DELAY = 5000;
-const SLIDE_TRANSITION = 50000;
+const SLIDE_TRANSITION = 500;
 
 function previousSlide(evt) {
   const element = evt.currentTarget.element;
