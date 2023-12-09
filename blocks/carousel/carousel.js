2f13a0a32165a3ab1a861237cd8a1818cf2fdbac
Amlan Sengupta <amlans3@gmail.com>
2023-12-10 01:12:35 +0530

    Added custom Carousel library

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,106 +1,12 @@
 import { button, div, span } from '../../scripts/dom-builder.js';
+import Carousel from '../../scripts/Carousel.js';
 
-const SLIDE_DELAY = 2000;
+const SLIDE_DELAY = 3000;
 const SLIDE_TRANSITION = 500;
-
-function previousSlide(evt) {
-  const element = evt.currentTarget.element;
-  const sliders = element.querySelectorAll('div[data-carousel-item]');
-  let counter = 0;
-  for (let index = sliders.length - 1; index >= 0; index -= 1) {
-    const isActive = ['active'].includes(sliders[index].getAttribute('data-carousel-item'));
-
-    sliders[index].classList.remove('hidden');
-    if (isActive && !sliders[index].previousElementSibling) {
-      document.querySelector('.carousel-paginate').innerHTML = `${itemIndex+1}/${sliders.length}`;
-      // SET LAST SLIDE ACTIVE IF THERE IS NO PREVIOUS SIBLING ( WORKS FOR FIRST INDEX POSITION SLIDE ONLY )
-      sliders[sliders.length-1].setAttribute('data-carousel-item', 'active');
-      sliders[sliders.length-1].classList.replace('-translate-x-full', 'translate-x-0');
-    }
-    if (!isActive) {
-      if (counter === 1) {
-        document.querySelector('.carousel-paginate').innerHTML = `${itemIndex+1}/${sliders.length}`;
-        // SET THE CURRENT SLIDE ACTIVE
-        sliders[index].setAttribute('data-carousel-item', 'active');
-        sliders[index].classList.replace('-translate-x-full', 'translate-x-0');
-      } else if (sliders[index].nextElementSibling && ['active'].includes(sliders[index].nextElementSibling.getAttribute('data-carousel-item'))) {
-
-        sliders[index].classList.replace('translate-x-full', '-translate-x-full');
-      }
-      counter = 0;
-    } else {
-      // MAKE CHANGES FOR THE CURRENT SLIDE ( TO NULLIFY ITS ACTIVE STATUS )
-      sliders[index].setAttribute('data-carousel-item', '');
-      if (sliders[index].previousElementSibling) {
-        sliders[index].classList.replace('translate-x-0', 'translate-x-full');
-        if (sliders[index].nextElementSibling) {
-          sliders[index].nextElementSibling.classList.add('hidden');
-          sliders[index].nextElementSibling.classList.replace('translate-x-full', '-translate-x-full');
-        }
-        else sliders[0].classList.replace('translate-x-full', '-translate-x-full');
-      }
-      else {
-        // SET FIRST-INDEX-POSITION-SLIDE TO NEGATIVE POSITION IF THERE IS NO PREVIOUS SIBLING ( WORKS FOR FIRST-INDEX-POSITION-SLIDE ONLY )
-        sliders[index].classList.replace('translate-x-0', 'translate-x-full');
-      }
-      counter = 1;
-    }
-  }
-}
-
-function nextSlide(evt) {
-  const element = evt.currentTarget.element;
-  const sliders = element.querySelectorAll('div[data-carousel-item]');
-  // console.log(allSlides);
-  let counter = 0;
-  sliders.forEach((item, itemIndex) => {
-    item.classList.remove('hidden');
-    if (['active'].includes(item.getAttribute('data-carousel-item')) && !item.nextElementSibling) {
-      document.querySelector('.carousel-paginate').innerHTML = `${itemIndex+1}/${sliders.length}`;
-      sliders[0].setAttribute('data-carousel-item', 'active');
-      sliders[0].classList.replace('-translate-x-full', 'translate-x-0');
-    }
-    if (item.getAttribute('data-carousel-item') !== 'active') {
-      if (counter === 1) {
-        document.querySelector('.carousel-paginate').innerHTML = `${itemIndex+1}/${sliders.length}`;
-        item.setAttribute('data-carousel-item', 'active');
-        item.classList.replace('translate-x-full', 'translate-x-0');
-        item.classList.replace('-translate-x-full', 'translate-x-0');
-      } else if (item.previousElementSibling && ['active'].includes(item.previousElementSibling.getAttribute('data-carousel-item'))) {
-        item.classList.replace('-translate-x-full', 'translate-x-full');
-      }
-      counter = 0;
-    } else {
-      item.setAttribute('data-carousel-item', '');
-      if (item.nextElementSibling) item.classList.replace('translate-x-0', '-translate-x-full');
-      else item.classList.replace('translate-x-0', 'translate-x-full');
-      counter = 1;
-    }
-    if (element.querySelector('.translate-x-full') && element.querySelector('.translate-x-full').nextElementSibling) {
-      element.querySelector('.translate-x-full').nextElementSibling.classList.add('hidden');
-      if (element.querySelector('.translate-x-full').nextElementSibling.nextElementSibling) element.querySelector('.translate-x-full').nextElementSibling.nextElementSibling.classList.add('hidden');
-    }
-  });
-}
-
-function serializeSliders(element) {
-  const sliders = element.querySelectorAll('.carousel-slider');
-  sliders.forEach((item, index) => {
-    if (index === 0) {
-      item.setAttribute('data-carousel-item', 'active');
-      item.classList.add('translate-x-0');
-    } else {
-      item.setAttribute('data-carousel-item', '');
-      if (item.previousElementSibling.getAttribute('data-carousel-item') === 'active') item.classList.add('translate-x-full');
-      else item.classList.add('-translate-x-full');
-    }
-  });
-}
 
 function configureNavigation(element) {
   const elementControls = element.querySelector('.carousel-controls');
   const previousBtn = button({ type: 'button', class: 'flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none', 'data-carousel-prev': '' });
-  previousBtn.addEventListener('click', previousSlide, false);
   previousBtn.element = element;
   previousBtn.innerHTML = `
     <span
@@ -112,8 +18,7 @@ function configureNavigation(element) {
       <span class="sr-only">Previous</span>
     </span>
   `;
-  const nextBtn = button({ type: 'button', class: 'flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none', 'data-carousel-prev': '' });
-  nextBtn.addEventListener('click', nextSlide, false);
+  const nextBtn = button({ type: 'button', class: 'flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none', 'data-carousel-next': '' });
   nextBtn.element = element;
   nextBtn.innerHTML = `
     <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-danaherpurple-50 group-hover:danaherpurple-25">
@@ -125,6 +30,7 @@ function configureNavigation(element) {
   `;
   elementControls.prepend(previousBtn);
   elementControls.append(nextBtn);
+  return elementControls;
 }
 
 function configurePagination(element) {
@@ -133,59 +39,17 @@ function configurePagination(element) {
   element.append(paginateWrapper);
 }
 
-function carouselControls(element) {
-  const sliders = element.querySelectorAll('.carousel-slider');
-  setInterval(() => {
-    let counter = 0;
-    sliders.forEach((item, itemIndex) => {
-      const isActive = ['active'].includes(item.getAttribute('data-carousel-item'));
-      item.classList.remove('hidden');
-      if (isActive && !item.nextElementSibling) {
-        document.querySelector('.carousel-paginate').innerHTML = `${itemIndex+1}/${sliders.length}`;
-        sliders[0].setAttribute('data-carousel-item', 'active');
-        sliders[0].classList.replace('-translate-x-full', 'translate-x-0');
-      }
-      if (!isActive) {
-        if (counter === 1) {
-          item.setAttribute('data-carousel-item', 'active');
-          document.querySelector('.carousel-paginate').innerHTML = `${itemIndex+1}/${sliders.length}`;
-          item.classList.replace('translate-x-full', 'translate-x-0');
-          item.classList.replace('-translate-x-full', 'translate-x-0');
-        } else if (item.previousElementSibling && ['active'].includes(item.previousElementSibling.getAttribute('data-carousel-item'))) {
-          item.classList.replace('-translate-x-full', 'translate-x-full');
-        }
-        counter = 0;
-      } else {
-        item.setAttribute('data-carousel-item', '');
-        if (item.nextElementSibling) item.classList.replace('translate-x-0', '-translate-x-full');
-        else item.classList.replace('translate-x-0', 'translate-x-full');
-        counter = 1;
-      }
-      if (element.querySelector('.translate-x-full') && element.querySelector('.translate-x-full').nextElementSibling) {
-        element.querySelector('.translate-x-full').nextElementSibling.classList.add('hidden');
-        if (element.querySelector('.translate-x-full').nextElementSibling.nextElementSibling) element.querySelector('.translate-x-full').nextElementSibling.nextElementSibling.classList.add('hidden');
-      }
-    });
-  }, SLIDE_DELAY);
-}
-
 export default function decorate(block) {
-  // console.log(block);
   const uuid = crypto.randomUUID(4).substring(0, 6);
   block.classList.add(...'relative h-56 md:h-[40rem] overflow-hidden'.split(' '));
-  const groupElements = [...block.children].reduce((prev, curr, index) => {
-    // console.log(prev, curr, index);
-    // if (prev.length && (index % 2) === 0) prev[prev.length - 1].push(curr);
-    // else prev.push([curr]);
-    // return prev;
+  const groupElements = [...block.children].reduce((prev, curr) => {
     prev.push([...curr.children]);
     return prev;
   }, []);
-  // console.log(groupElements);
   block.innerHTML = '';
-  groupElements.map((ele, eleIndex) => {
+  const slides = groupElements.map((ele, eleIndex) => {
     if (ele.length > 1) {
-      const carouselSlider = div({ class: `${eleIndex} carousel-slider duration-${SLIDE_TRANSITION} ease-in-out absolute inset-0 transition-transform z-10`, 'data-carousel-item': '' });
+      const carouselSlider = div({ class: `${eleIndex} carousel-slider duration-${SLIDE_TRANSITION} ease-in-out absolute inset-0 transition-transform transform z-10`, 'data-carousel-item': '' });
       ele.map((el, index) => {
         if (index === 0) {
           el.classList.add(...'px-4 sm:px-10 lg:w-1/2 xl:pr-10 space-y-6 pb-20 pt-8 md:pt-16 lg:py-20'.split(' '));
@@ -204,20 +68,32 @@ export default function decorate(block) {
         }
         return index;
       });
-      // console.log(carouselSlider);
       block.append(carouselSlider);
-      return carouselSlider;
+      return { position: parseInt(eleIndex, 10), el: carouselSlider };
     }
     return null;
   }).filter((item) => item);
   if (block.parentElement.className.includes('carousel-wrapper')) {
-    block.parentElement.classList.add(...'relative w-full hidden'.split(' '));
+    let controls;
+    block.parentElement.classList.add(...'relative w-full'.split(' '));
     block.parentElement.setAttribute('data-carousel', 'slide');
     block.parentElement.setAttribute('id', uuid);
-    serializeSliders(block.parentElement, { navigation: true, pagination: true });
-    configurePagination(block.parentElement);
-    configureNavigation(block.parentElement);
-    block.parentElement.classList.remove('hidden');
-    carouselControls(block.parentElement);
+    if (block.children.length > 1) {
+      configurePagination(block.parentElement);
+      controls = configureNavigation(block.parentElement);
+    }
+    const options = {
+      defaultPosition: 0,
+      interval: SLIDE_DELAY,
+      onChange: (elIndex) => {
+        block.parentElement.querySelector('.carousel-paginate').innerHTML = `${elIndex + 1}/${slides.length}`;
+      },
+    };
+    const carousel = new Carousel(block, slides, options);
+    if (block.children.length > 1) {
+      controls.querySelector('button[data-carousel-prev]').addEventListener('click', carousel.prev);
+      controls.querySelector('button[data-carousel-next]').addEventListener('click', carousel.next);
+      carousel.loop();
+    }
   }
 }
