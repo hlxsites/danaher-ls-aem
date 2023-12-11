d4d9a2db27be6e306a85275f8eb298a32107578a
rgravitvl <ravitvl83@gmail.com>
2023-12-11 19:47:15 +0530

    fixed lint issues

--- tools/importer/transformers/carousel.js
+++ tools/importer/transformers/carousel.js
@@ -60,8 +60,8 @@ const createCarousel = (main, document) => {
       getCarousel(carousel, cells, document);
       const block = WebImporter.DOMUtils.createTable(cells, document);
       carousel.append(block);
-      if (i === carousels.length-1 && !carousel.parentNode.nextElementSibling) carousel.prepend(document.createElement('hr'));
-      else carousel.append(document.createElement('hr'))
+      if (i === carousels.length - 1 && !carousel.parentNode.nextElementSibling) carousel.prepend(document.createElement('hr'));
+      else carousel.append(document.createElement('hr'));
     });
   }
 };
