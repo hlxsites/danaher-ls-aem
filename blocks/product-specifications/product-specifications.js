130fc3ee7aed85c91c9de2dab5324b5f9a238e7a
wz914876 <sharanagouda_patil@pall.com>
2024-01-09 15:03:01 +0530

    product specification attributes changes

--- blocks/product-specifications/product-specifications.js
+++ blocks/product-specifications/product-specifications.js
@@ -1,23 +1,50 @@
 import { div, h1 } from '../../scripts/dom-builder.js';
 
 export default function decorate(block) {
-    let response;    
-    if (localStorage.getItem('product-hero')) response = JSON.parse(localStorage.getItem('product-hero'));
-    if (response) {        
-        const main = document.querySelector('main');
-        const productSpec = main.querySelector('.product-specifications');
-        console.log(productSpec);
-        //productSpec.append(h1({ class: 'text-xl font-normal leading-6 text-black' }, productSpec));
-        let defaultContent = div();
-        defaultContent.innerHTML = response[0]?.raw.specificationsjson;
-        console.log(defaultContent);
-        // defaultContent.append(
-        //     div(
-        //         p(response[0]?.raw.specificationsjson),
-        //     ),
-        // );
-        // productSpec.parentElement.classList.add(...'stretch'.split(' '));
-        // productSpec.innerHTML = '';
-        // productSpec.append(div({ class: 'hero-default-content' }, defaultContent));
+  let response;
+  if (localStorage.getItem('product-hero')) {
+    response = JSON.parse(localStorage.getItem('product-hero'));
+    const attrJson = JSON.parse(response[0]?.raw.attributejson);
+    const attrContainer = div({ class: 'block' });
+    if (attrJson.length > 0) {
+      attrJson.forEach((item) => {
+        const attrWrapper = div({ class: 'attr-wrapper' });
+        const tableCaption = div(
+          { class: 'sm:flex sm:items-center pt-12' },
+          div(
+            { class: 'sm:flex-auto' },
+            h1({ class: 'text-xl font-normal leading-6 text-black' }, item.label),
+          ),
+        );
+        const tableContainer = div({ class: 'min-w-full mt-2 border divide-y divide-gray-300 rounded-lg' });
+        if (item.value.length) {
+          item.value.forEach((items) => {
+            const tableRow = div(
+              { class: 'flex flex-row flex-wrap h-full min-w-full align-middle' },
+              div(
+                { class: 'flex w-full p-4 text-sm font-medium text-gray-900 bg-gray-100 md:w-1/3' },
+                div(
+                  { class: 'my-auto' },
+                  items.label,
+                ),
+              ),
+              div(
+                { class: 'flex w-full p-4 text-sm text-gray-700 break-words md:w-2/3' },
+                div(
+                  { class: 'my-auto' },
+                  items.value,
+                ),
+              ),
+            );
+            tableContainer.append(tableRow);
+          });
+        }
+        attrWrapper.append(tableCaption);
+        attrWrapper.append(tableContainer);
+        attrContainer.append(attrWrapper);
+      });
     }
+    block.innerHTML = '';
+    block.append(attrContainer);
+  }
 }
