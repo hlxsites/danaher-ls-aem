b07a007f757f0faa0b9c3c45c8d09ba25077e9d8
wz914876 <sharanagouda_patil@pall.com>
2024-01-15 19:49:50 +0530

    review comments changes

--- blocks/product-citations/product-citations.js
+++ blocks/product-citations/product-citations.js
@@ -4,10 +4,6 @@ import {
 import { loadScript } from '../../scripts/lib-franklin.js';
 
 export default async function decorate(block) {
-  const attrs = { defer: true };
-  await loadScript('https://cdn.bioz.com/assets/jquery-2.2.4.js', attrs);
-  await loadScript('https://cdn.bioz.com/assets/bioz-w-api-6.0.min.js', attrs);
-
   const citationsLinkEl = block.querySelector('a');
   if (citationsLinkEl) {
     const id = citationsLinkEl.getAttribute('title');
@@ -19,7 +15,11 @@ export default async function decorate(block) {
     block.append(div(
       { id, class: 'text-xs text-danaherblue-900' },
       img({ src: '/icons/bioz-favicon.png', class: 'w-3 h-3 align-top pb-0 ml-0 mb-0 float-none' }),
-      a({ href: 'https://www.bioz.com/', target: '_blank' }, 'Powered by Bioz See more details on Bioz © 2024'),
+      a({ href: 'https://www.bioz.com/', target: '_blank', title: 'link' }, 'Powered by Bioz See more details on Bioz © 2024'),
     ));
+
+    const attrs = { defer: true };
+    await loadScript('https://cdn.bioz.com/assets/jquery-2.2.4.js', attrs);
+    await loadScript('https://cdn.bioz.com/assets/bioz-w-api-6.0.min.js', attrs);
   }
 }
