13fa2a1876498b583e0518dccfcbba505815a917
wz914876 <sharanagouda_patil@pall.com>
2024-01-15 16:41:03 +0530

    used dom-builder to construct block

--- blocks/product-citations/product-citations.js
+++ blocks/product-citations/product-citations.js
@@ -1,31 +1,25 @@
-import { object } from '../../scripts/dom-builder.js';
+import {
+  a, div, img, object,
+} from '../../scripts/dom-builder.js';
 import { loadScript } from '../../scripts/lib-franklin.js';
-
-function buildCitations(id, data) {
-  const citations = object({
-    id,
-    type: 'text/html',
-    data,
-    style: 'width:100%; height: 193px',
-  });
-  return citations;
-}
 
 export default async function decorate(block) {
   const attrs = { defer: true };
   await loadScript('https://cdn.bioz.com/assets/jquery-2.2.4.js', attrs);
   await loadScript('https://cdn.bioz.com/assets/bioz-w-api-6.0.min.js', attrs);
 
-  const biozDiv = `<div id="bioz-w-pb-3230-bzen-q" href="https://www.bioz.com/" target="_blank" style="font-size: 12px; text-decoration: none; color: rgb(10, 67, 133);">
-                  <img src="https://cdn.bioz.com/assets/favicon.png" 
-                       style="width: 11px; height: 11px; vertical-align: baseline; padding-bottom: 0px; margin-left: 0px; margin-bottom: 0px; float: none;">
-                  Powered by Bioz See more details on Bioz © 2024</div>`;
-
   const citationsLinkEl = block.querySelector('a');
   if (citationsLinkEl) {
     const id = citationsLinkEl.getAttribute('title');
     const data = citationsLinkEl.getAttribute('href');
-    block.innerHTML = biozDiv;
-    block.prepend(buildCitations(id, data));
+    block.innerHTML = '';
+    block.append(object({
+      id, type: 'text/html', data, class: 'w-full h-48',
+    }));
+    block.append(div(
+      { id, class: 'text-xs text-danaherblue-900' },
+      img({ src: '/icons/bioz-favicon.png', class: 'w-3 h-3 align-top pb-0 ml-0 mb-0 float-none' }),
+      a({ href: 'https://www.bioz.com/', target: '_blank' }, 'Powered by Bioz See more details on Bioz © 2024'),
+    ));
   }
 }
