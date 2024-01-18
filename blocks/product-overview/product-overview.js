d894573107cd687f98cb1e3d3db9a69bde45943e
rgravitvl <ravitvl83@gmail.com>
2024-01-18 10:35:29 +0530

    added the check

--- blocks/product-overview/product-overview.js
+++ blocks/product-overview/product-overview.js
@@ -4,6 +4,6 @@ export default async function decorate(block) {
   const response = getProductResponse();
   block.classList.add(...'text-lg leading-7 overview xl:w-3/4 href-text v-html text-danahergray-700'.split(' '));
   if (response?.length > 0) {
-    block.innerHTML = response[0].raw ? response[0].raw.richlongdescription : '';
+    block.innerHTML = response[0].raw?.richlongdescription ? response[0].raw.richlongdescription : '';
   }
 }
