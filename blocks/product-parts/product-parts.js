654b39e9af45e932144188f39afc1e052b83591d
rgravitvl <ravitvl83@gmail.com>
2024-01-22 11:05:34 +0530

    removed unwanted check

--- blocks/product-parts/product-parts.js
+++ blocks/product-parts/product-parts.js
@@ -23,30 +23,28 @@ export default async function decorate(block) {
         ),
       ));
       const bundleDetails = JSON.parse(response[0].raw?.bundlepreviewjson);
-      if (bundleDetails?.length > 0) {
-        bundleDetails.forEach((product) => {
-          block.append(div(
-            { class: 'grid grid-cols-12 text-sm text-black gap-y-4 border-b' },
+      bundleDetails.forEach((product) => {
+        block.append(div(
+          { class: 'grid grid-cols-12 text-sm text-black gap-y-4 border-b' },
+          div(
+            { class: 'flex flex-row col-span-8 lg:col-span-10 pt-4 pb-4' },
+            img({ src: `${product.image}`, alt: `${product.title}`, class: 'w-16 h-16 rounded-md shadow-lg' }),
             div(
-              { class: 'flex flex-row col-span-8 lg:col-span-10 pt-4 pb-4' },
-              img({ src: `${product.image}`, alt: `${product.title}`, class: 'w-16 h-16 rounded-md shadow-lg' }),
-              div(
-                { class: 'flex flex-col items-start pl-4' },
-                p(`${product.title}`),
-                p({ class: 'text-xs' }, `${product.sku}`),
-              ),
+              { class: 'flex flex-col items-start pl-4' },
+              p(`${product.title}`),
+              p({ class: 'text-xs' }, `${product.sku}`),
             ),
-            div(
-              { class: 'col-span-4 md:col-span-2 lg:col-span-1 flex justify-center pt-4 pb-4' },
-              p({ class: '!content-center' }, `${product.quantity ? product.quantity : 1}`),
-            ),
-            div(
-              { class: 'hidden md:flex justify-center col-span-2 lg:col-span-1 pt-4 pb-4' },
-              p('--'),
-            ),
-          ));
-        });
-      }
+          ),
+          div(
+            { class: 'col-span-4 md:col-span-2 lg:col-span-1 flex justify-center pt-4 pb-4' },
+            p({ class: '!content-center' }, `${product.quantity ? product.quantity : 1}`),
+          ),
+          div(
+            { class: 'hidden md:flex justify-center col-span-2 lg:col-span-1 pt-4 pb-4' },
+            p('--'),
+          ),
+        ));
+      });
     } catch (e) {
       // eslint-disable-next-line no-console
       console.error(e);
