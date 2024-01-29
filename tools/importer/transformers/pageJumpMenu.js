d857657e479b62f53bef5874377441b70f9a99a0
rgravitvl <ravitvl83@gmail.com>
2024-01-29 21:48:58 +0530

    updated for page jump menu

---
+++ tools/importer/transformers/pageJumpMenu.js
@@ -0,0 +1,23 @@
+/* global WebImporter */
+const createPageJumpMenu = (main, document) => {
+  const jumpMenu = main.querySelector('page-jump-menu');
+  const menus = [];
+  if (jumpMenu) {
+    // eslint-disable-next-line no-undef
+    const menuList = JSON.parse(decodeHtmlEntities(jumpMenu.getAttribute('list')));
+    menuList.forEach((element) => {
+      const urlEl = document.createElement('a');
+      urlEl.href = element.linkUrl;
+      urlEl.textContent = element.linkName;
+      const imgEl = document.createElement('img');
+      imgEl.src = element.icon;
+      menus.push([imgEl, urlEl]);
+    });
+    const cells = [['Page Jump Menu'], ...menus];
+    if (menus.length > 0) {
+      const block = WebImporter.DOMUtils.createTable(cells, document);
+      jumpMenu.append(block);
+    }
+  }
+};
+export default createPageJumpMenu;
