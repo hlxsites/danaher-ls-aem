dba5dd39837c0cd2a401c00d641077ca3f25472f
Dave Nichols <dave_nichols@europe.pall.com>
2024-01-31 18:18:32 +0000

    Banner POC for css loading

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,3 +1,7 @@
+import { loadCSS } from '../../scripts/lib-franklin.js';
+
+loadCSS('/blocks/banner/banner.css');
+
 export default function decorate(block) {
   const main = document.querySelector('main');
   main.parentNode.insertBefore(block, main);
