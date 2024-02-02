653ed12d12eb08f8435edb326505a7a74bfb4473
wz914876 <sharanagouda_patil@pall.com>
2024-02-02 16:05:23 +0530

    Remove block css from banner

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,4 +1,12 @@
 export default function decorate(block) {
   const main = document.querySelector('main');
+  const content = block.querySelector('div');
+  content.parentNode.setAttribute('style', 'background: linear-gradient(to right bottom, #430f9f, #5d12b5, #7814cc, #9414e2, #b110f7)');
+  content.parentNode.classList.add(...'px-6'.split(' '));
+  content.classList.add(...'relative min-h-[13rem] h-max w-full flex justify-start items-center'.split(' '));
+  const innerContent = content.querySelector('div');
+  innerContent.classList.add(...'relative max-w-7xl mx-auto w-full py-4 text-white'.split(' '));
+  const contentH1 = innerContent.querySelector('h1');
+  contentH1.classList.add(...'text-4xl font-extrabold tracking-tight text-white'.split(' '));
   main.parentNode.insertBefore(block, main);
 }
