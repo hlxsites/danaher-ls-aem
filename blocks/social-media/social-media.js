10050815833fb7150cae193e71bf2152b6ca0059
wz914876 <sharanagouda_patil@pall.com>
2024-02-06 19:48:58 +0530

    minor changes

--- blocks/social-media/social-media.js
+++ blocks/social-media/social-media.js
@@ -25,18 +25,9 @@ export default function decorate(block) {
   block.classList.add(...'relative z-10 flex items-center justify-between mb-4 text-gray-600 pt-6 pb-2'.split(' '));
   const goParentBack = button({ 'aria-label': 'back', class: 'back-btn', title: 'Back' }, span({ class: 'icon icon-back' }), span({ class: 'my-auto' }, 'Back'));
   goParentBack.addEventListener('click', goBack);
-  const childDivs = block.childNodes;
-  childDivs.forEach((divs, index) => {
-    if (index === 1) divs.remove();
-  });
   if (goParentBack.className.includes('back-btn')) {
     goParentBack.classList.add(...'font-normal inline-flex items-center gap-4 transition leading-6 py-1 px-1.5 rounded-lg hover:bg-slate-900/[0.03]'.split(' '));
   }
-  block.querySelectorAll('div.social-links').forEach((item) => {
-    item?.classList?.add(...'flex items-center gap-3'.split(' '));
-    const ancs = item?.querySelector('a');
-    ancs?.classList?.add(...'font-normal inline-flex items-center gap-4 transition leading-6 py-1 px-1.5 rounded-lg hover:bg-slate-900/[0.03]'.split(' '));
-  });
   block.innerHTML = social;
   block.prepend(goParentBack);
   decorateIcons(block);
