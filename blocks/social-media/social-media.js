4eabcd7bf021525b0d1a18c4d336f51dce43ad88
wz914876 <sharanagouda_patil@pall.com>
2024-02-06 19:30:00 +0530

    icons missing

--- blocks/social-media/social-media.js
+++ blocks/social-media/social-media.js
@@ -25,7 +25,6 @@ export default function decorate(block) {
   block.classList.add(...'relative z-10 flex items-center justify-between mb-4 text-gray-600 pt-6 pb-2'.split(' '));
   const goParentBack = button({ 'aria-label': 'back', class: 'back-btn', title: 'Back' }, span({ class: 'icon icon-back' }), span({ class: 'my-auto' }, 'Back'));
   goParentBack.addEventListener('click', goBack);
-  block.innerHTML = social;
   const childDivs = block.childNodes;
   childDivs.forEach((divs, index) => {
     if (index === 1) divs.remove();
@@ -38,6 +37,7 @@ export default function decorate(block) {
     const ancs = item?.querySelector('a');
     ancs?.classList?.add(...'font-normal inline-flex items-center gap-4 transition leading-6 py-1 px-1.5 rounded-lg hover:bg-slate-900/[0.03]'.split(' '));
   });
+  block.innerHTML = social;
   block.prepend(goParentBack);
   decorateIcons(block);
 }
