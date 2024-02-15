55af7acc9d2e1af6649f88c4f41541bfd9987a52
wz914876 <sharanagouda_patil@pall.com>
2024-02-15 17:08:16 +0530

    797 [Solutions] Converter updates for MarketoForm

--- tools/importer/transformers/embedScript.js
+++ tools/importer/transformers/embedScript.js
@@ -1,21 +1,40 @@
 /* global WebImporter */
 const embedScript = (main, document) => {
-  const scriptEl = main.querySelector('div.script');
-  if (scriptEl) {
-    const iFrame = scriptEl.querySelector('iframe');
-    if (iFrame) {
-      const anc = document.createElement('a');
-      anc.textContent = iFrame.getAttribute('title');
-      anc.href = iFrame.getAttribute('src');
+  const scriptEl = main.querySelectorAll('div.script');
+  if (scriptEl.length > 0) {
+    scriptEl.forEach((scrpt) => {
+      const iFrame = scrpt.querySelector('iframe');
+      if (iFrame) {
+        const anc = document.createElement('a');
+        anc.textContent = iFrame.getAttribute('title');
+        anc.href = iFrame.getAttribute('src');
 
-      const cells = [
-        ['embed'],
-        [anc],
-      ];
-      const block = WebImporter.DOMUtils.createTable(cells, document);
-      scriptEl.innerHTML = '';
-      scriptEl.append(block);
-    }
+        const cells = [['embed'], [anc]];
+        const block = WebImporter.DOMUtils.createTable(cells, document);
+        scrpt.innerHTML = '';
+        scrpt.append(block);
+      }
+
+      const forms = [];
+      const mktoForms = scrpt.querySelectorAll('form');
+      if (mktoForms.length > 0) {
+        mktoForms.forEach((form, index) => {
+          const divFormEl = document.createElement('div');
+          const divFormThankYouEl = document.createElement('div');
+          if (index === 0) {
+            divFormEl.textContent = form?.getAttribute('id');
+            forms.push([['Marketo'], divFormEl]);
+          } else {
+            divFormThankYouEl.textContent = form?.getAttribute('id');
+            forms.push([['Thank you'], divFormThankYouEl]);
+          }
+        });
+        const cells = [['Marketo Form'], ...forms];
+        const block = WebImporter.DOMUtils.createTable(cells, document);
+        scrpt.innerHTML = '';
+        scrpt.append(block);
+      }
+    });
   }
 };
 export default embedScript;
