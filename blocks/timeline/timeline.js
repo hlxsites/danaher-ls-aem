593691e51a10ded89aed8e2d5687312faf31768a
rgravitvl <ravitvl83@gmail.com>
2024-02-20 21:04:31 +0530

    updated for mobile view

--- blocks/timeline/timeline.js
+++ blocks/timeline/timeline.js
@@ -1,23 +1,19 @@
 import { div } from '../../scripts/dom-builder.js';
 
-function isMobileDevice() {
-  return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-}
-
-function decoratePicture(picture) {
+function decoratePicture(picture, timeline) {
   picture.classList.add(...'w-full mx-auto'.split(' '));
-  const pictureDiv = div({ class: 'timeline-img hidden lg:block w-[47%] right relative bottom-4 px-4 py-8' }, picture);
-  return pictureDiv;
+  const pictureDiv = div({ class: 'timeline-img float-right hidden lg:block w-[47%] relative bottom-4 px-4 py-8' }, picture);
+  timeline.append(pictureDiv);
 }
 
-function decorateItemNo(item) {
+function decorateItemNo(item, timeline) {
   const itemNo = item.querySelector('div:first-child');
   itemNo.classList.add(...'md:w-10 h-10 m-0 bg-danaherblue-600 text-white rounded flex justify-center items-center'.split(' '));
   const itemNoDiv = div({ class: '!w-[15%] lg:!w-[6%] relative py-4' }, itemNo);
-  return itemNoDiv;
+  timeline.append(itemNoDiv);
 }
 
-function decorateContent(item) {
+function decorateContent(item, timeline) {
   const content = item.querySelector('div:last-child');
   content.classList.add(...'flex flex-col'.split(' '));
   content.querySelector('p > em').classList.add(...'text-xs text-gray-400 tracking-wider pb-2 not-italic'.split(' '));
@@ -25,8 +21,8 @@ function decorateContent(item) {
   content.querySelector('p:nth-child(3)').classList.add('text-danahergray-700');
   content.querySelector('p:nth-child(4)').classList.add(...'flex flex-col md:flex-row gap-3 mt-4'.split(' '));
   content.querySelector('p:nth-child(4) > a').classList.add('!rounded-full');
-  const itemContent = div({ class: 'w-[85%] lg:!w-[47%] right relative bottom-4 px-4 py-8' }, content);
-  return itemContent;
+  const itemContent = div({ class: 'w-[85%] lg:!w-[47%] relative bottom-4 px-4 py-8' }, content);
+  timeline.append(itemContent);
 }
 
 export default function decorate(block) {
@@ -35,12 +31,12 @@ export default function decorate(block) {
   [...items].forEach((item, idx) => {
     item.classList.add(...'flex flex-row items-stretch px-2 w-full'.split(' '));
     const picture = item.querySelector('div:last-child > p > picture');
-    if (isMobileDevice()) {
-      item.prepend(decoratePicture(picture), decorateItemNo(item), decorateContent(item));
-    } else if (idx % 2 === 0) {
-      item.prepend(decoratePicture(picture), decorateItemNo(item), decorateContent(item));
-    } else {
-      item.append(decorateContent(item), decorateItemNo(item), decoratePicture(picture));
-    }
+    const timeline = (idx % 2 === 0)
+      ? div({ class: 'flex flex-row items-stretch px-2 w-full' })
+      : div({ class: 'flex flex-row lg:flex-row-reverse items-stretch px-2 w-full' });
+    decoratePicture(picture, timeline);
+    decorateItemNo(item, timeline);
+    decorateContent(item, timeline);
+    item.replaceWith(timeline);
   });
 }
