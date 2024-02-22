7e7dd2f7e17bf92b541e00b6b99d23e9641c9e5a
rgravitvl <ravitvl83@gmail.com>
2024-02-22 15:55:09 +0530

    Fixed product not found page

--- scripts/commerce.js
+++ scripts/commerce.js
@@ -98,7 +98,7 @@ export async function getProductResponse() {
 
     if (!response) {
       localStorage.removeItem('product-details');
-      await fetch(makePublicUrl('/us/en/404'))
+      await fetch('/404.html')
         .then((html) => html.text())
         .then((data) => {
           const parser = new DOMParser();
