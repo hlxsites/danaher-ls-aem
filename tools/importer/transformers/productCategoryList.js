46f9928ac8ddd96b93a2eb847b37d87497ee1e80
wz914876 <sharanagouda_patil@pall.com>
2024-02-23 14:52:52 +0530

    initial commit

--- tools/importer/transformers/productCategoryList.js
+++ tools/importer/transformers/productCategoryList.js
@@ -1,14 +1,37 @@
 /* global WebImporter */
+const URLS_TO_BLOCK_MAP = [
+  {
+    url: '/us/en/solutions/mrna-therapy/mrna-development-manufacturing/products',
+    blockName: 'Solutions products list (workflow)',
+  },
+  {
+    url: '/us/en/products/brands/',
+    blockName: 'Product category list (opco-home)',
+  },
+  {
+    url: '/us/en/products',
+    blockName: 'Product category list',
+  },
+];
+
 const createProductCategoryList = (main, document) => {
   const url = document.querySelector('[property="og:url"]')?.content;
-  const placeholder = main.querySelector('div#product-category-list');
-  if (placeholder) {
-    let blockName = 'Product category list';
-    if (url && url.includes('/us/en/products/brands/')) blockName = 'Product category list (opco-home)';
-    const block = [[blockName], ['']];
-    const table = WebImporter.DOMUtils.createTable(block, document);
-    placeholder.innerHTML = '';
-    placeholder.append(table);
+  const placeholderProdCategory = main.querySelector('div#product-category-list');
+  const placeholderProdCard = main.querySelector('div.grid');
+  if (url) {
+    const blockName = URLS_TO_BLOCK_MAP.find((item) => url.includes(item.url))?.blockName;
+    if (blockName) {
+      const block = [[blockName], ['']];
+      const table = WebImporter.DOMUtils.createTable(block, document);
+      if (placeholderProdCategory) {
+        placeholderProdCategory.innerHTML = '';
+        placeholderProdCategory.append(table);
+      } else if (placeholderProdCard) {
+        placeholderProdCard.innerHTML = '';
+        placeholderProdCard.append(table);
+      }
+    }
   }
 };
+
 export default createProductCategoryList;
