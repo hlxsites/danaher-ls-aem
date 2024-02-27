80f9c73bef294fa33eb97c5522aa3f3e37d953b4
rgravitvl <ravitvl83@gmail.com>
2024-02-27 13:04:33 +0530

    Updated schema changes for product category list

--- scripts/schema.js
+++ scripts/schema.js
@@ -78,3 +78,46 @@ export function buildProductSchema() {
     'product',
   );
 }
+
+// eslint-disable-next-line import/prefer-default-export
+export function buildProductCategorySchema(products) {
+  const data = {
+    '@context': 'http://schema.org',
+    '@type': 'ProductItemList',
+    '@id': `https://lifesciences.danaher.com${makePublicUrl(window.location.pathname)}`,
+    name: getMetadata('og:title').replace(' | Danaher Life Sciences', ''),
+    image: getMetadata('og:image'),
+    description: getMetadata('description'),
+    itemListElement: [],
+  };
+
+  products.forEach((product) => {
+    data.itemListElement.push({
+      '@type': 'Product',
+      '@id': `https://lifesciences.danaher.com${makePublicUrl(product.path)}`,
+      name: product.title,
+      image: `https://lifesciences.danaher.com${product.image}`,
+      description: product.description,
+      brand: {
+        '@type': 'Brand',
+        name: product.brand,
+      },
+      offers: {
+        '@type': 'Offer',
+        priceCurrency: product.priceCurrency,
+        price: product.price,
+        availability: product.availability,
+        url: `https://lifesciences.danaher.com${makePublicUrl(product.path)}`,
+      },
+      mainEntityOfPage: {
+        '@type': 'WebPage',
+        '@id': `https://lifesciences.danaher.com${makePublicUrl(product.path)}`,
+      },
+    });
+  });
+
+  setJsonLd(
+    data,
+    'productItemList',
+  );
+}
