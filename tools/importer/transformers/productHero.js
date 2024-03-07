8f2dcdccb8355315b1ae1259d87b2ff2fbc7a5fb
rgravitvl <ravitvl83@gmail.com>
2024-03-07 18:04:02 +0530

    Updated the main branch host

--- tools/importer/transformers/productHero.js
+++ tools/importer/transformers/productHero.js
@@ -3,8 +3,12 @@
 async function getProductTitle(url, host) {
   if (url.pathname.match(/^\/content\/danaher\/ls\/us\/en\/products\/(family\/|sku\/)/)) {
     const urlStr = url.pathname.replace(/^\/content\/danaher\/ls/, '').replace(/\.html$/, '');
-    const productMeta = await fetch(`${host}e/metadata-products.json`)
-      .then((response) => response.json());
+    const productMeta = await fetch(`${host}/metadata-products.json`)
+      .then((response) => response.json())
+      .catch((error) => {
+        // eslint-disable-next-line no-console
+        console.error('Error:', error);
+      });
     const reqestedProduct = Array.from(productMeta.data)
       .filter((product) => product.URL === urlStr);
     if (reqestedProduct.length === 1) {
@@ -15,20 +19,21 @@ async function getProductTitle(url, host) {
 }
 
 const createProductHero = async (main, document, param, url) => {
-  const host = param.opts?.converterCfg?.liveUrls[0];
+  const host = param.opts?.converterCfg?.internalHost;
   const product = main.querySelector('product-page');
   if (product) {
-    const btnText = product.getAttribute('rfqbuttontext');
     const productCells = [
       ['Product Hero'],
-      [btnText],
     ];
-
     const title = document.createElement('h1');
     const titleVal = await getProductTitle(url, host);
     title.textContent = titleVal;
     if (titleVal) productCells.push([title]);
-    if (btnText) {
+
+    const btnText = product.getAttribute('rfqbuttontext');
+    if (btnText) productCells.push([btnText]);
+
+    if (productCells.length > 1) {
       const block = WebImporter.DOMUtils.createTable(productCells, document);
       product.append(block, document.createElement('hr'));
     }
