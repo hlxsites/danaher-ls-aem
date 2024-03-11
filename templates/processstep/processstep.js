1bf302840719ed36c984874986913bc65904b9c4
rgravitvl <ravitvl83@gmail.com>
2024-03-11 18:00:02 +0530

    updated the process step template

--- templates/processstep/processstep.js
+++ templates/processstep/processstep.js
@@ -3,8 +3,9 @@ import { buildBlock } from '../../scripts/lib-franklin.js';
 
 export default async function buildAutoBlocks() {
   const main = document.querySelector('main');
-  const sideNavBlock = div(buildBlock('side-nav', { elems: [] }));
+  const defaultContent = main.querySelector('main > div > h1').parentElement;
+  console.log(defaultContent);
+  const sideNavBlock = div({class: 'default-content-wrapper'}, buildBlock('side-nav', { elems: [] }), defaultContent);
   sideNavBlock.querySelector('.side-nav').classList.add('process-steps');
-  console.log(main.children[1]);
   main.children[1].insertAdjacentElement('afterend', sideNavBlock);
 }
