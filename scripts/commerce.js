e44091f85bd436140a7bc4c6930f71d780d9639f
rgravitvl <ravitvl83@gmail.com>
2024-03-11 23:39:40 +0530

    fixed lint issues

--- scripts/commerce.js
+++ scripts/commerce.js
@@ -115,13 +115,14 @@ export async function getProductResponse() {
 }
 
 function getWorkflowFamily() {
-  const pageUrl = window.location.pathname.replace(/^\/content\/danaher\/ls\/us\/en\/solutions\//, '').replace(/\.html$/, '').split('/');
-  if (Array.isArray(pageUrl) && pageUrl.length > 1) {
-    pageUrl?.pop();
-    const popedValue = pageUrl?.pop();
-    return `${pageUrl?.pop()}|${popedValue}`;
+  const pageUrl = window.location.pathname.replace(/^\/us\/en\/solutions\//, '').replace(/\.html$/, '').split('/');
+  let params;
+  if (pageUrl.includes('process-steps') && pageUrl.length === 4) {
+    params = pageUrl.filter((param) => param !== 'process-steps');
+  } else if (pageUrl.length === 3) {
+    params = pageUrl.filter((param) => param !== 'products');
   }
-  return '';
+  return params.join('|');
 }
 
 function getProductsOnSolutionsApiPayload(qParam) {
--- templates/processstep/processstep.js
+++ templates/processstep/processstep.js
@@ -4,8 +4,7 @@ import { buildBlock } from '../../scripts/lib-franklin.js';
 export default async function buildAutoBlocks() {
   const main = document.querySelector('main');
   const defaultContent = main.querySelector('main > div > h1').parentElement;
-  console.log(defaultContent);
-  const sideNavBlock = div({class: 'default-content-wrapper'}, buildBlock('side-nav', { elems: [] }), defaultContent);
+  const sideNavBlock = div({ class: 'default-content-wrapper' }, buildBlock('side-nav', { elems: [] }), defaultContent);
   sideNavBlock.querySelector('.side-nav').classList.add('process-steps');
   main.children[1].insertAdjacentElement('afterend', sideNavBlock);
 }
