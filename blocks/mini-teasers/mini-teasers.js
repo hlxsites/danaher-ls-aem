f8f4b6b409dc5f95ec062e7bba7d3b32ce47e5f4
rgravitvl <ravitvl83@gmail.com>
2024-03-19 19:59:51 +0530

    Updated for mini teaser UI

--- blocks/mini-teasers/mini-teasers.js
+++ blocks/mini-teasers/mini-teasers.js
@@ -1,11 +1,14 @@
 import { decorateModals } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-  block.classList.add(...'grid max-w-7xl w-full mx-auto gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 px-2 my-16'.split(' '));
+  block.classList.add(...'grid max-w-7xl w-full mx-auto gap-6 grid-cols-1 sm:grid-cols-2 px-2 my-16'.split(' '));
   [...block.children].forEach((element) => {
+    if ([...block.children].length === 4) block.classList.add('lg:grid-cols-4');
+    else block.classList.add('lg:grid-cols-3');
     element.classList.add(...'items-center'.split(' '));
     element.querySelector('span')?.classList.add(...'w-16 h-16'.split(' '));
     element.querySelector('h3')?.classList.add(...'text-lg pl-2 mt-4 mb-0 sm:text-lg font-medium'.split(' '));
+    element.querySelector('p')?.classList.add('pl-2');
     const link = element.querySelector('a');
     if (link) {
       link.innerHTML += ' ->';
@@ -13,6 +16,6 @@ export default function decorate(block) {
       if (link.href.endsWith('#RequestAQuote')) link.classList.add('show-modal-btn');
     }
   });
-  block.parentNode.classList.add(...'my-6 border-t border-b border-solid border-black'.split(' '));
+  if (!block.classList.contains('no-border')) block.parentNode.classList.add(...'my-6 border-t border-b border-solid border-black'.split(' '));
   decorateModals(block);
 }
