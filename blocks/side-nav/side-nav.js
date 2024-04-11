051ad8126e9eadb57194547b085cf0a74b950fe4
wz914876 <sharanagouda_patil@pall.com>
2024-04-11 10:35:16 +0530

    minor alignments

--- blocks/side-nav/side-nav.js
+++ blocks/side-nav/side-nav.js
@@ -25,7 +25,7 @@ function renderSideNav(sideNavItems) {
           class: 'flex gap-3',
         },
         a({
-          class: 'py-2 text-base',
+          class: 'pb-2 py-4 text-base',
           href: makePublicUrl(sideNavItem.path),
         }, sideNavItem.title),
       ),
@@ -64,7 +64,7 @@ export default async function decorate(block) {
   sideNavElements = renderSideNav(sideNavItems);
   selectedNavItem = sideNavElements.querySelector(`.side-nav-item a[href="${window.location.pathname}"]`)?.closest('.side-nav-item');
   if (selectedNavItem) selectedNavItem.classList.add(...'font-bold bg-danaherpurple-50 hover:bg-danaherpurple-50'.split(' '));
-  block.append(div({ class: 'text-lg py-4' }, strong(sideNavTitle)), sideNavElements);
+  block.append(div({ class: 'text-lg py-6' }, strong(sideNavTitle)), sideNavElements);
   block.classList.add(...'pt-6 pr-2'.split(' '));
   return block;
 }
