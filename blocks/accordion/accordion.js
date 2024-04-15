bb77e7aa6303ac98313ea3de13517422c865cd7e
wz914876 <sharanagouda_patil@pall.com>
2024-04-15 20:33:44 +0530

    removed aria-title

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -38,7 +38,6 @@ function createAccordionBlock(question, answer, image, uuid, index, customUUID) 
       'aria-expanded': false,
       'aria-controls': `accordion-${uuid}-${index}`,
       'aria-label': question,
-      'aria-title': question,
       'aria-labelledby': `accordion-${uuid}-${index}`,
       class: 'flex items-center justify-between w-full text-left font-semibold py-2 cursor-pointer peer-[&_span.plus]:opacity-100 peer-checked:[&_span.plus]:opacity-0 peer-checked:[&_span.plus]:rotate-45 peer-[&_span.minus]:opacity-0 peer-[&_span.minus]:rotate-90 peer-checked:[&_span.minus]:rotate-180 peer-checked:[&_span.minus]:opacity-100 peer-checked:[&_span.minus]:opacity-100',
     },
