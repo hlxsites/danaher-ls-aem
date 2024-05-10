5d02845ade3eb9dff656444d8c1dbca9bf07bb75
rgravitvl <ravitvl83@gmail.com>
2024-05-10 14:09:00 +0530

    Fixed page jump menu

--- scripts/scripts-dev.js
+++ scripts/scripts-dev.js
@@ -93,6 +93,29 @@ export function generateUUID() {
   return Math.floor(1000 + Math.random() * 9000);
 }
 
+let originalOffset = 0;
+
+export function scrollJumpMenuFixed(pageJumpMenuContainer) {
+  if (!originalOffset) {
+    const rectPageTabs = pageJumpMenuContainer.getBoundingClientRect();
+    originalOffset = rectPageTabs.top;
+  }
+  if (window.scrollY > originalOffset) {
+    pageJumpMenuContainer.classList.add(...'w-full fixed mt-[-1px] bg-white shadow-lg inset-x-0 top-[83px] py-2 z-10 [&_.page-jump-menu-wrapper]:md:max-w-7xl [&_ul>li>a]:flex-row [&_ul>li>a]:items-center [&_ul>li>a]:h-full [&_li>a>span.icon-chevron-down]:hidden'.split(' '));
+    document.querySelector('.page-jump-menu-container.fixed ul')?.classList.add('shadow-none', 'rounded-none');
+    document.querySelectorAll('.page-jump-menu-container.fixed ul li')?.forEach((el) => {
+      el?.firstElementChild?.classList.add('rounded-full');
+      el?.firstElementChild?.querySelector('span.icon svg use')?.classList.add('stroke-danaherpurple-500');
+    });
+    document.querySelector('.page-jump-menu-container.fixed li[aria-selected="true"] a span.icon svg')?.classList.add('stroke-white');
+    pageJumpMenuContainer.classList.remove(...'[&_.page-jump-menu-wrapper]:md:max-w-max [&_ul]:divide-x [&_ul>li>a]:h-40 [&_ul>li>a]:flex-col [&_ul>li>a]:justify-center'.split(' '));
+  } else {
+    pageJumpMenuContainer.classList.remove(...'w-full fixed mt-[-1px] bg-white shadow-lg inset-x-0 top-[83px] py-2 z-10 [&_.page-jump-menu-wrapper]:md:max-w-7xl [&_ul>li>a]:flex-row [&_ul>li>a]:items-center [&_ul>li>a]:h-full [&_li>a>span.icon-chevron-down]:hidden'.split(' '));
+    document.querySelectorAll('.page-jump-menu-container ul li')?.forEach((el) => el?.firstElementChild?.classList.remove('rounded-full'));
+    pageJumpMenuContainer.classList.add(...'[&_.page-jump-menu-wrapper]:md:max-w-max [&_ul]:divide-x [&_ul>li>a]:h-40 [&_ul>li>a]:flex-col [&_ul>li>a]:justify-center'.split(' '));
+  }
+}
+
 /**
  * Returns the valid public url with or without .html extension
  * @param {string} url
