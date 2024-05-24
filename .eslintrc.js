7b4e8635794f33ef69e133a8cda2a9e5eb5f193c
wz914876 <sharanagouda_patil@pall.com>
2024-05-24 15:52:53 +0530

    1141 [ReBrand] Converter update for logo-cloud

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,7 +16,7 @@ module.exports = {
   rules: {
     // allow reassigning param
     'no-param-reassign': [2, { props: false }],
-    'linebreak-style': ['error', 'unix'],
+    'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix'],
     'import/extensions': ['error', {
       js: 'always',
     }],
