0841b63d1f6781215da5cdd4a79074de54cde545
rgravitvl <ravitvl83@gmail.com>
2024-11-15 13:03:25 +0530

    Upated the converter for column

--- tools/importer/transformers/fullLayoutSection.js
+++ tools/importer/transformers/fullLayoutSection.js
@@ -3,11 +3,16 @@ const createFullLayoutSection = (main, document) => {
   let styleIndex = 0;
   main.querySelectorAll('fulllayout').forEach((e, i, arr) => {
     const div = e.querySelector('div');
-    const style = div.getAttribute('class');
-    const fullLayoutClass = style + ',' + div.getAttribute('id');
+    let style = div.getAttribute('class');
+    const idArray = div.getAttribute('id')?.split(':');
+    let value;
+    if (idArray && idArray.at(0) === 'additionalstyles') {
+      value = idArray.at(1);
+      style = `${style},${value}`;
+    }
     if (style) {
       if (styleIndex === 0 && e.parentNode.previousElementSibling) e.prepend(document.createElement('hr'));
-      const cells = [['Section Metadata'], ['style', fullLayoutClass]];
+      const cells = [['Section Metadata'], ['style', style]];
       const table = WebImporter.DOMUtils.createTable(cells, document);
       e.after(table);
       if ((arr.length === 1 || i < arr.length - 1)
