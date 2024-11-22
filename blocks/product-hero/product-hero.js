4ccaa72c6e5a9598a30ca03a71cc30bb582c4c3f
Raja-danaher <raja.tchandra@dhlifesciences.com>
2024-11-22 15:45:18 +0530

    Update product-hero.js

--- blocks/product-hero/product-hero.js
+++ blocks/product-hero/product-hero.js
@@ -37,7 +37,8 @@ function loadMore() {
 }
 
 function imageSlider(allImages, productName = 'product') {
-  const slideContent = div({ class: 'image-content' }, createOptimizedS7Picture(allImages[0], `${productName} - image`, true));
+  const imageToUse = allImages[0] ? allImages[0] : 'https://s7d9.scene7.com/is/image/danaherstage/no-image-availble';
+  const slideContent = div({ class: 'image-content' }, createOptimizedS7Picture(imageToUse, `${productName} - image`, true));
   const verticalSlides = div();
   allImages.map((image, index) => {
     const imageElement = createOptimizedS7Picture(image, `${productName} - image ${index + 1}`, false);
