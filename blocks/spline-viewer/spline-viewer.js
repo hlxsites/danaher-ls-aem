cf6deeac2401be8f3fc947724090117d5ffd913e
rgravitvl <ravitvl83@gmail.com>
2024-12-06 22:14:05 +0530

    Fixed spline viewer issue

--- blocks/spline-viewer/spline-viewer.js
+++ blocks/spline-viewer/spline-viewer.js
@@ -1,4 +1,5 @@
 import { getFragmentFromFile } from '../../scripts/scripts.js';
+import { loadScript } from '../../scripts/lib-franklin.js';
 
 export default async function decorate(block) {
   try {
@@ -7,9 +8,7 @@ export default async function decorate(block) {
     block.innerHTML = '';
     const parser = new DOMParser();
     const fragmentHtml = parser.parseFromString(fragment, 'text/html');
-    [...fragmentHtml.body.children].forEach((item) => {
-      block.append(item);
-    });
+    block.append(fragmentHtml?.body?.firstElementChild);
   } catch (e) {
     block.textContent = '';
     // eslint-disable-next-line no-console
