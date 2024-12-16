85aa02a56052e43bb2f2f4e609e89ffe9d850c16
Saniya Mew <saniyamew.alvarez@dhlscontractors.com>
2024-12-16 22:16:53 +0530

    SlideOutImplementation

--- scripts/slideout.js
+++ scripts/slideout.js
@@ -2,7 +2,7 @@ import {
   div, p, button, img
 } from '../../scripts/dom-builder.js';
 
-export  function createCartSlideout(main, json) {
+export function createCartSlideout(main, json) {
 
   const slideout = div({ class: 'slideout p-4' });
   const slideoutHeading = div({ class: '' },
@@ -10,6 +10,7 @@ export  function createCartSlideout(main, json) {
       p({ class: 'text-lg font-bold' }, 'Item added to your cart'),
       img({ class: 'closeIconStyle', src: '/icons/close.svg', alt: 'closeIcon' })
     ),
+    //Heading
     div({ class: 'flex' },
       div({ class: 'w-1/6' }, ''),
       div({ class: 'text-xs w-1/2 ml-2' }, 'Description'),
@@ -17,14 +18,16 @@ export  function createCartSlideout(main, json) {
       div({ class: 'text-xs w-1/4 text-center' }, 'Unit Price')
     )
   );
-
   slideout.append(slideoutHeading);
+
   jsonIterate(json, slideout);
 
   // Checkout Button
   const checkoutButton =  button({ class: 'btn-primary-purple rounded-full px-6 btn btn-lg font-medium w-full mt-5' }, 'Checkout');
   slideout.appendChild(checkoutButton);
   main.append(slideout);
+
+  //Dismiss slideout with close Icon
   const imgElement = document.querySelector('.closeIconStyle');
   imgElement.addEventListener('click', toggleClass);
   toggleClass();
@@ -34,6 +37,7 @@ function toggleClass() {
     document.querySelector('.slideout').classList.toggle('on');
 }
 
+//Showing products that has been added to the cart from json
 function jsonIterate(json, slideout) {
   json.forEach((item) => {
     const contentBox = div({ class: 'flex pb-6 mb-6 border-b pb-4' });
