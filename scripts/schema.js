0d31f5ede1ce796ef198c8294bc0e62b0714cce9
lakshmithi <lakshminarayanan.thi@dhlifesciences.com>
2025-01-17 13:04:10 +0530

    fix for itemschema element description

--- scripts/schema.js
+++ scripts/schema.js
@@ -204,7 +204,7 @@ export function buildItemListSchema(srcObj, type) {
           obj.clickUri,
           obj.title,
           obj?.raw?.images?.at(0),
-          obj.excerpt,
+          obj?.raw?.description,
         ));
         break;
       case 'resources':
