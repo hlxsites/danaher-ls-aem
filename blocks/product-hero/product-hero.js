f3639cf1d8604ed1c4c2261fa52cff0f26af572c
lakshmithi <lakshminarayanan.thi@dhlifesciences.com>
2025-02-19 20:18:16 +0530

    test fix for branch conflict issue

--- blocks/product-hero/product-hero.js
+++ blocks/product-hero/product-hero.js
@@ -249,6 +249,7 @@ export default async function decorate(block) {
       infoDiv.querySelector('a .ext-link').innerHTML = `Visit ${response[0]?.raw.opco}<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="inline-block w-5 h-5 pb-1"><path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"/></svg>`;
     }
 
+
     try {
       if (response[0].raw?.bundlepreviewjson) {
         const bundleDetails = JSON.parse(response[0].raw?.bundlepreviewjson);
