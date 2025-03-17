7123e990d856cdadf251965d3cb2c6893789f99f
Raja-danaher <raja.tchandra@dhlifesciences.com>
2025-03-17 14:11:38 +0530

    new tiled-viewer block

---
+++ blocks/tiled-viewer/tiled-viewer.js
@@ -0,0 +1,25 @@
+import { loadScript } from '../../scripts/lib-franklin.js';
+import { getFragmentFromFile } from '../../scripts/scripts.js';
+
+export default async function decorate(block) {
+  try {
+    // get the content
+    const fragment = await getFragmentFromFile('/fragments/tiled.html');
+    block.innerHTML = '';
+    const parser = new DOMParser();
+    const fragmentHtml = parser.parseFromString(fragment, 'text/html');
+    const observer = new IntersectionObserver((entries) => {
+      if (entries.some((e) => e.isIntersecting)) {
+        observer.disconnect();
+        loadScript(fragmentHtml?.head?.firstElementChild?.src, { type: 'module' });
+        block.append(fragmentHtml?.body?.firstElementChild);
+      }
+    });
+    observer.observe(block);
+    block.classList.add('!h-[200px]', 'md:!h-[600px]');
+  } catch (e) {
+    block.textContent = '';
+    // eslint-disable-next-line no-console
+    console.warn(`cannot load snippet at ${e}`);
+  }
+}
