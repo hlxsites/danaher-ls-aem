2760ee18aaa0d3a3fd71c6b991f2bbecac747333
rgravitvl <ravitvl83@gmail.com>
2025-03-25 11:57:30 +0530

    Updated the block name with class

--- tools/importer/transformers/opcoHomeArticlesList.js
+++ tools/importer/transformers/opcoHomeArticlesList.js
@@ -1,10 +1,12 @@
 /* global WebImporter */
 const createOpcoHomeArticlesList = (main, document) => {
+  const url = document.querySelector('[property="og:url"]')?.content;
   const placeholder = main.querySelector('div#articles-opco-home');
   if (placeholder) {
     const pEl = document.createElement('p');
     pEl.textContent = placeholder?.innerHTML;
-    const blockName = 'Articles list';
+    let blockName = 'Articles list';
+    if(url.endsWith('/us/en/new-lab')) blockName = 'Articles list (new-lab)';
     const block = [[blockName], [pEl.textContent]];
     const table = WebImporter.DOMUtils.createTable(block, document);
     placeholder.innerHTML = '';
