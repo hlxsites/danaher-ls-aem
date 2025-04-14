ad4a89c898b4bbadf12e5e819884beac0331af76
Raja-danaher <raja.tchandra@abcam.com>
2025-04-14 13:55:22 +0530

    update

--- blocks/opco-consent/opco-consent.js
+++ blocks/opco-consent/opco-consent.js
@@ -93,6 +93,7 @@ async function updateConsent(email, hashId) {
 
   debugLog('Calling Boomi API:', { endpoint: url, email: obfuscateEmail(email) });
 
+  
   try {
     const response = await fetch('https://stage.lifesciences.danaher.com/content/danaher/services/boomi/opcopreferences', {
       method: 'POST',
