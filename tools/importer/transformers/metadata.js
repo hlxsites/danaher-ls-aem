44bb016f50b9abb86ba23767a2c1c77fc41b2eba
Raja-danaher <raja.tchandra@abcam.com>
2025-04-21 17:40:10 +0530

    pageorderupdate

--- tools/importer/transformers/metadata.js
+++ tools/importer/transformers/metadata.js
@@ -53,6 +53,7 @@ const addSolutionMeta = (url, meta) => {
     const solution = url.pathname.replace(/^\/content\/danaher\/ls\/us\/en\/solutions\//, '').replace(/\.html$/, '').split('/');
     if (url.pathname.includes('/process-steps/')) {
       meta.solution = solution.at(1);
+      meta.pageorder = solution?.at(2);
     }
   }
 };
