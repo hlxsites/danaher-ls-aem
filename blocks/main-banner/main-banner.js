9d7f7ef10fa78db32b4b03609a85fd021a2e43be
NithinBasetti <nithin.basetti@gspann.com>
2025-04-23 11:34:48 +0530

    created json object and mapped

--- blocks/main-banner/main-banner.js
+++ blocks/main-banner/main-banner.js
@@ -7,16 +7,23 @@ const container = div({
   class: 'flex flex-col md:flex-row h-screen bg-white',
   id: ''
 });
-
+const bannerInfo = [
+  {
+    heading:
+      "The world leader in\nmicroscopes and scientific\ninstruments",
+      buttonText: "Browse Categories",
+  },
+];
+const { heading, description } = bannerInfo[0];
 // Left Section
 const left = div({
   class: 'md:w-1/2 flex flex-col justify-center items-start px-10 py-12 space-y-6'
 },
   p({ class: 'text-sm text-indigo-600 font-medium' }, 'Leica Microsystems'),
-  h1({ class: 'text-3xl md:text-4xl font-semibold text-gray-900' }, 'The world leader in\nmicroscopes and scientific\ninstruments'),
+  h1({ class: 'text-3xl md:text-4xl font-semibold text-gray-900' }, heading),
   button({
     class: 'bg-purple-600 text-white px-6 py-2 rounded-full hover:bg-purple-700 transition'
-  }, 'Browse Categories')
+  }, buttonText)
 );
 
 // Right Section
