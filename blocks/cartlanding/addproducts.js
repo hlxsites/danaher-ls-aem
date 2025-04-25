28cefb1cfcb04493e9e2bd1dc39c32c12f525731
DeepikaYadav02 <deepika.yadav@dhlscontractors.com>
2025-04-25 13:02:16 +0530

    integrated changes for search box

--- blocks/cartlanding/addproducts.js
+++ blocks/cartlanding/addproducts.js
@@ -20,35 +20,50 @@ export const addProducts = () => {
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vestibulum semper mollis. Integer pharetra dui sed urna cursus, eu pretium tortor sodales. "
     ),
     div({
-        class: "w-24 h-10 left-[63px] top-[57px]  bg-white"
+      class: "w-24 h-10 left-[63px] top-[57px]  bg-white",
     }),
-    div({
-        class:"w-80 justify-start text-gray-700 text-base font-bold font-['TWK_Lausanne_Pan'] leading-snug"
-    },"Search for a product"),
-    div({
-        class: "w-24 h-4 left-[63px] top-[57px]  bg-white"
-    }),
-    div({
-        class:"max-w-md h-10 px-4 border-solid border-2 rounded-md inline-flex  justify-between items-center"
-    },
-    div({
-        class:""
-    }, '🔍'),
-     input({
-            type: 'text',
-            placeholder: 'Search by name or SKU ID'
-          }),
-
+    div(
+      {
+        class:
+          "w-80 justify-start text-gray-700 text-base font-bold font-['TWK_Lausanne_Pan'] leading-snug",
+      },
+      "Search for a product"
     ),
-    button({
-        class:"btn btn-lg font-medium btn-primary-purple rounded-full px-6"
-     },"Get started"),
-    
-     div({
-        class: "w-24 h-10 left-[63px] top-[57px]  bg-white"
+    div({
+      class: "w-10 h-4 left-[63px] top-[57px]  bg-white",
     }),
-    
+    div(
+      {
+        class: "inline-flex gap-2 bg-white",
+      },
+      div(
+        {
+          class:
+            "w-[75px] h-10 px-6 border-solid border-2 rounded-md inline-flex justify-between items-center",
+        },
+        div(
+          {
+            class: "",
+          },
+          "🔍"
+        ),
+        div({
+          class: "flex-1 justify-start text-gray-500 font-extralight text-base ",
+         
+           
+        }, "Search by name or SKU ID")
+      ),
+      button(
+        {
+          class: "h-10 btn btn-lg font-medium btn-primary-purple rounded-full ",
+        },
+        "Get started"
+      )
+    ),
+
+    div({
+      class: "w-24 h-10 left-[63px] top-[57px]  bg-white",
+    })
   );
   return productContainer;
 };
-
