e0bee5ee22eb29d327a5bb0ce0f193619d2f94d5
SanyaKasaudhan1 <sanya.kasaudhan@dhlscontractors.com>
2025-05-07 11:26:42 +0530

    banner css

--- blocks/product-banner/product-banner.js
+++ blocks/product-banner/product-banner.js
@@ -30,7 +30,6 @@ export default async function decorate(block) {
       links: [block.querySelector('[data-aue-prop="carousel_button11"]')?.textContent,  block.querySelector('[data-aue-prop="carousel_button21"]')?.textContent ],
     },
   ];
-  block.textContent = ""; 
   categoryHeroBanner.forEach((banner) => {
     const {
       categoryHeading,
@@ -145,7 +144,7 @@ export default async function decorate(block) {
     categoryBannerRight.append(categoryBannerIcon, categoryBannerDetails);
   
     categoryBanner.append(categoryBannerLeft, categoryBannerRight);
-    
+    block.innerHTML = '';
     block.append(categoryBanner);
   });
   
