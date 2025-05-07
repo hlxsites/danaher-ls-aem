d116e87da4758a88c7b14a2fb933b5a99fc809db
SanyaKasaudhan1 <sanya.kasaudhan@dhlscontractors.com>
2025-05-07 12:46:55 +0530

    banner

--- blocks/product-banner/product-banner.js
+++ blocks/product-banner/product-banner.js
@@ -1,453 +1,126 @@
-import {
-  div,
-  h3,
-  input,
-  label,
-  span,
-  button,
-  fieldset,
-  ul,
-  li,
-  a,
-  img,
-  p,
-} from "../../scripts/dom-builder.js";
-import { decorateIcons } from "../../scripts/lib-franklin.js";
+import { div, span, img, p } from "../../scripts/dom-builder.js";
 
-export default async function decorate(block) {
-
+export default function decorate(block) {
   console.log("PROD block", block);
-  
-  const categoryHeroBanner = [
-    {
-      categoryHeading: block.querySelector('[data-aue-prop="content_heading"]')?.textContent || '',
-      linkText: block.querySelector('[data-aue-prop="banner_heading1"]')?.textContent,
-      categoryDescription:block.querySelector('[data-aue-prop="banner_heading2"]')?.textContent,
-      links: [block.querySelector('[data-aue-prop="banner_button1"]')?.textContent,  block.querySelector('[data-aue-prop="banner_button2"]')?.textContent ],   
-      image: block.querySelector('img[data-aue-prop="banner_image"]'),
-      alt: imageEl?.getAttribute('alt') || 'category image',
-      details:block.querySelector('[data-aue-prop="content_desc"]')?.textContent,
-      detailsLink: "Read More",
-       },
+  const categoryHeading =block.querySelector('[data-aue-prop="content_heading"]')?.textContent || '';
+  const linkText =
+    block.querySelector('[data-aue-prop="banner_heading1"]')?.textContent || "";
+  const categoryDescription =
+    block.querySelector('[data-aue-prop="banner_heading2"]')?.textContent || "";
+  const links = [
+    block.querySelector('[data-aue-prop="banner_button1"]')?.textContent || "",
+    block.querySelector('[data-aue-prop="banner_button2"]')?.textContent || "",
   ];
-  categoryHeroBanner.forEach((banner) => {
-    const {
-      categoryHeading,
-      linkText,
-      categoryDescription,
-      image,
-      alt,
-      details,
-      detailsLink,
-      links,
-    } = banner;
-  
-    const categoryBanner = div({
-      class:
-        "category_banner flex flex-col lg:flex-row self-stretch justify-start items-center",
-    });
-  
-    const categoryBannerLeft = div({
-      class:
-        "category_banner-left ml-4 w-80 lg:w-[600px] pt-6 lg:pt-12 flex flex-col justify-start items-start",
-    });
-  
-    const categoryBannerRight = div({
-      class:
-        "category_banner-right mr-4 w-80 lg:w-[600px] relative flex flex-col justify-start items-start",
-    });
-  
-    const categoryBannerTitle = p(
-      {
-        class:
-          'text-black text-4xl font-normal  leading-[48px]',
-      },
-      categoryHeading
-    );
-  
-    const categoryBannerCta = div(
-      {
-        class:
-          "category_banner-cta flex flex-wrap justify-start items-start mb-4 mt-4",
-      },
-      div(
-        {
-          class:
-            "px-6 py-3 bg-violet-600 rounded-[30px] shadow-md flex justify-center items-center overflow-hidden",
-        },
-        div(
-          {
-            class:
-              'text-white text-base font-normal  leading-snug',
-          },
-          linkText
-        )
-      )
-    );
-  
-    const categoryBannerDescription = div(
-      {
-        class:
-          'category_banner-description text-gray-800 text-base font-extralight  leading-snug',
-      },
-      categoryDescription
-    );
-  
-    const categoryBannerLinks = div(
-      {
-        class: "category_banner-links self-stretch flex flex-col mt-4",
-      },
-      ...links.map((text) =>
-        div(
-          {
-            class:
-              'text-violet-600 text-base font-bold  leading-snug',
-          },
-          text
-        )
-      )
-    );
-  
-    const categoryBannerIcon = img({
-      src: image,
-      alt,
-      class: "h-[460px] object-contain",
-    });
-  
-    const categoryBannerDetails = div(
-      {
-        class: "category_banner-details w-full justify-start",
-      },
-      span(
-        {
-          class:
-            'text-black text-base font-extralight  leading-snug',
-        },
-        details
-      ),
-      span(
-        {
-          class:
-            'text-violet-600 text-base font-bold  leading-snug ml-2',
-        },
-        detailsLink
-      )
-    );
-  
-    categoryBannerLeft.append(
-      categoryBannerTitle,
-      categoryBannerCta,
-      categoryBannerDescription,
-      categoryBannerLinks
-    );
-  
-    categoryBannerRight.append(categoryBannerIcon, categoryBannerDetails);
-  
-    categoryBanner.append(categoryBannerLeft, categoryBannerRight);
-    console.log("categoryBanner", block, categoryBanner);
-    block.append(categoryBanner);
+  const details =
+    block.querySelector('[data-aue-prop="content_desc"]')?.textContent || "";
+  const detailsLink = "Read More";
+  const image = block.querySelector("img");
+  const alt = image?.getAttribute("alt") || "category image";
+
+  const categoryBanner = div({
+    class:
+      "category_banner flex flex-col lg:flex-row self-stretch justify-start items-center",
   });
-  
-  //line break
-  const lineBr = div({
-    class: 'w-full h-px bg-gray-400 mt-10',
+
+  const categoryBannerLeft = div({
+    class:
+      "category_banner-left ml-4 w-80 lg:w-[600px] pt-6 lg:pt-12 flex flex-col justify-start items-start",
   });
-  block.append(lineBr);
-  
-  // const subProductData=[{
-  //   subProductTitle: block.querySelector('[data-aue-prop="prod_hero_title"]')?.textContent || "Primary Antibodies",
-  //   "subRead": "Read More",
-  //   subProductDescription: block.querySelector('[data-aue-prop="prod_hero_description"]')?.textContent ||
-  //     "Our extensive primary antibody catalog features widely referenced monoclonal and polyclonal antibodies, along with an exceptional selection of recombinant monoclonal antibodies. Additionally, we provide a wide selection of fluorescently conjugated antibodies and carrier-free, conjugation-ready antibodies.",
-  // }]
-  // subProductData.forEach((banner) => {
-  //   const {
-  //     subProductTitle, subProductDescription, subRead,
-  //   } = banner;
-  // // ---- Primary Antibodies Section ----
-  // const primaryAntibodies = div({
-  //   class:
-  //     "primary_antibodies self-stretch py-12 bg-white border-b border-gray-400 inline-flex flex-col justify-center items-start gap-12 overflow-hidden",
-  // });
 
-  // const primaryHeader = div({
-  //   class:
-  //     "primary_antibodies-header self-stretch flex flex-col justify-start items-start gap-5",
-  // });
-
-  // const primaryTitleWrapper = div(
-  //   {
-  //     class: "self-stretch inline-flex justify-start items-center gap-12",
-  //   },
-  //   div(
-  //     {
-  //       class:
-  //         'flex-1 justify-start text-black text-3xl font-normal  leading-10',
-  //     },
-  //     subProductTitle
-  //   )
-  // );
-
-  // const primaryDescription = div(
-  //   {
-  //     class: "self-stretch flex flex-col justify-start items-start gap-4",
-  //   },
-  //   div(
-  //     {
-  //       class: "self-stretch justify-start",
-  //     },
-  //     span(
-  //       {
-  //         class:
-  //           'text-black text-base font-extralight  leading-snug',
-  //       },
-  //       subProductDescription
-  //     ),
-  //     span(
-  //       {
-  //         class:
-  //           'text-violet-600 text-base font-bold  leading-snug',
-  //       },
-  //       subRead
-  //     )
-  //   )
-  // );
-
-  // primaryHeader.append(primaryTitleWrapper, primaryDescription);
-  // primaryAntibodies.append(primaryHeader);
-  // block.append(primaryAntibodies);
-  // });
-
-  // feature
-
-  const featureHighlight = {
-    category: "Antibodies",
-    title:
-      "Anti-SARS-CoV-2 S antibody [EPR24852-116] - Human IgG1 (Chimeric) - BSA and Azide free(AB323002)",
-    description:
-      "Human Recombinant Monoclonal SPIKE antibody. Carrier free. Suitable for WB, I-ELISA and reacts with SARS-CoV-2 samples.",
-    image: {
-      src: "https://feature-em15--danaher-ls-aem--hlxsites.hlx.page/icons/feature-section.png",
-      alt: "feature-section",
-    },
-    button: {
-      text: "Learn More",
-    },
-  };
-  
-  const {
-    category,
-    title,
-    description,
-    image,
-    button
-  } = featureHighlight;
-  
-  const featureDiv = div({
-    class: "flex flex-col lg:flex-row w-full mt-12",
+  const categoryBannerRight = div({
+    class:
+      "category_banner-right mr-4 w-80 lg:w-[600px] relative flex flex-col justify-start items-start",
   });
-  
-  const blackBoxSection = div(
+
+  const categoryBannerTitle = p(
     {
-      class:
-        "w-full lg:w-1/2 bg-black/5 flex flex-col justify-start items-start overflow-hidden",
+      class: "text-black text-4xl font-normal leading-[48px]",
     },
-    span(
-      {
-        class: "w-full aspect-[2/1] origin-top-left",
-      },
-      img({
-        src: image.src,
-        alt: image.alt,
-        class: "w-full h-full object-contain",
-      })
-    )
+    categoryHeading
   );
-  
-  const violetSection = div(
+
+  const categoryBannerCta = div(
     {
       class:
-        "w-full lg:w-1/2 p-8 bg-violet-600 flex flex-col justify-center items-start gap-6",
+        "category_banner-cta flex flex-wrap justify-start items-start mb-4 mt-4",
     },
     div(
       {
-        class: "w-full flex flex-col justify-start items-start gap-4",
+        class:
+          "px-6 py-3 bg-violet-600 rounded-[30px] shadow-md flex justify-center items-center overflow-hidden",
       },
       div(
         {
-          class:
-            'text-white text-lg font-normal  leading-normal',
+          class: "text-white text-base font-normal leading-snug",
         },
-        category
-      ),
-      div(
-        {
-          class:
-            'text-white text-2xl font-normal  leading-loose',
-        },
-        title
-      ),
-      div(
-        {
-          class:
-            'text-white text-base font-extralight  leading-snug',
-        },
-        description
-      ),
-      div(
-        {
-          class: "inline-flex justify-start items-start gap-4",
-        },
-        div(
-          {
-            dataState: "Hover",
-            dataTheme: "Default",
-            class:
-              "px-6 py-3 bg-white rounded-[30px] shadow-[0px_1px_2px_0px_rgba(0,0,0,0.05)] flex justify-center items-center overflow-hidden",
-          },
-          div(
-            {
-              class:
-                'text-violet-600 text-base font-bold  leading-snug',
-            },
-            button.text
-          )
-        )
+        linkText
       )
     )
   );
-  
-  featureDiv.append(blackBoxSection, violetSection);
-  block.append(featureDiv);
-  
-  //Related categories
-  const relatedCategories = [
-    {
-      title: "Recombinant Monoclonal",
-      description:
-        "Our capillary electrophoresis systems are designed to address your analytical needs and challenges.",
-      image: "https://feature-em15--danaher-ls-aem--hlxsites.hlx.page/icons/feature-section.png",
-      linkText: "Browse All Products →",
-    },
-    {
-      title: "Carrier Free Antibodies",
-      description:
-        "Our capillary electrophoresis systems are designed to address your analytical needs and challenges.",
-      image: "https://feature-em15--danaher-ls-aem--hlxsites.hlx.page/icons/aldevron-4c.png",
-      linkText: "Browse All Products →",
-    },
-    {
-      title: "Polyclonal Antibodies",
-      description:
-        "Our capillary electrophoresis systems are designed to address your analytical needs and challenges.",
-      image: "https://feature-em15--danaher-ls-aem--hlxsites.hlx.page/icons/HemoCue.png",
-      linkText: "Browse All Products →",
-    },
-  ];
-  
-  
-  const relatedCategoriesSection = div({
-    class:
-      "self-stretch flex flex-col lg:flex-row justify-start items-start gap-5 mt-12",
-  });
 
-  const leftTitle = div(
+  const categoryBannerDescription = div(
     {
       class:
-        'w-72 text-black text-3xl font-normal  leading-10',
+        "category_banner-description text-gray-800 text-base font-extralight leading-snug",
     },
-    "Related\nCategories"
+    categoryDescription
   );
 
-  const cardsContainer = div({
-    class:
-      "w-full flex flex-col lg:flex-row justify-end items-start gap-5 lg:gap-12",
-  });
-
-  relatedCategories.forEach((category) => {
-    const categoryCard = div(
-      {
-        class:
-          "w-full lg:w-72 min-h-96 bg-white outline outline-1 outline-gray-300 flex flex-col justify-start items-start",
-      },
-      img({
-        class: "self-stretch h-40 relative",
-        src: category.image,
-        alt: category.title,
-      }),
+  const categoryBannerLinks = div(
+    {
+      class: "category_banner-links self-stretch flex flex-col mt-4",
+    },
+    ...links.map((text) =>
       div(
         {
-          class: "self-stretch h-52 flex flex-col justify-between items-start",
+          class: "text-violet-600 text-base font-bold leading-snug",
         },
-        div(
-          {
-            class:
-              "self-stretch p-3 bg-white flex flex-col justify-start items-start gap-3",
-          },
-          div(
-            {
-              class:
-                "self-stretch flex flex-col justify-start items-start gap-1",
-            },
-            div(
-              {
-                class:
-                  "self-stretch flex flex-col justify-start items-start gap-2",
-              },
-              div(
-                {
-                  class:
-                    "self-stretch flex flex-col justify-start items-start gap-3",
-                },
-                div(
-                  {
-                    class:
-                      'self-stretch justify-start text-black text-xl font-normal  leading-7',
-                  },
-                  category.title
-                ),
-                div(
-                  {
-                    class:
-                      "self-stretch inline-flex justify-start items-center gap-3",
-                  },
-                  div(
-                    {
-                      class:
-                        "flex-1 inline-flex flex-col justify-start items-start",
-                    },
-                    div(
-                      {
-                        class:
-                          'self-stretch justify-start text-gray-700 text-base font-extralight  leading-snug',
-                      },
-                      category.description
-                    )
-                  )
-                )
-              )
-            )
-          )
-        ),
-        div(
-          {
-            class:
-              "self-stretch p-3 bg-white inline-flex justify-start items-center",
-          },
-          div(
-            {
-              class:
-                'justify-start text-violet-600 text-base font-bold  leading-snug',
-            },
-            category.linkText
-          )
-        )
+        text
       )
-    );
-    cardsContainer.append(categoryCard);
+    )
+  );
+
+  const categoryBannerIcon = img({
+    src: image?.src || "",
+    alt,
+    class: "h-[460px] object-contain",
   });
-  relatedCategoriesSection.append(leftTitle, cardsContainer);
 
-  block.append(relatedCategoriesSection);
+  const categoryBannerDetails = div(
+    {
+      class: "category_banner-details w-full justify-start",
+    },
+    span(
+      {
+        class: "text-black text-base font-extralight leading-snug",
+      },
+      details
+    ),
+    span(
+      {
+        class: "text-violet-600 text-base font-bold leading-snug ml-2",
+      },
+      detailsLink
+    )
+  );
 
-  }
+  categoryBannerLeft.append(
+    categoryBannerTitle,
+    categoryBannerCta,
+    categoryBannerDescription,
+    categoryBannerLinks
+  );
+
+  categoryBannerRight.append(categoryBannerIcon, categoryBannerDetails);
+
+    categoryBanner.append(categoryBannerLeft, categoryBannerRight);
+  console.log("categoryBanner", block, categoryBanner);
+  block.innerHTML = '';
+  block.appendChild(categoryBanner);
+
+  // Line break
+  const lineBr = div({
+    class: "w-full h-px bg-gray-400 mt-10",
+  });
+  block.append(lineBr);
+}
