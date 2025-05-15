2caab1d0d5029d9fab6a2e294caf8f002af55a17
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-05-15 18:02:08 +0530

    updating checkout block for summary module

---
+++ blocks/checkout/checkoutSummary.js
@@ -0,0 +1,397 @@
+import { div, span, p, h5, button, strong } from "../../scripts/dom-builder.js";
+import {
+  changeStep,
+  taxExemptModal,
+  getCheckoutSummary,
+  getUseAddresses,
+} from "./checkoutUtilities.js";
+// import  functions / modules from common utilities...
+import {
+  createModal,
+  getStoreConfigurations,
+} from "../../scripts/common-utils.js";
+
+// store config to use some predefined set of rules/values
+const storeConfigurations = await getStoreConfigurations();
+// get price type if its net or gross....
+let checkoutPriceType = "net";
+if (storeConfigurations.pricing?.priceType) {
+  checkoutPriceType = storeConfigurations.pricing.priceType;
+}
+//const currencyCode = checoutConfigProps.data.general.defaultCurrency;
+const currencyCode = "$";
+
+// generates the checkout summary module.......
+export const checkoutSummary = async () => {
+  const getCheckoutSummaryData = await getCheckoutSummary();
+  let checkoutSummaryData = [];
+  if (getCheckoutSummaryData && getCheckoutSummaryData.status === "success") {
+    checkoutSummaryData = getCheckoutSummaryData.data;
+  }
+
+  const getTotalValue = (type) =>
+    `${currencyCode}${
+      checkoutSummaryData.totals[type][
+        checkoutPriceType === "net" ? "net" : "gross"
+      ].value
+    }`;
+
+  // map the data from checkout summary (basket) to the keys....
+  const checkoutSummaryKeys = {
+    totalProductQuantity: checkoutSummaryData.totalProductQuantity,
+    undiscountedItemTotal: checkoutSummaryData.totals?.undiscountedItemTotal
+      ? getTotalValue("undiscountedItemTotal")
+      : "0",
+    itemTotal: checkoutSummaryData.totals?.itemTotal
+      ? getTotalValue("itemTotal")
+      : "0",
+    undiscountedShippingTotal: checkoutSummaryData.totals
+      ?.undiscountedShippingTotal
+      ? getTotalValue("undiscountedShippingTotal")
+      : "0",
+    shippingTotal: checkoutSummaryData.totals?.shippingTotal
+      ? getTotalValue("shippingTotal")
+      : "0",
+    total: checkoutSummaryData.totals?.grandTotal
+      ? getTotalValue("grandTotal")
+      : "0",
+    tax: checkoutSummaryData.totals?.grandTotal
+      ? `${currencyCode} ${checkoutSummaryData.totals.grandTotal.tax.value}`
+      : "0",
+    discounts: checkoutSummaryData.discounts ? "$77" : "0",
+  };
+
+  const summaryModule = div(
+    {
+      class: " mt-4 flex flex-col justify-start items-start gap-4 pt-6 mt-6",
+    },
+    div(
+      {
+        class: " flex flex-col justify-start items-start gap-y-6",
+        id: "checkoutSummaryWrapper",
+      },
+      div(
+        {
+          class: " flex flex-col justify-start items-start gap-4",
+        },
+        div(
+          {
+            class:
+              "checkout-summary-subtotal  flex justify-between w-full gap-9",
+            id: "checkoutSummarySubtotal",
+          },
+          span(
+            {
+              class: " justify-start text-black text-base font-bold ",
+            },
+            "Subtotal"
+          ),
+          span(
+            {
+              class:
+                " text-right flex flex-col justify-start text-black text-base font-bold ",
+            },
+            strong(
+              {
+                class: "",
+              },
+              checkoutSummaryKeys.itemTotal
+            ),
+            strong(
+              {
+                class:
+                  "line-through decoration-danaherpurple-500 text-extralight font-normal",
+              },
+              checkoutSummaryKeys.undiscountedItemTotal !==
+                checkoutSummaryKeys.itemTotal
+                ? checkoutSummaryKeys.undiscountedItemTotal
+                : ""
+            )
+          )
+        ),
+        div(
+          {
+            class: "checkoutSummaryDiscount  flex justify-between w-full",
+            id: "checkoutSummaryDiscount",
+          },
+          span(
+            {
+              class:
+                " justify-start text-black text-base text-right font-extralight ",
+            },
+            "Discount"
+          ),
+          div(
+            {
+              class: " flex flex-col",
+            },
+            span(
+              {
+                class: "text-right text-black text-base font-extralight ",
+              },
+              checkoutSummaryKeys.discounts
+            ),
+            span(
+              {
+                class:
+                  " w-80 text-right  text-gray-500 text-xs font-normal leading-none",
+              },
+              "10% off first order"
+            )
+          )
+        ),
+        div(
+          {
+            class: "checkoutSummaryTax  flex justify-between w-full gap-4",
+            id: "checkoutSummaryTax",
+          },
+          div(
+            {
+              class: " flex justify-start items-start gap-4",
+            },
+            span(
+              {
+                class:
+                  "w-20 justify-start text-black text-base font-extralight ",
+              },
+              "Sales Tax*"
+            ),
+            span(
+              {
+                id: "checkoutSummaryTaxExempt",
+                class:
+                  "text-right text-violet-600 text-sm cursor-pointer text-danaherpurple-500 hover:text-danaherpurple-800 font-normal underline",
+              },
+              "Tax exempt?"
+            )
+          ),
+          span(
+            {
+              class:
+                " text-right justify-start text-black text-base font-extralight ",
+            },
+            checkoutSummaryKeys.tax
+          )
+        ),
+        div(
+          {
+            class:
+              "checkout-summary-shipping flex justify-between w-full gap-4",
+            id: "checkoutSummaryShipping",
+          },
+          span(
+            {
+              class: "w-20 justify-start text-black text-base font-extralight ",
+            },
+            "Shipping*"
+          ),
+          span(
+            {
+              class:
+                " text-right flex flex-col justify-start text-black text-base font-extralight ",
+            },
+            strong(
+              {
+                class: "",
+              },
+              checkoutSummaryKeys.shippingTotal
+            ),
+            strong(
+              {
+                class:
+                  "line-through decoration-danaherpurple-500 text-extralight font-normal",
+              },
+              checkoutSummaryKeys.undiscountedShippingTotal !==
+                checkoutSummaryKeys.shippingTotal
+                ? checkoutSummaryKeys.undiscountedShippingTotal
+                : ""
+            )
+          )
+        )
+      ),
+      div(
+        {
+          class:
+            "checkout-summary-total border-t justify-between flex w-full  border-gray-200 border-solid mb-4 pt-6",
+          id: "checkoutSummaryTotal",
+        },
+        span(
+          {
+            class: " justify-start text-black text-xl font-bold ",
+          },
+          `Total (${checkoutSummaryKeys["totalProductQuantity"]} items)`
+        ),
+        span(
+          {
+            class: " text-right justify-start text-black text-xl font-bold ",
+          },
+          checkoutSummaryKeys.total
+        )
+      )
+    ),
+    div(
+      {
+        class: " flex flex-col justify-center w-full items-start gap-4",
+      },
+      button(
+        {
+          class:
+            "proceed-button w-full text-white text-xl font-extralight btn btn-lg font-medium btn-primary-purple rounded-full px-6",
+          id: "proceed-button",
+          "data-tab": "shippingMethods",
+        },
+        "Proceed to Shipping"
+      ),
+      div(
+        {
+          class:
+            "w-full justify-start text-black-500 text-xs font-normal leading-none",
+        },
+        "*estimated sales tax. Additional tax may apply upon actual calculation of order"
+      )
+    )
+  );
+
+  // button to change steps when clicked on proceed or step icon.....
+  const proceedButton = summaryModule.querySelector("#proceed-button");
+  if (proceedButton) {
+    proceedButton.addEventListener("click", function (e) {
+      e.preventDefault();
+      changeStep(this);
+    });
+  }
+  const checkoutSummaryWrapper = summaryModule.querySelector(
+    "#checkoutSummaryWrapper"
+  );
+  if (checkoutSummaryWrapper) {
+    const getUseAddressesResponse = await getUseAddresses();
+
+    if (getUseAddressesResponse) {
+      if (
+        getUseAddressesResponse.data.invoiceToAddress &&
+        getUseAddressesResponse.data.invoiceToAddress.id !==
+          getUseAddressesResponse.data.commonShipToAddress.id
+      ) {
+        const invoiceToAddress = div(
+          {
+            id: "checkoutSummaryCommonBillToAddress",
+            class:
+              "flex-col w-full border-solid border-2 rounded border-gray-400 px-4",
+          },
+          div(
+            {
+              class: " flex flex-col pb-2",
+            },
+            h5(
+              {
+                class: "font-bold mb-2 mt-2",
+              },
+              "Bill to Address"
+            ),
+            h5(
+              {
+                class: "font-normal m-0",
+              },
+              getUseAddressesResponse.data.invoiceToAddress.companyName2
+            ),
+            p(
+              {
+                class: "text-black text-base font-extralight",
+              },
+              getUseAddressesResponse.data.invoiceToAddress.addressLine1
+            ),
+            p(
+              {
+                class: "text-black text-base font-extralight",
+              },
+              getUseAddressesResponse.data.invoiceToAddress.city
+            ),
+            p(
+              {
+                class: "text-black text-base font-extralight",
+              },
+              `${getUseAddressesResponse.data.invoiceToAddress.mainDivision}, ${getUseAddressesResponse.data.invoiceToAddress.countryCode}, ${getUseAddressesResponse.data.invoiceToAddress.postalCode}`
+            )
+          )
+        );
+        if (invoiceToAddress) {
+          checkoutSummaryWrapper.insertAdjacentElement(
+            "afterbegin",
+            invoiceToAddress
+          );
+        }
+      }
+      if (getUseAddressesResponse.data.commonShipToAddress) {
+        const commonShipToAddress = div(
+          {
+            id: "checkoutSummaryCommonShipAddress",
+            class:
+              "flex-col w-full border-solid border-2 rounded border-gray-400 px-4",
+          },
+          div(
+            {
+              class: " flex flex-col pb-2",
+            },
+            h5(
+              {
+                class: "font-bold mb-2 mt-2",
+              },
+              "Shipping Address"
+            ),
+            h5(
+              {
+                class: "font-normal m-0",
+              },
+              getUseAddressesResponse.data.commonShipToAddress.companyName2
+            ),
+            p(
+              {
+                class: "text-black text-base font-extralight",
+              },
+              getUseAddressesResponse.data.commonShipToAddress.addressLine1
+            ),
+            p(
+              {
+                class: "text-black text-base font-extralight",
+              },
+              getUseAddressesResponse.data.commonShipToAddress.city
+            ),
+            p(
+              {
+                class: "text-black text-base font-extralight",
+              },
+              `${getUseAddressesResponse.data.commonShipToAddress.mainDivision}, ${getUseAddressesResponse.data.commonShipToAddress.countryCode}, ${getUseAddressesResponse.data.commonShipToAddress.postalCode}`
+            )
+          )
+        );
+        if (commonShipToAddress) {
+          checkoutSummaryWrapper.insertAdjacentElement(
+            "afterbegin",
+            commonShipToAddress
+          );
+        }
+      }
+    }
+  }
+  const showShippingModalButton =
+    summaryModule.querySelector("#showShippingModal");
+  if (showShippingModalButton) {
+    showShippingModalButton.addEventListener("click", function (e) {
+      e.preventDefault();
+      const shippingFormModal = addressForm("", "shipping");
+      createModal(shippingFormModal, true, false);
+    });
+  }
+  if (summaryModule) {
+    const checkoutSummaryTaxExempt = summaryModule.querySelector(
+      "#checkoutSummaryTaxExempt"
+    );
+    if (checkoutSummaryTaxExempt) {
+      checkoutSummaryTaxExempt.addEventListener("click", function () {
+        const taxModal = taxExemptModal();
+        createModal(taxModal, false, true);
+      });
+    }
+  }
+  return summaryModule;
+};
