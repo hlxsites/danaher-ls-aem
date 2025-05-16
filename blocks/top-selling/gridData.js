bd5d036ada4092be81a97949b55e8a617dcaea98
SanyaKasaudhan1 <sanya.kasaudhan@dhlscontractors.com>
2025-05-16 19:51:19 +0530

    input

--- blocks/top-selling/gridData.js
+++ blocks/top-selling/gridData.js
@@ -57,7 +57,6 @@ export function renderGridCard(item) {
         type: "number",
         value: "1",
         min: "1",
-        step: "1",
         class: "w-14 self-stretch px-4 py-1.5 bg-white rounded-md shadow-sm outline outline-1 outline-offset-[-1px] outline-gray-300 text-black text-base font-normal font-['Inter'] leading-normal text-center",
       }),
       button(
