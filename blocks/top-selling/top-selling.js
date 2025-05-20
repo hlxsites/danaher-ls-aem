0cd93569b38d75db51f648c82abaf6d131e77785
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-05-20 18:09:39 +0530

    fixing top selling

--- blocks/top-selling/top-selling.js
+++ blocks/top-selling/top-selling.js
@@ -30,10 +30,10 @@ export default async function decorate(block) {
   }
 
   const headingText = block
-    .querySelector('[data-aue-prop="titleText"]')
+    .querySelector('[data-aue-prop="top_selling_titleText"]')
     ?.textContent.trim();
   const linkText = block
-    .querySelector('[data-aue-prop="card_hrefText"]')
+    .querySelector('[data-aue-prop="top_selling_hrefText"]')
     ?.textContent.trim();
   const rawIds =
     block.querySelector('[data-aue-prop="productid"]')?.textContent.trim() ||
