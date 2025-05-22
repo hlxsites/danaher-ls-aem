2b81e96ace6ceea8afcef0d9516c8594ab1b4e1d
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-05-22 21:07:07 +0530

    shop home dual carousel

--- blocks/dual-carousel/dual-carousel.js
+++ blocks/dual-carousel/dual-carousel.js
@@ -1,5 +1,65 @@
 import { div, p, img, a, span } from "../../scripts/dom-builder.js";
 
+function createCarousel(
+  side,
+  carouselTitle,
+  carouselProducts,
+  carouselLinkText
+) {
+  const carouselWrapper = div({
+    id: `${side}CarouselWrapper`,
+    class: "dualCarouselItem",
+  });
+  const carouselContent = div({ class: "dualCarouselContent" });
+  const carouselLeftArrow = span(
+    {
+      class:
+        "w-8 h-8 mr-2 border rounded-full flex items-center justify-center cursor-pointer transition opacity-50 pointer-events-none text-blue-600 border-blue-600",
+      title: "Scroll Left",
+    },
+    "←"
+  );
+
+  const carouselRightArrow = span(
+    {
+      class:
+        "w-8 h-8 border rounded-full flex items-center justify-center cursor-pointer transition text-blue-600 border-blue-600",
+      title: "Scroll Right",
+    },
+    "→"
+  );
+  const carouselTitleWrapper = div(
+    { class: "flex justify-between items-center mb-4" },
+    p({ class: "text-lg font-semibold text-gray-800" }, carouselTitle),
+    div({ class: "flex items-center" }, carouselLeftArrow, carouselRightArrow)
+  );
+  carouselProducts.forEach((product) => {
+    if (!product) return;
+    const { image, brand, title, url } = product;
+
+    const card = div(
+      {
+        class: "flex-shrink-0 bg-white border p-5 space-y-4 h-[360px] w-1/2",
+      },
+      img({ src: image, alt: title, class: "w-full h-32 object-contain" }),
+      p({ class: "text-xs font-bold text-purple-600" }, brand),
+      p({ class: "text-sm text-gray-900 font-normal leading-tight" }, title),
+      a(
+        {
+          href: url,
+          class: "text-purple-600 text-sm font-medium flex items-center gap-1",
+        },
+        carouselLinkText,
+        span({ class: "ml-1" }, "→")
+      )
+    );
+
+    carouselContent.appendChild(card);
+  });
+  carouselWrapper.append(carouselTitleWrapper, carouselContent);
+  return carouselWrapper;
+}
+
 export default async function decorate(block) {
   console.log("type of dual carousel block: ", typeof block);
   console.log("dual carousel block: ", block);
@@ -14,6 +74,10 @@ export default async function decorate(block) {
     .querySelector('[data-aue-prop="left_carousel_product_id"]')
     ?.textContent.trim()
     .split(",");
+  const leftCarouselLinkText =
+    block
+      .querySelector('[data-aue-prop="left_carousel_link_label"]')
+      ?.textContent.trim() || "Continue";
   const rightCarouselTitle = block
     .querySelector('[data-aue-prop="right_carousel_title"]')
     ?.textContent.trim();
@@ -21,6 +85,12 @@ export default async function decorate(block) {
     .querySelector('[data-aue-prop="right_carousel_product_id"]')
     ?.textContent.trim()
     .split(",");
+  const rightCarouselLinkText =
+    block
+      .querySelector('[data-aue-prop="right_carousel_link_label"]')
+      ?.textContent.trim() || "Continue";
+
+  Object.keys(block).forEach((key) => delete block[key]);
 
   const leftCarouselProducts = await Promise.all(
     leftCarouselProductIds.map(async (id) => {
@@ -54,79 +124,18 @@ export default async function decorate(block) {
   );
   console.log("leftCarouselProducts: ", leftCarouselProducts);
 
-  const leftCarouselContainer = div({
-    id: "leftCarouselContainer",
-    class:
-      "flex flex-wrap transition-all duration-300 ease-in-out space-x-4 bg-gray-100",
-    style: "transform: translateX(0);",
-  });
-
-  const leftCarouselLinkText =
-    block
-      .querySelector('[data-aue-prop="left_carousel_link_label"]')
-      ?.textContent.trim() || "Continue";
-  leftCarouselProducts.forEach((product) => {
-    if (!product) return;
-    const { image, brand, title, url } = product;
-
-    const card = div(
-      {
-        class: "flex-shrink-0 bg-white border p-5 space-y-4 h-[360px] w-1/2",
-      },
-      img({ src: image, alt: title, class: "w-full h-32 object-contain" }),
-      p({ class: "text-xs font-bold text-purple-600" }, brand),
-      p({ class: "text-sm text-gray-900 font-normal leading-tight" }, title),
-      a(
-        {
-          href: url,
-          class: "text-purple-600 text-sm font-medium flex items-center gap-1",
-        },
-        leftCarouselLinkText,
-        span({ class: "ml-1" }, "→")
-      )
-    );
-
-    leftCarouselContainer.appendChild(card);
-  });
   const leftCarouselScrollWrapper = div(
     {
       id: "leftCarouselScrollWrapper",
       class: "w-1/2 overflow-hidden flex flex-col",
     },
-    leftCarouselContainer
-  );
-  const leftCarouselLeftArrow = span(
-    {
-      class:
-        "w-8 h-8 mr-2 border rounded-full flex items-center justify-center cursor-pointer transition opacity-50 pointer-events-none text-blue-600 border-blue-600",
-      title: "Scroll Left",
-    },
-    "←"
-  );
-
-  const leftCarouselRightArrow = span(
-    {
-      class:
-        "w-8 h-8 border rounded-full flex items-center justify-center cursor-pointer transition text-blue-600 border-blue-600",
-      title: "Scroll Right",
-    },
-    "→"
-  );
-  const leftCarouselTitleWrapper = div(
-    { class: "flex justify-between items-center mb-4" },
-    p({ class: "text-lg font-semibold text-gray-800" }, leftCarouselTitle),
-    div(
-      { class: "flex items-center" },
-      leftCarouselLeftArrow,
-      leftCarouselRightArrow
+    createCarousel(
+      "left",
+      leftCarouselTitle,
+      leftCarouselProducts,
+      leftCarouselLinkText
     )
   );
-  leftCarouselScrollWrapper.append(
-    leftCarouselTitleWrapper,
-    leftCarouselContainer
-  );
   dualCarouselWrapper.append(leftCarouselScrollWrapper);
-  block.innerHtml = "";
   block.append(dualCarouselWrapper);
-  Object.keys(block).forEach((key) => delete block[key]);
 }
