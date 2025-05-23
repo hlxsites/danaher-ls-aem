bc0dd326d2a815b6f5c107c5f5ab48e3363d5f6e
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-05-23 18:22:37 +0530

    fising shop home

--- blocks/dual-carousel/dual-carousel.js
+++ blocks/dual-carousel/dual-carousel.js
@@ -48,7 +48,7 @@ function createCarousel(
           "flex-shrink-0 flex flex-col gap-3 bg-white border p-[12px] space-y-4 h-[360px] w-1/2 max-w-[48%]",
       },
       img({ src: image, alt: title, class: "w-full h-40 object-contain" }),
-      p({ class: "text-xs font-bold text-purple-600" }, brand),
+      p({ class: "text-xs font-semibold text-purple-600" }, brand),
       p({ class: "text-sm text-gray-900 font-normal leading-tight" }, title),
       a(
         {
