27af518a51f433befb93d17a8b8a42c466b78150
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-05-27 12:38:55 +0530

    shob

--- scripts/common-utils.js
+++ scripts/common-utils.js
@@ -118,7 +118,7 @@ export async function getProductInfo(id) {
   if (api) {
     try {
       const res1 = await getApiData(
-        `https://sstage.lifesciences.danaher.com/us/en/product-data/productInf/?product=${id}`
+        `https://stage.lifesciences.danaher.com/us/en/product-data/productInfo/?product=${id}`
       );
 
       if (res1?.status === "success") {
