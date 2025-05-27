efae7b0a843fafff0c628ce0cdcc1cdbcdced6a6
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-05-27 18:52:39 +0530

    shob

--- blocks/shop-featured-products/shop-featured-products.js
+++ blocks/shop-featured-products/shop-featured-products.js
@@ -8,7 +8,6 @@ import {
   h2,
   span,
 } from "../../scripts/dom-builder.js";
-import { decorateIcons } from "../../scripts/lib-franklin.js";
 export default function decorate(block) {
   const sectionHeading =
     block
@@ -73,8 +72,8 @@ export default function decorate(block) {
   let setItemsPerPage = 1;
   const prevEnabled = setControls ? currentIndex > 0 : currentPage > 1;
   const nextEnabled = setControls
-    ? currentIndex + setItemsPerPage < slides.length
-    : currentPage < Math.ceil(slides.length / setItemsPerPage);
+    ? currentIndex + setItemsPerPage < items.length
+    : currentPage < Math.ceil(items.length / setItemsPerPage);
   prevDiv.innerHTML = `
       <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="none">
         <path d="M18.3333 25L13.3333 20M13.3333 20L18.3333 15M13.3333 20L26.6667 20M5 20C5 11.7157 11.7157 5 20 5C28.2843 5 35 11.7157 35 20C35 28.2843 28.2843 35 20 35C11.7157 35 5 28.2843 5 20Z"
