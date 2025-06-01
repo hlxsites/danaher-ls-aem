992a7d0074dbd03eca42c6191200a6ee40d49852
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-06-01 13:52:24 +0530

    adding separator

--- blocks/separator-line/separator-line.js
+++ blocks/separator-line/separator-line.js
@@ -22,6 +22,11 @@ export default function decorate(block) {
     // Text Block
     div({ class: "flex flex-col items-start max-w-3xl" })
   );
-  block.innerHTML = "";
   block.appendChild(bannerSection);
+  // Hide authored AEM content
+  [...block.children].forEach((child) => {
+    if (!child.contains(container)) {
+      child.style.display = "none";
+    }
+  });
 }
