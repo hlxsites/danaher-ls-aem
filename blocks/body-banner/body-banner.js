4740e71938b18c70db7292c10ec4ac3e775ef636
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-06-01 20:16:55 +0530

    fixing multiple

--- blocks/body-banner/body-banner.js
+++ blocks/body-banner/body-banner.js
@@ -54,12 +54,12 @@ export default function decorate(block) {
   // === Right Text Section ===
   const rightSection = div(
     {
-      class: "flex md:w-1/2 justify-center items-center",
-      style: `background-color: ${rightColor}; padding: 83.667px 32px 83.563px 32px;`,
+      class: "flex md:w-1/2 justify-center items-center p-8 min-h-[413px]",
+      style: `background-color: ${rightColor};`,
     },
     div(
       {
-        class: "flex flex-col gap-6",
+        class: "flex flex-col gap-4",
       },
       p(
         {
