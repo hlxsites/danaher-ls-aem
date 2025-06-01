6d3b532c7505801db2dcfc2cf91336703ec28f31
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-06-01 21:17:19 +0530

    fixing lint

--- blocks/card-list/articleCard.js
+++ blocks/card-list/articleCard.js
@@ -2,74 +2,74 @@ import {
   formatDateUTCSeconds,
   imageHelper,
   makePublicUrl,
-} from "../../scripts/scripts.js";
-import { li, a, p, div, time, span, h3 } from "../../scripts/dom-builder.js";
-import { decorateIcons, getMetadata } from "../../scripts/lib-franklin.js";
+} from '../../scripts/scripts.js';
+import {
+  li, a, p, div, time, span, h3,
+} from '../../scripts/dom-builder.js';
+import { decorateIcons, getMetadata } from '../../scripts/lib-franklin.js';
 
-const template = getMetadata("template");
-let linkText = "";
+const template = getMetadata('template');
+let linkText = '';
 switch (template) {
-  case "new-lab":
-    linkText = "Unlock All Offers";
+  case 'new-lab':
+    linkText = 'Unlock All Offers';
     break;
-  case "wsaw":
-    linkText = "Learn More";
+  case 'wsaw':
+    linkText = 'Learn More';
     break;
   default:
-    linkText = "Read Article";
+    linkText = 'Read Article';
 }
 
 export default function createCard(article, firstCard = false) {
-  const cardTitle =
-    article.title.split("| Danaher Life Sciences")[0] || article.title;
+  const cardTitle = article.title.split('| Danaher Life Sciences')[0] || article.title;
 
   const cardWrapper = a(
     {
-      class: "group h-full",
+      class: 'group h-full',
       href: makePublicUrl(article.path),
       title: article.title,
     },
     imageHelper(article.image, article.title, firstCard),
     div(
-      { class: "" },
-      p({ class: "eyebrow-sm" }, article.brand || "Danaher Corporation"),
+      { class: '' },
+      p({ class: 'eyebrow-sm' }, article.brand || 'Danaher Corporation'),
       h3(
         {
           class:
-            "text-black font-medium mb-4 mt-4 line-clamp-3 break-words !h-24",
+            'text-black font-medium mb-4 mt-4 line-clamp-3 break-words !h-24',
         },
-        cardTitle
+        cardTitle,
       ),
       div(
         {
           class:
-            "mt-auto inline-flex items-center w-full hover:underline py-5 text-base text-danaherpurple-500 font-semibold",
+            'mt-auto inline-flex items-center w-full hover:underline py-5 text-base text-danaherpurple-500 font-semibold',
         },
         linkText,
         span({
           class:
-            "icon icon-arrow-right dhls-arrow-right-icon fill-current [&_svg>use]:stroke-danaherpurple-500 [&_svg>use]:hover:stroke-danaherpurple-800",
-        })
-      )
-    )
+            'icon icon-arrow-right dhls-arrow-right-icon fill-current [&_svg>use]:stroke-danaherpurple-500 [&_svg>use]:hover:stroke-danaherpurple-800',
+        }),
+      ),
+    ),
   );
   decorateIcons(cardWrapper);
   const showDateTime = p(
-    { class: "text-base text-gray-500 font-extralight" },
+    { class: 'text-base text-gray-500 font-extralight' },
     time(
       { datetime: formatDateUTCSeconds(article.publishDate) },
-      formatDateUTCSeconds(article.publishDate, { month: "long" })
+      formatDateUTCSeconds(article.publishDate, { month: 'long' }),
     ),
-    span({ class: "pl-2" }, `${article.readingTime} min read`)
+    span({ class: 'pl-2' }, `${article.readingTime} min read`),
   );
-  if (template !== "wsaw")
-    cardWrapper.querySelector(".eyebrow-sm")?.after(showDateTime);
+  if (template !== 'wsaw') cardWrapper.querySelector('.eyebrow-sm')?.after(showDateTime);
 
   return li(
     {
       class:
-        "w-full h-full article flex flex-col col-span-1 relative mx-auto justify-center overflow-hidden bg-white transform transition duration-500 hover:scale-105",
+        'w-full h-full article flex flex-col col-span-1 relative mx-auto justify-center overflow-hidden bg-white transform transition duration-500 hover:scale-105',
     },
-    cardWrapper
+    cardWrapper,
   );
 }
