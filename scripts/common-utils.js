843e59f83cf0573d2f1b1b7bcd52b92365400275
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-06-03 12:04:04 +0530

    fixing str

--- scripts/common-utils.js
+++ scripts/common-utils.js
@@ -25,7 +25,7 @@ export function capitalizeFirstLetter(str) {
   console.log("capitalize str: ", str);
 
   if (!str) return str;
-  return str.charAt(0).toUpperCase() + str.slice(1);
+  return str?.charAt(0).toUpperCase() + str?.slice(1);
 }
 /*
  ::::::::::::::::::::
