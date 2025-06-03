441a883c405f5ec18cab31e1842908a3489fd61d
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-06-03 14:36:25 +0530

    fix

--- blocks/checkout/shippingAddress.js
+++ blocks/checkout/shippingAddress.js
@@ -776,6 +776,11 @@ export const shippingAddressModule = async () => {
       c.preventDefault();
       setTimeout(function () {
         console.log("checkbox action", c.target.checked);
+        if (c.target.checked) {
+          c.target.checked = false;
+        } else {
+          c.target.checked = true;
+        }
       }, 0);
     });
     shippingAsBillingAddressInput?.addEventListener("change", async (c) => {
