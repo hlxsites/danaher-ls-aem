a529268ae7b0a5f75f83a37e61d51cfbe6126a9a
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-06-04 13:20:18 +0530

    fixing opco banner

--- blocks/opco-banner/opco-banner.js
+++ blocks/opco-banner/opco-banner.js
@@ -110,7 +110,7 @@ export default function decorate(block) {
       div(
         {
           class:
-            "text-[16px] leading-[22px] font-medium font-primary text-black w-full",
+            "text-[16px] leading-[22px] font-medium font-extralight text-black w-full",
         },
         opcoBannerDescription.textContent.trim()
       )
