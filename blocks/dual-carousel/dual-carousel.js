44c21cc64d36b83bfd802be9a24a89deb513dd60
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-06-06 17:45:59 +0530

    updating skus

--- blocks/dual-carousel/dual-carousel.js
+++ blocks/dual-carousel/dual-carousel.js
@@ -5,7 +5,7 @@ import {
 } from "../../scripts/common-utils.js";
 import { decorateIcons } from "../../scripts/lib-franklin.js";
 
-async function createCarousel(
+function createCarousel(
   side,
   carouselTitle,
   carouselProducts,
@@ -49,7 +49,7 @@ async function createCarousel(
   );
   decorateIcons(carouselTitleWrapper);
 
-  const productsList = await carouselProducts;
+  const productsList = carouselProducts;
   console.log("carouselProducts : ", carouselProducts);
   console.log("productsList : ", productsList);
 
@@ -207,24 +207,16 @@ export default async function decorate(block) {
   block.textContent = "";
   Object.keys(block).forEach((key) => delete block[key]);
 
-  let leftCarouselProducts = (
-    await Promise.allSettled(
-      leftCarouselProductIds.map(
-        async (sku) => await getProductInfo(sku, false)
-      )
-    )
+  let leftCarouselProducts = Promise.allSettled(
+    leftCarouselProductIds.map((sku) => getProductInfo(sku, false))
   ).filter((product) => product.status !== "error");
 
   if (leftCarouselProducts.length === 0) {
     leftCarouselProducts = renderProductJsonResponse(10);
   }
 
-  let rightCarouselProducts = (
-    await Promise.allSettled(
-      rightCarouselProductIds.map(
-        async (sku) => await getProductInfo(sku, false)
-      )
-    )
+  let rightCarouselProducts = Promise.allSettled(
+    rightCarouselProductIds.map((sku) => getProductInfo(sku, false))
   ).filter((product) => product.status !== "error");
 
   if (rightCarouselProducts.length === 0) {
