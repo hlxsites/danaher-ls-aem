937abec45f76ea8814a10fb4ffc918ec6c4664e1
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-06-09 19:30:15 +0530

    fixing ui

--- blocks/opco-grid/opco-grid.js
+++ blocks/opco-grid/opco-grid.js
@@ -44,10 +44,11 @@ export default function decorate(block) {
     const cardWrapper = readMoreLink
       ? a({ href: makePublicUrl(readMoreLink.href), title: readMoreLink.title })
       : div();
+    console.log("row: ", row);
 
     cardWrapper.className =
       "opco-grid-wrapper w-[294px] flex flex-col col-span-1 mx-auto justify-center max-w-xl overflow-hidden p-0 border-l-[0.5px] border-gray-300 transform transition duration-500 hover:scale-105";
-    cardWrapper?.parentElement?.classList.add("w-[294px]");
+    row?.classList.add("w-[294px]");
     // if (!block.classList.contains("opco"))
     //   cardWrapper.classList.remove(
     //     ..."border-l-[0.5px]
