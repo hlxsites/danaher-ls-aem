f4c0cdc13269100b12f93c012e0096a68bfa8422
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-06-09 19:24:12 +0530

    fixing lint

--- blocks/opco-grid/opco-grid.js
+++ blocks/opco-grid/opco-grid.js
@@ -46,7 +46,7 @@ export default function decorate(block) {
       : div();
 
     cardWrapper.className =
-      "opco-grid-wrapper flex flex-col col-span-1 mx-auto justify-center max-w-xl overflow-hidden p-0 border-l-[0.5px] border-gray-300 transform transition duration-500 hover:scale-105";
+      "opco-grid-wrapper w-[294px] flex flex-col col-span-1 mx-auto justify-center max-w-xl overflow-hidden p-0 border-l-[0.5px] border-gray-300 transform transition duration-500 hover:scale-105";
     // if (!block.classList.contains("opco"))
     //   cardWrapper.classList.remove(
     //     ..."border-l-[0.5px]
