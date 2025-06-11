83b3d251b78f452f833228f8023a895d96735e1b
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-06-11 19:05:38 +0530

    login

--- scripts/common-utils.js
+++ scripts/common-utils.js
@@ -790,8 +790,7 @@ export const buildInputElement = (
     label(
       {
         for: fieldLable,
-        class:
-          "self-stretch p-3 bg-white shadow-sm outline outline-1 outline-offset-[-1px] outline-gray-700 inline-flex justify-start items-center overflow-hidden",
+        class: "font-normal text-sm leading-4 ",
       },
       field,
       dataRequired
@@ -804,7 +803,7 @@ export const buildInputElement = (
       autocomplete: autoCmplte,
       "data-required": required,
       class:
-        "input-focus text-base w-full block text-gray-600 font-extralight border border-solid border-gray-300 rounded-md px-3 py-2",
+        "self-stretch p-3 bg-white shadow-sm outline outline-1 outline-offset-[-1px] outline-gray-700 inline-flex justify-start items-center overflow-hidden",
       "aria-label": dtName,
     }),
     span({
