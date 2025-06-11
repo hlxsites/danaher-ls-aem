8d222f45b3206dd7e0c2dba6bad6160b34eb5ff3
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-06-11 15:43:30 +0530

    loign

--- blocks/login/login.js
+++ blocks/login/login.js
@@ -10,10 +10,9 @@ import {
 export default async function decorate(block) {
   block?.parentElement?.parentElement?.removeAttribute("class");
   block?.parentElement?.parentElement?.removeAttribute("style");
-  document.querySelector("header")?.classList.add("hidden");
-  document.querySelector("breadcrumb")?.classList.add("hidden");
+  document.querySelector("header")?.remove();
   document.querySelector("breadcrumb")?.remove();
-  document.querySelector("footer")?.classList.add("hidden");
+  document.querySelector("footer")?.remove();
   console.log("login block: ", block);
 
   const loginHeading = block.querySelector("[data-aue-prop='loginHeading']");
