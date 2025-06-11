6b1b1199e8b80219b04d85169070336c63eb3938
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-06-11 14:10:22 +0530

    login page

--- blocks/login/login.js
+++ blocks/login/login.js
@@ -1,32 +1,32 @@
-import { div, button } from '../../scripts/dom-builder.js';
-import { userLogin } from '../../scripts/auth-utils.js';
-import { removePreLoader, showPreLoader } from '../../scripts/common-utils.js';
+import { div, button } from "../../scripts/dom-builder.js";
+import { userLogin } from "../../scripts/auth-utils.js";
+import { removePreLoader, showPreLoader } from "../../scripts/common-utils.js";
 
 export default async function decorate(block) {
-  block?.parentElement?.parentElement?.removeAttribute('class');
-  block?.parentElement?.parentElement?.removeAttribute('style');
+  block?.parentElement?.parentElement?.removeAttribute("class");
+  block?.parentElement?.parentElement?.removeAttribute("style");
   const loginWrapper = div({
     class:
-      'dhls-container px-5 lg:px-10 dhlsBp:p-0 mb-12 bg-danaher-purple-100 flex items-center justify-center',
+      "dhls-container px-5 lg:px-10 dhlsBp:p-0 mb-12 bg-danaher-purple-100 flex items-center justify-center",
   });
   const loginButton = button(
     {
-      class: ' w-[100px]',
-      id: 'tempLoginButton',
+      class: " w-[100px]",
+      id: "tempLoginButton",
     },
-    'Login',
+    "Login"
   );
-  loginButton.addEventListener('click', async (event) => {
+  loginButton.addEventListener("click", async (event) => {
     event.preventDefault();
     showPreLoader();
-    const loginResponse = await userLogin('customer');
-    if (loginResponse && loginResponse.status !== 'error') {
+    const loginResponse = await userLogin("customer");
+    if (loginResponse && loginResponse.status !== "error") {
       removePreLoader();
       return true;
     }
     return false;
   });
   loginWrapper.append(loginButton);
-  block.innerHtml = '';
+  block.innerHtml = "";
   block.append(loginWrapper);
 }
