30768965cf735b2dc81b273891bfb3ad62913992
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-06-16 14:00:12 +0530

    dashboard

--- blocks/dashboard/dashboard.js
+++ blocks/dashboard/dashboard.js
@@ -1,20 +1,22 @@
-import { div } from "../../scripts/dom-builder.js";
+import { div, h1 } from "../../scripts/dom-builder.js";
 
 export default function decorate(block) {
   block?.parentElement?.parentElement?.removeAttribute("class");
   block?.parentElement?.parentElement?.removeAttribute("style");
   const dashboardTitle = block.querySelector(
-    '[data-aue-prop="dashboardTitle"]'
+    "[data-aue-prop='dashboardTitle']"
   );
 
   const wrapper = div({
     class:
       "flex flex-col bg-gray-100 md:flex-row w-full dhls-container lg:px-10 dhlsBp:p-0 items-center",
   });
-  const content = div({
-    class: "bg-white w-full md:w-[80%]",
-  });
-  content.append(dashboardTitle);
+  const content = div(
+    {
+      class: "bg-white w-full md:w-[80%]",
+    },
+    h1({}, dashboardTitle ?? "")
+  );
   const sidebar = div({
     class: "bg-white w-full md:w-[20%] flex flex-col",
   });
