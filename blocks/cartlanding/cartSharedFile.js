ff5ad779eef24ced9b4e1a5c83c29a5911b36749
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-06-19 12:01:01 +0530

    guest user

--- blocks/cartlanding/cartSharedFile.js
+++ blocks/cartlanding/cartSharedFile.js
@@ -5,7 +5,6 @@ import {
   getApiData,
 } from "../../scripts/api-utils.js";
 import { getAuthenticationToken } from "../../scripts/token-utils.js";
-import { userLogin } from "../../scripts/auth-utils.js";
 
 /*
 :::::::::::::::::::::::::::
@@ -15,10 +14,9 @@ Function to update current basket details
 export async function updateBasketDetails() {
   const authenticationToken = await getAuthenticationToken();
   if (authenticationToken?.status === "error") {
-    await userLogin("guest");
     // window.location.href =
     //   "/us/en/eds-stage-test/login.html?ref=feature-cart-checkout-summary";
-    // return { status: 'error', data: 'Unauthorized access.' };
+    return { status: "error", data: "Unauthorized access." };
   }
   const defaultHeader = new Headers({
     "Content-Type": "Application/json",
