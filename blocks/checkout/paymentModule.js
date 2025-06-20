35f6447cae3b28f581dc656c81ef72083eca5116
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-06-20 17:58:38 +0530

    payment methods

--- blocks/checkout/paymentModule.js
+++ blocks/checkout/paymentModule.js
@@ -1,6 +1,4 @@
-import {
-  h2, h5, div, p,
-} from '../../scripts/dom-builder.js';
+import { h2, h5, div, p, img, input } from "../../scripts/dom-builder.js";
 
 /*
  :::::::::::::::
@@ -17,25 +15,44 @@ const paymentModule = async () => {
     const moduleContent = div({});
     const moduleHeader = div(
       {
-        class: 'border-b relative flex flex-col pt-6 pb-6 mb-4',
+        class: "border-b relative flex flex-col pt-6 pb-6 mb-4",
       },
-      h2({}, 'Choose your payment method'),
+      h2({}, "Choose your payment method"),
       p(
         {},
-        'Simplify your logistics by shipping with our trusted carrier. Enjoy competitive rates, real-time tracking, and reliable delivery for all your products. Let us handle the shipping while you focus on your business.',
-      ),
+        "Simplify your logistics by shipping with our trusted carrier. Enjoy competitive rates, real-time tracking, and reliable delivery for all your products. Let us handle the shipping while you focus on your business."
+      )
     );
 
     const paymentMethodsWrapper = div({
-      id: 'paymentMethodsWrapper',
-      class: 'border-solid flex flex-col border-2 border-gray-300 w-full',
+      id: "paymentMethodsWrapper",
+      class: "border-solid flex flex-col border-2 border-gray-300 w-full",
     });
+    const paymentMethods = div(
+      {
+        class: "border-2 border-gray-300 flex flex-col",
+      },
+      div(
+        {
+          class: "flex px-4 py-3 w-full justify-between",
+        },
+        input({
+          name: `creditCard`,
+          type: "radio",
+          id: `creditCard`,
+        }),
+        img({
+          src: "icons/creditCards.png",
+          class: "w-[176px]",
+        })
+      )
+    );
 
     moduleContent?.append(moduleHeader, paymentMethodsWrapper);
 
     return moduleContent;
   } catch (error) {
-    return div(h5({ class: 'text-red' }, 'Error Loading Payment Module.'));
+    return div(h5({ class: "text-red" }, "Error Loading Payment Module."));
   }
 };
 
