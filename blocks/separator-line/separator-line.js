bc5fc851f709816597d10e6a13c629010f2e8691
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-06-20 17:12:13 +0530

    separator

--- blocks/separator-line/separator-line.js
+++ blocks/separator-line/separator-line.js
@@ -3,19 +3,18 @@ import { div } from "../../scripts/dom-builder.js";
 export default function decorate(block) {
   block?.parentElement?.parentElement?.removeAttribute("class");
   block?.parentElement?.parentElement?.removeAttribute("style");
-  const bgColorContainer = block.querySelector('[data-aue-prop="bg-color"]');
+  const bgColorContainer = block.querySelector("a");
   const separatorHeight = block.querySelector(
     '[data-aue-prop="separatorHeight"]'
   );
-  console.log(" block: ", block);
 
-  //   const bgColor = bgColorContainer?.textContent?.trim() || "#D1D5DB";
-  //   const height = separatorHeight?.textContent?.trim() || "1";
+  const bgColor = bgColorContainer?.textContent?.trim() || "#D1D5DB";
+  const height = separatorHeight?.textContent?.trim() || "1";
 
-  //   const separatorSection = div({
-  //     style: `background-color: ${bgColor};height: ${height}px`,
-  //     class: " dhls-container px-5 lg:px-10 dhlsBp:p-0 ",
-  //   });
-  // //  block.innerHtml = "";
-  //   block.append(separatorSection);
+  const separatorSection = div({
+    style: `background-color: ${bgColor};height: ${height}px`,
+    class: " dhls-container px-5 lg:px-10 dhlsBp:p-0 ",
+  });
+  //  block.innerHtml = "";
+  block.append(separatorSection);
 }
