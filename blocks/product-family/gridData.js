54269453654c85fec69a59250cd6a42b8f58ac42
SanyaKasaudhan1 <sanya.kasaudhan@dhlscontractors.com>
2025-06-24 12:59:22 +0530

    bug fixes for product -family

--- blocks/product-family/gridData.js
+++ blocks/product-family/gridData.js
@@ -10,7 +10,7 @@ import { createModal } from '../../scripts/common-utils.js';
 export default function renderProductGridCard(item) {
   const card = div({
     class:
-      'lg:w-[294px] w-[331px] min-h-80 bg-white outline outline-1 outline-gray-300 flex flex-col justify-start items-start',
+      'lg:w-[305px] w-[331px] min-h-80 bg-white outline outline-1 outline-gray-300 flex flex-col justify-start items-start',
   });
 
   const imageElement = imageHelper(item.raw.images?.[0] || '', item.title, {
