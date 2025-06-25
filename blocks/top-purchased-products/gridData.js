be2dc1643d07e07f4e78ccdbf72deb60e2b6cd68
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-06-25 18:10:49 +0530

    fixing images

--- blocks/top-purchased-products/gridData.js
+++ blocks/top-purchased-products/gridData.js
@@ -16,7 +16,7 @@ export default function renderGridCard(item) {
     img({
       src: imageUrl,
       alt: item?.title.replace(/<[^>]*>/g, "") || "",
-      class: "w-full h-[164px] p-4 object-contain",
+      class: "w-full h-[164px] p-0 object-contain",
     }),
     p(
       {
