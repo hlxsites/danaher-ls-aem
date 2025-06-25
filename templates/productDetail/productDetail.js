af34cca4d9bf723a78882d33c57b22c914bab468
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-06-25 10:22:03 +0530

    working on target to same or new tab

--- templates/productDetail/productDetail.js
+++ templates/productDetail/productDetail.js
@@ -1,48 +1,42 @@
-import {
-  div, ul, li, a,
-} from '../../scripts/dom-builder.js';
-import { buildBlock } from '../../scripts/lib-franklin.js';
-import { getProductResponse } from '../../scripts/commerce.js';
-import { buildProductSchema } from '../../scripts/schema.js';
+import { div, ul, li, a } from "../../scripts/dom-builder.js";
+import { buildBlock } from "../../scripts/lib-franklin.js";
+import { getProductResponse } from "../../scripts/commerce.js";
+import { buildProductSchema } from "../../scripts/schema.js";
 
 function addProductBreadCrumb(response, breadcrumbEl) {
   const clickUrl = new URL(response.at(0)?.ClickUri);
-  const ulEl = breadcrumbEl.querySelector('ul');
+  const ulEl = breadcrumbEl.querySelector("ul");
   const liEl = li(a({ href: clickUrl.pathname }, response.at(0)?.Title));
   ulEl.append(liEl);
 }
 
 export default async function buildAutoBlocks() {
   const response = await getProductResponse();
-  const productHeroEl = document.querySelector('main > div > div.product-hero');
-  const breadcrumb = document.querySelector('main > div > div.breadcrumb');
+  const productHeroEl = document.querySelector("main > div > div.product-hero");
+  const breadcrumb = document.querySelector("main > div > div.breadcrumb");
   if (productHeroEl) {
     let breadcrumbBlock = breadcrumb;
     if (!breadcrumbBlock) {
       const breadcrumbEl = div(
-        { class: 'breadcrumb' },
-        div(
-          div(
-            ul(
-              li(
-                a({ href: '/us/en/products' }, 'Products'),
-              ),
-            ),
-          ),
-        ),
+        { class: "breadcrumb" },
+        div(div(ul(li(a({ href: "/us/en/products" }, "Products")))))
       );
       if (response) {
         addProductBreadCrumb(response, breadcrumbEl);
       }
-      breadcrumbBlock = buildBlock('breadcrumb', { elems: [breadcrumbEl] });
+      breadcrumbBlock = buildBlock("breadcrumb", { elems: [breadcrumbEl] });
     }
-    const breadcrumbSection = breadcrumbBlock.parentElement || div(breadcrumbBlock);
-    productHeroEl.parentElement.insertAdjacentElement('afterend', breadcrumbSection);
+    const breadcrumbSection =
+      breadcrumbBlock.parentElement || div(breadcrumbBlock);
+    productHeroEl.parentElement.insertAdjacentElement(
+      "afterend",
+      breadcrumbSection
+    );
   }
 
   buildProductSchema();
 
   // build page tabs
-  const pageTabsBlock = div(buildBlock('page-tabs', { elems: [] }));
-  productHeroEl?.parentElement.insertAdjacentElement('afterend', pageTabsBlock);
+  const pageTabsBlock = div(buildBlock("page-tabs", { elems: [] }));
+  productHeroEl?.parentElement.insertAdjacentElement("afterend", pageTabsBlock);
 }
