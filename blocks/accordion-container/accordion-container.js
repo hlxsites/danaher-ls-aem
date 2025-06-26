e89658dd7ac9c8213e318343f4487c6247b2f7a2
Rajesh Chithaluri <rajesh.chithaluri@gspann.com>
2025-06-26 18:44:36 +0530

    updated the logic

--- blocks/accordion-container/accordion-container.js
+++ blocks/accordion-container/accordion-container.js
@@ -92,9 +92,16 @@ function createAccordionBlock(
   );
 
   answer.forEach((element) => {
-    element.querySelectorAll('a')?.forEach(aEle => {
-      aEle.classList.add(...'text-black decoration-black hover:decoration-danaherpurple-500 hover:bg-danaherpurple-25 hover:text-danaherpurple-500'.split(' '))
-    })
+    if (element?.querySelectorAll) {
+      const aTags = element.querySelectorAll('a');
+      if (aTags.length > 0) {
+        aTags.forEach((aEle) => {
+          aEle.classList.add(
+            ...'text-black decoration-black hover:decoration-danaherpurple-500 hover:bg-danaherpurple-25 hover:text-danaherpurple-500'.split(' '),
+          );
+        });
+      }
+    }
     panel.querySelector('.accordion-answer').innerHTML += element;
   });
 
