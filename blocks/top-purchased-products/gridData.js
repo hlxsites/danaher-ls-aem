9d468395bf6b85eb026ec228c4d761c98aca9a7d
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-06-27 10:19:41 +0530

    fixing links

--- blocks/top-purchased-products/gridData.js
+++ blocks/top-purchased-products/gridData.js
@@ -1,8 +1,6 @@
-import {
-  div, p, span, img, a,
-} from '../../scripts/dom-builder.js';
+import { div, p, span, img, a } from "../../scripts/dom-builder.js";
 
-import { decorateIcons } from '../../scripts/lib-franklin.js';
+import { decorateIcons } from "../../scripts/lib-franklin.js";
 /**
  * Renders a product card in grid view.
  * @param {Object} item - Product data containing title, url, images, description, price, etc.
@@ -13,53 +11,52 @@ export default function renderGridCard(item) {
   const card = div(
     {
       class:
-        'w-full hover:shadow-md  cursor-pointer min-w-[264px] sm:w-[calc(50%-10px)] lg:w-[calc(25%-15px)] min-h-80 bg-white border border-gray-300  flex flex-col justify-start items-start gap-3  transform transition duration-500 hover:scale-105',
-      onclick: () => window.open(
-        item?.url,
-        item?.url?.includes('http') ? '_blank' : '_self',
-      ),
+        "w-full hover:shadow-md  cursor-pointer min-w-[264px] sm:w-[calc(50%-10px)] lg:w-[calc(25%-15px)] min-h-80 bg-white border border-gray-300  flex flex-col justify-start items-start gap-3  transform transition duration-500 hover:scale-105",
+      onclick: () =>
+        window.open(
+          item?.url,
+          item?.url?.includes("http") ? "_blank" : "_self"
+        ),
     },
     img({
       src: imageUrl,
-      alt: item?.title.replace(/<[^>]*>/g, '') || '',
-      class: 'w-full h-[164px] p-0 object-contain',
+      alt: item?.title.replace(/<[^>]*>/g, "") || "",
+      class: "w-full h-[164px] p-0 object-contain",
     }),
     p(
       {
         class:
-          'text-sm font-medium text-danaherpurple-800  !px-3  leading-tight',
+          "text-sm font-medium text-danaherpurple-800  !px-3  leading-tight",
       },
-      item?.brand.replace(/<[^>]*>/g, '') || '',
+      item?.brand.replace(/<[^>]*>/g, "") || ""
     ),
     p(
       {
         class:
-          'text-xl !m-0 !p-0  !px-3  text-black flex-grow font-medium leading-7 !line-clamp-3 !break-words',
+          "text-xl !m-0 !p-0  !px-3  text-black flex-grow font-medium leading-7 !line-clamp-3 !break-words",
       },
-      item.title.replace(/<[^>]*>/g, '') || '',
+      item.title.replace(/<[^>]*>/g, "") || ""
     ),
-    a(
+    span(
       {
-        href: item?.url || '#',
-        target: item?.url?.includes('http') ? '_blank' : '_self',
         class:
-          'text-danaherpurple-500  [&_svg>use]:hover:stroke-danaherpurple-800 hover:text-danaherpurple-800 !px-3  self-stretch px-3 pb-3 flex justify-start items-center text-base font-bold leading-snug flex items-center',
+          "text-danaherpurple-500  [&_svg>use]:hover:stroke-danaherpurple-800 hover:text-danaherpurple-800 !px-3  self-stretch px-3 pb-3 flex justify-start items-center text-base font-bold leading-snug flex items-center",
       },
-      'View Details',
+      "View Details",
       span({
         class:
-          'icon icon-arrow-right dhls-arrow-right-icon fill-current [&_svg>use]:stroke-danaherpurple-500 [&_svg>use]:hover:stroke-danaherpurple-800',
-      }),
-    ),
+          "icon icon-arrow-right dhls-arrow-right-icon fill-current [&_svg>use]:stroke-danaherpurple-500 [&_svg>use]:hover:stroke-danaherpurple-800",
+      })
+    )
   );
   decorateIcons(card);
 
-  const imgElement = card.querySelector('img');
+  const imgElement = card.querySelector("img");
   if (imgElement) {
     imgElement.onerror = () => {
-      if (!imgElement.getAttribute('data-fallback-applied')) {
-        imgElement.src = '/content/dam/danaher/system/icons/preview-image.png';
-        imgElement.setAttribute('data-fallback-applied', 'true');
+      if (!imgElement.getAttribute("data-fallback-applied")) {
+        imgElement.src = "/content/dam/danaher/system/icons/preview-image.png";
+        imgElement.setAttribute("data-fallback-applied", "true");
       }
     };
   }
