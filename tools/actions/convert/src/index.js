c984b26f7dd4dac7db65e7e28aabbe3892b681cd
Rajesh Chithaluri <rajesh.chithaluri@gspann.com>
2025-06-27 16:06:00 +0530

    pushing the converter change

--- tools/actions/convert/src/index.js
+++ tools/actions/convert/src/index.js
@@ -183,6 +183,7 @@ function skipConverter(path) {
   if (!path) return false;
   if (path.includes('.json')) return true;
   if (path.includes('/us/en/products/brands/leica')) return true;
+  
   // if (path.includes('/us/en/blog/')) return true;
   // if (path.includes('/us/en/news/')) return true;
   // skip the converter for pages like **/products/*/topics/**
