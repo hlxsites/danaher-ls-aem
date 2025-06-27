ff2996762e725c6fd97f4c2007d708b332b03a8b
Rajesh Chithaluri <rajesh.chithaluri@gspann.com>
2025-06-27 19:08:11 +0530

    Removed the fullCategory on the all

--- blocks/product-categories/product-categories.js
+++ blocks/product-categories/product-categories.js
@@ -96,7 +96,7 @@ export default async function decorate(block) {
     const renderGrid = (list) => {
       grid.innerHTML = '';
       list.slice(0, maxCards).forEach((item) => {
-        if (item.type === 'Category' && !item.fullCategory.includes('|')) {
+        if (item.type === 'Category') {
           grid.appendChild(createCard(item));
           decorateIcons(grid);
         }
