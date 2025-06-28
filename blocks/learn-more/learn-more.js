d34fc5fe91c68802fc5424d356809f48aed3a31d
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-06-28 20:58:09 +0530

    learn more

--- blocks/learn-more/learn-more.js
+++ blocks/learn-more/learn-more.js
@@ -1,142 +1,154 @@
-import { div, span } from '../../scripts/dom-builder.js';
-import { decorateIcons } from '../../scripts/lib-franklin.js';
+import { div, span } from "../../scripts/dom-builder.js";
+import { decorateIcons } from "../../scripts/lib-franklin.js";
 
 export default function decorate(block) {
-  block?.parentElement?.parentElement?.removeAttribute('class');
-  block?.parentElement?.parentElement?.removeAttribute('style');
-  const [title, brandAddress, callDescription, browseDescription] = block.children;
-
+  block?.parentElement?.parentElement?.removeAttribute("class");
+  block?.parentElement?.parentElement?.removeAttribute("style");
+  const [title, brandAddress, callDescription, browseDescription] =
+    block.children;
+  brandAddress
+    ?.querySelector("div")
+    ?.classList.add(
+      "flex flex-col",
+      "gap-4",
+      "text-black",
+      "text-base",
+      "leading-snug",
+      "items-start"
+    );
   const addressSectionContent = brandAddress;
   addressSectionContent?.classList.add(
-    'flex',
-    'flex-col',
-    'gap-4',
-    'text-black',
-    'text-base',
-    'text-black',
-    'leading-snug',
-    'items-start',
+    "flex",
+    "flex-col",
+    "gap-4",
+    "text-black",
+    "text-base",
+    "text-black",
+    "leading-snug",
+    "items-start"
   );
-  const addressSectionAnchor = addressSectionContent?.querySelectorAll('a') ?? [];
+  const addressSectionAnchor =
+    addressSectionContent?.querySelectorAll("a") ?? [];
   if (addressSectionAnchor.length) {
     addressSectionAnchor?.forEach((anchor) => {
-      const linkHref = anchor?.getAttribute('href');
+      const linkHref = anchor?.getAttribute("href");
       anchor.setAttribute(
-        'target',
-        linkHref?.includes('http') ? '_blank' : '_self',
+        "target",
+        linkHref?.includes("http") ? "_blank" : "_self"
       );
       anchor?.classList.add(
-        'text-danaherpurple-500',
-        'cursor-pointer',
-        'hover:text-danaherpurple-800',
-        '[&_svg>use]:hover:stroke-danaherpurple-800',
-        'text-base',
-        'font-semibold',
-        'flex',
-        'items-center',
-        'leading-snug',
-        'link',
+        "text-danaherpurple-500",
+        "cursor-pointer",
+        "hover:text-danaherpurple-800",
+        "[&_svg>use]:hover:stroke-danaherpurple-800",
+        "text-base",
+        "font-semibold",
+        "flex",
+        "items-center",
+        "leading-snug",
+        "link"
       );
-      anchor?.classList.remove('btn', 'btn-outline-primary');
-      anchor?.parentElement?.classList.remove('btn', 'btn-outline-primary');
-      anchor.textContent = anchor.textContent.replace(/->/g, '');
+      anchor?.classList.remove("btn", "btn-outline-primary");
+      anchor?.parentElement?.classList.remove("btn", "btn-outline-primary");
+      anchor.textContent = anchor.textContent.replace(/->/g, "");
       anchor?.append(
         span({
           class:
-            'icon icon-arrow-right dhls-arrow-right-icon fill-current [&_svg>use]:stroke-danaherpurple-500 [&_svg>use]:hover:stroke-danaherpurple-800',
-        }),
+            "icon icon-arrow-right dhls-arrow-right-icon fill-current [&_svg>use]:stroke-danaherpurple-500 [&_svg>use]:hover:stroke-danaherpurple-800",
+        })
       );
     });
   }
   const callSectionContent = callDescription;
   callSectionContent?.classList.add(
-    'flex',
-    'flex-col',
-    'gap-4',
-    'text-black',
-    'text-base',
-    'text-black',
-    'leading-snug',
-    'items-start',
+    "flex",
+    "flex-col",
+    "gap-4",
+    "text-black",
+    "text-base",
+    "text-black",
+    "leading-snug",
+    "items-start"
   );
-  const callSectionAnchor = callSectionContent?.querySelectorAll('a') ?? [];
+  const callSectionAnchor = callSectionContent?.querySelectorAll("a") ?? [];
   if (callSectionAnchor.length) {
     callSectionAnchor?.forEach((anchor) => {
-      const linkHref = anchor?.getAttribute('href');
+      const linkHref = anchor?.getAttribute("href");
       anchor.setAttribute(
-        'target',
-        linkHref?.includes('http') ? '_blank' : '_self',
+        "target",
+        linkHref?.includes("http") ? "_blank" : "_self"
       );
       anchor?.classList.add(
-        'text-danaherpurple-500',
-        'cursor-pointer',
-        'hover:text-danaherpurple-800',
-        '[&_svg>use]:hover:stroke-danaherpurple-800',
-        'text-base',
-        'font-semibold',
-        'flex',
-        'items-center',
-        'leading-snug',
-        'link',
+        "text-danaherpurple-500",
+        "cursor-pointer",
+        "hover:text-danaherpurple-800",
+        "[&_svg>use]:hover:stroke-danaherpurple-800",
+        "text-base",
+        "font-semibold",
+        "flex",
+        "items-center",
+        "leading-snug",
+        "link"
       );
-      anchor?.classList.remove('btn', 'btn-outline-primary');
-      anchor?.parentElement?.classList.remove('btn', 'btn-outline-primary');
-      anchor.textContent = anchor.textContent.replace(/->/g, '');
+      anchor?.classList.remove("btn", "btn-outline-primary");
+      anchor?.parentElement?.classList.remove("btn", "btn-outline-primary");
+      anchor.textContent = anchor.textContent.replace(/->/g, "");
       anchor?.append(
         span({
           class:
-            'icon icon-arrow-right dhls-arrow-right-icon fill-current [&_svg>use]:stroke-danaherpurple-500 [&_svg>use]:hover:stroke-danaherpurple-800',
-        }),
+            "icon icon-arrow-right dhls-arrow-right-icon fill-current [&_svg>use]:stroke-danaherpurple-500 [&_svg>use]:hover:stroke-danaherpurple-800",
+        })
       );
     });
   }
 
   const browseDescriptionContent = browseDescription;
   browseDescriptionContent?.classList.add(
-    'flex',
-    'flex-col',
-    'gap-4',
-    'text-black',
-    'text-base',
-    'text-black',
-    'leading-snug',
-    'items-start',
+    "flex",
+    "flex-col",
+    "gap-4",
+    "text-black",
+    "text-base",
+    "text-black",
+    "leading-snug",
+    "items-start"
   );
-  const browseDescriptionAnchor = browseDescriptionContent?.querySelectorAll('a') ?? [];
+  const browseDescriptionAnchor =
+    browseDescriptionContent?.querySelectorAll("a") ?? [];
 
   if (browseDescriptionAnchor.length) {
     browseDescriptionAnchor?.forEach((anchor) => {
-      const linkHref = anchor?.getAttribute('href');
+      const linkHref = anchor?.getAttribute("href");
       anchor.setAttribute(
-        'target',
-        linkHref?.includes('http') ? '_blank' : '_self',
+        "target",
+        linkHref?.includes("http") ? "_blank" : "_self"
       );
       anchor?.classList.add(
-        'text-danaherpurple-500',
-        'cursor-pointer',
-        'hover:text-danaherpurple-800',
-        '[&_svg>use]:hover:stroke-danaherpurple-800',
-        'text-base',
-        'font-semibold',
-        'flex',
-        'items-center',
-        'leading-snug',
-        'link',
+        "text-danaherpurple-500",
+        "cursor-pointer",
+        "hover:text-danaherpurple-800",
+        "[&_svg>use]:hover:stroke-danaherpurple-800",
+        "text-base",
+        "font-semibold",
+        "flex",
+        "items-center",
+        "leading-snug",
+        "link"
       );
-      anchor?.classList.remove('btn', 'btn-outline-primary');
-      anchor?.parentElement?.classList.remove('btn-outline-primary');
-      anchor.textContent = anchor.textContent.replace(/->/g, '');
+      anchor?.classList.remove("btn", "btn-outline-primary");
+      anchor?.parentElement?.classList.remove("btn-outline-primary");
+      anchor.textContent = anchor.textContent.replace(/->/g, "");
       anchor?.append(
         span({
           class:
-            'icon icon-arrow-right dhls-arrow-right-icon fill-current [&_svg>use]:stroke-danaherpurple-500 [&_svg>use]:hover:stroke-danaherpurple-800',
-        }),
+            "icon icon-arrow-right dhls-arrow-right-icon fill-current [&_svg>use]:stroke-danaherpurple-500 [&_svg>use]:hover:stroke-danaherpurple-800",
+        })
       );
     });
   }
   // === Main Container
   const learnMoreContainer = div({
-    class: ' dhls-container px-5 lg:px-10 dhlsBp:p-0 mb-12',
+    class: " dhls-container px-5 lg:px-10 dhlsBp:p-0 mb-12",
   });
 
   // === Inner Flex Row
@@ -150,34 +162,34 @@ export default function decorate(block) {
 
   // === Left: Title
   const titleLearnMore = div(
-    { class: 'min-w-[120px] font-medium text-black text-3xl leading-[1.5rem]' },
-    title?.textContent?.trim() || '',
+    { class: "min-w-[120px] font-medium text-black text-3xl leading-[1.5rem]" },
+    title?.textContent?.trim() || ""
   );
 
   // === Middle: SCIEX address
   // const addressNodes = getHTMLNodes("brandaddress");
-  let addressSection = '';
+  let addressSection = "";
 
   // === Right: Call & Browse
-  let callSection = '';
+  let callSection = "";
 
-  let browseSection = '';
+  let browseSection = "";
   if (addressSectionContent) {
-    addressSection = div({ class: 'text-center md:text-left mt-2' });
+    addressSection = div({ class: "text-center md:text-left mt-2" });
     addressSection?.append(addressSectionContent);
   }
   if (callSectionContent) {
-    callSection = div({ class: 'space-y-1' });
+    callSection = div({ class: "space-y-1" });
     callSection?.append(callSectionContent);
   }
   if (browseDescriptionContent) {
-    browseSection = div({ class: 'space-y-1' });
+    browseSection = div({ class: "space-y-1" });
     browseSection?.append(browseDescriptionContent);
   }
   const rightSection = div(
-    { class: 'space-y-6 text-right md:text-left mt-2' },
+    { class: "space-y-6 text-right md:text-left mt-2" },
     callSection,
-    browseSection,
+    browseSection
   );
 
   // === Assemble Columns
@@ -185,6 +197,6 @@ export default function decorate(block) {
   learnMoreContainer.appendChild(innerLearnMore);
 
   decorateIcons(learnMoreContainer);
-  block.textContent = '';
+  block.textContent = "";
   block.appendChild(learnMoreContainer);
 }
