e031c3911a064611020519de494ca61e2f086707
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-07-01 15:23:54 +0530

    added index.js in tools src

--- tools/actions/convert/src/index.js
+++ tools/actions/convert/src/index.js
@@ -194,6 +194,7 @@ function skipConverter(path) {
   if (!path) return false;
   if (path.includes('.json')) return true;
   if( path.includes('us/en/products') && !path.includes('/topics-jck1/') ) {
+    console.log('Inside the emarketplace');
     const pathsToConvert = pathConfig.convertPaths.some(convertPath =>
     path.includes(convertPath)
   );
