ac6e5870cabdf14adc7abe55706dd4eb0e220cc8
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-07-02 12:44:20 +0530

    opco banner

--- blocks/opco-banner/opco-banner.js
+++ blocks/opco-banner/opco-banner.js
@@ -463,6 +463,7 @@ export default async function decorate(block) {
     left,
     right
   );
+  block.textContent = '';
   block.append(container);
   [...block.children].forEach((child) => {
     if (!child.contains(container)) {
