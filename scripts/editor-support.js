4b52e0e731fd076282f15a2d5114341ce4922113
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-07-05 15:46:44 +0530

    opti

--- scripts/editor-support.js
+++ scripts/editor-support.js
@@ -108,6 +108,8 @@ function attachEventListners(main) {
   ].forEach((eventType) =>
     main?.addEventListener(eventType, async (event) => {
       event.stopPropagation();
+      console.log('evenr: ', event);
+
       const applied = await applyChanges(event);
       if (!applied) window.location.reload();
     })
