e57f73e90738591517751e0eb0d213dd74d9ece3
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-07-14 14:54:52 +0530

    payment

--- blocks/checkout/paymentModule.js
+++ blocks/checkout/paymentModule.js
@@ -83,7 +83,7 @@ const paymentModule = async () => {
     cardsWrapper?.querySelector('div > div')?.classList.add('flex','flex-row-reverse','items-center');
     invoiceWrapper?.querySelector('div')?.classList.add('flex','flex-row-reverse','items-center');
     paymentMethodsWrapper?.append(cardsWrapper, invoiceWrapper);
-    paymentMethodsWrapper?.querySelectorAll('input')?.forEach((inp) => {
+    paymentMethodsWrapper?.querySelectorAll('field-wrapper')?.forEach((inp) => {
       const inpu = inp?.querySelector('label');
       if (inpu?.classList.contains('font-normal')) {
         inpu?.classList.remove('font-normal');
