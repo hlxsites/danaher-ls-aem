5cd12a81afe2bf3969f65a5eb899a1f098469bf4
APRO2010 <prasad.esarapu@rankuno.com>
2025-07-17 18:46:47 +0530

    committing fewer lines code

--- blocks/article-info-new/article-info-new.js
+++ blocks/article-info-new/article-info-new.js
@@ -1,23 +1,20 @@
 import {
   div, input, span, img,
 } from '../../scripts/dom-builder.js';
+import { getBlockConfig } from '../../scripts/lib-franklin.js';
 
-export default function decorate(block) {
+export default async function decorate(block) {
   block.innerHTML = '';
 
-  const [
-    authorName,
-    authorJobTitle,
-    publishDate,
-    readingTime,
-    imageAlt,
-    articleOpco,
-    authorImage,
-  ] = block.children;
-  const expectedPublishFormat = new Date();
+  // Load properties from component-models.json
+  const config = await getBlockConfig(block);
 
-  const authorName_1 = authorName?.innerHTML;
-  console.log('authorName_1', authorName_1);
+  const authorName = config.authorName || '';
+  const authorJobTitle = config.authorTitle || '';
+  const publishDate = config.publishDate || '';
+  const readingTime = config.readingTime || '';
+  const authorImage = config.authorImage || '';
+  const expectedPublishFormat = new Date(publishDate);
 
   block.append(
     div(
@@ -28,7 +25,7 @@ export default function decorate(block) {
           { class: 'items-center flex justify-start my-4 w-full col-span-2' },
           div(
             { class: 'space-y-1 text-lg leading-6' },
-            div({ class: 'text-danaherblack-500 font-medium' }, authorName_1),
+            div({ class: 'text-danaherblack-500 font-medium' }, authorName),
             div({ class: 'text-sm text-danaherblack-500 w-full' }, authorJobTitle),
           ),
         ),
@@ -62,21 +59,4 @@ export default function decorate(block) {
       <path d="M9.01172 5.66667V9L11.5117 11.5M16.5117 9C16.5117 13.1421 13.1539 16.5 9.01172 16.5C4.86958 16.5 1.51172 13.1421 1.51172 9C1.51172 4.85786 4.86958 1.5 9.01172 1.5C13.1539 1.5 16.5117 4.85786 16.5117 9Z" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path>
     </svg>
   `;
-
-  const toBeRemoved = ['social-media-wrapper', 'columns-wrapper', 'article-info-wrapper', 'tags-list-wrapper', 'related-articles-wrapper'];
-  const sectionEl = document.querySelector('main > div:nth-child(1)');
-  sectionEl.classList.remove('article-info-container');
-  const leftSideElements = div({ class: 'mt-4' });
-  Array.from(sectionEl.children).forEach((element) => {
-    if (!toBeRemoved.includes(element.classList[0])) {
-      leftSideElements.append(element);
-    }
-  });
-
-  const divEl = div(
-    { class: 'article-info-container' },
-    sectionEl.querySelector('.article-info-wrapper'),
-    leftSideElements,
-  );
-  sectionEl.querySelector('.columns-wrapper')?.after(divEl);
 }
