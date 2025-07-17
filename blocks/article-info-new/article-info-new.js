71bfa06e5a1f11207e0e5c9113152883ab8807a0
APRO2010 <prasad.esarapu@rankuno.com>
2025-07-17 18:57:14 +0530

    committing fewer lines code

--- blocks/article-info-new/article-info-new.js
+++ blocks/article-info-new/article-info-new.js
@@ -8,7 +8,7 @@ export default async function decorate(block) {
   const blockPath = block.closest('.block')?.dataset?.blockName || 'article-info-new';
 
   // Load JSON manually
-  const jsonPath = `${window.location.pathname}${blockPath}/component-models.json`;
+  const jsonPath = `${window.location.pathname}${blockPath}/article-info-new.json`;
   let config = {};
 
 
@@ -51,18 +51,4 @@ export default async function decorate(block) {
       ),
     ),
   );
-
-  if (authorImage) {
-    const items = block.querySelector('.items-center');
-    items.insertBefore(img({ class: 'h-16 w-16 rounded-full lg:h-20 lg:w-20 mr-7', src: authorImage, alt: authorName }), items.firstChild);
-    const imageEl = block.querySelector('.articleinfo')?.querySelector('.items-center')?.querySelector('img');
-    imageEl.remove();
-    block.querySelector('.articleinfo')?.firstChild?.prepend(imageEl);
-  }
-
-  block.querySelector('.reading-icon').innerHTML = `
-    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M9.01172 5.66667V9L11.5117 11.5M16.5117 9C16.5117 13.1421 13.1539 16.5 9.01172 16.5C4.86958 16.5 1.51172 13.1421 1.51172 9C1.51172 4.85786 4.86958 1.5 9.01172 1.5C13.1539 1.5 16.5117 4.85786 16.5117 9Z" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path>
-    </svg>
-  `;
 }
