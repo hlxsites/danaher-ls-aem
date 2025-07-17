85b8320a8f313e628ef090b2548af385e6992b48
APRO2010 <prasad.esarapu@rankuno.com>
2025-07-17 18:36:33 +0530

    committing fewer lines code

--- blocks/article-info-new/article-info-new.js
+++ blocks/article-info-new/article-info-new.js
@@ -1,53 +1,81 @@
+import {
+  div, input, span, img,
+} from '../../scripts/dom-builder.js';
+
 export default function decorate(block) {
-  const main = document.querySelector('main');
-  const content = block.querySelector('div');
-  const innerContent = content?.querySelector('div');
+  block.innerHTML = '';
 
-  if (!innerContent) {
-    return;
-  }
-
-  // Add utility classes to the inner wrapper
-  const classes = ['items-center', 'flex', 'justify-start', 'my-4', 'w-full', 'col-span-2'];
-  innerContent.classList.add(...classes);
-
-  const authorName = innerContent.querySelector('.authorName');
-  const authorTitle = innerContent.querySelector('.authorTitle');
-  const image = innerContent.querySelector('.image');
-  const imageAlt = innerContent.querySelector('.imageAlt');
-  const articleOpco = innerContent.querySelector('.articleOpco');
-  const publishDateEl = innerContent.querySelector('.publishDate');
-  const readingTime = innerContent.querySelector('.readingTime');
-  const textClasses = ['text-danaherblack-500', 'font-medium'];
-
-  const elements = [
+  const [
     authorName,
-    authorTitle,
-    image,
+    authorJobTitle,
+    publishDate,
+    readingTime,
     imageAlt,
     articleOpco,
-    readingTime,
-    publishDateEl,
-  ];
+    authorImage,
+  ] = block.children;
+  const expectedPublishFormat = new Date();
 
-  elements.forEach((el) => {
-    if (el) {
-      el.classList.add(...textClasses);
+  const authorName_1 = authorName?.innerHTML;
+
+  block.append(
+    div(
+      { class: 'articleinfo' },
+      div(
+        { class: 'max-w-4xl mx-auto' },
+        div(
+          { class: 'items-center flex justify-start my-4 w-full col-span-2' },
+          div(
+            { class: 'space-y-1 text-lg leading-6' },
+            div({ class: 'text-danaherblack-500 font-medium' }, authorName_1),
+            div({ class: 'text-sm text-danaherblack-500 w-full' }, authorJobTitle),
+          ),
+        ),
+        div(
+          { class: 'w-max items-center flex justify-end col-span-1 text-sm mr-4 my-4 text-danaherblack-500' },
+          `${expectedPublishFormat.getDate()} ${expectedPublishFormat.toLocaleString('default', { month: 'long' })}, ${expectedPublishFormat.getFullYear()}`,
+          input({ id: 'publishdate', class: 'hidden', value: publishDate }),
+        ),
+        div(
+          { class: 'items-center flex justify-start col-span-1 my-4' },
+          div({ class: 'reading-icon' }),
+          div(
+            { class: 'text-sm text-danaherblack-500 pl-1' },
+            span({ id: 'timetoread' }, `${readingTime} Mins`),
+          ),
+        ),
+      ),
+    ),
+  );
+
+  if (authorImage) {
+    const items = block.querySelector('.items-center');
+    items.insertBefore(img({ class: 'h-16 w-16 rounded-full lg:h-20 lg:w-20 mr-7', src: authorImage, alt: authorName }), items.firstChild);
+    const imageEl = block.querySelector('.articleinfo')?.querySelector('.items-center')?.querySelector('img');
+    imageEl.remove();
+    block.querySelector('.articleinfo')?.firstChild?.prepend(imageEl);
+  }
+
+  block.querySelector('.reading-icon').innerHTML = `
+    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M9.01172 5.66667V9L11.5117 11.5M16.5117 9C16.5117 13.1421 13.1539 16.5 9.01172 16.5C4.86958 16.5 1.51172 13.1421 1.51172 9C1.51172 4.85786 4.86958 1.5 9.01172 1.5C13.1539 1.5 16.5117 4.85786 16.5117 9Z" stroke="#000000" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"></path>
+    </svg>
+  `;
+
+  const toBeRemoved = ['social-media-wrapper', 'columns-wrapper', 'article-info-wrapper', 'tags-list-wrapper', 'related-articles-wrapper'];
+  const sectionEl = document.querySelector('main > div:nth-child(1)');
+  sectionEl.classList.remove('article-info-container');
+  const leftSideElements = div({ class: 'mt-4' });
+  Array.from(sectionEl.children).forEach((element) => {
+    if (!toBeRemoved.includes(element.classList[0])) {
+      leftSideElements.append(element);
     }
   });
 
-  // Format publish date if present
-  if (publishDateEl?.textContent) {
-    const rawDate = new Date(publishDateEl.textContent.trim());
-    const formattedDate = rawDate.toLocaleDateString('en-US', {
-      year: 'numeric',
-      month: 'short',
-      day: '2-digit',
-    });
-    publishDateEl.textContent = formattedDate;
-  }
-  const section = main.querySelector('section');
-  if (section) {
-    section.appendChild(block);
-  }
+  const divEl = div(
+    { class: 'article-info-container' },
+    sectionEl.querySelector('.article-info-wrapper'),
+    leftSideElements,
+  );
+  sectionEl.querySelector('.columns-wrapper')?.after(divEl);
 }
