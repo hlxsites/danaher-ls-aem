9c8e1fd71a56343fb3fd26f5e414f3ca210007d8
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-07-18 16:54:45 +0530

    Update opco-banner.js

--- blocks/opco-banner/opco-banner.js
+++ blocks/opco-banner/opco-banner.js
@@ -415,7 +415,7 @@ export default async function decorate(block) {
       {
         id: `opcoBannerSlide${index}`,
         'data-index': index,
-        class: ` ${opcoBannerItemBgImage ? 'hasBg justify-center' : ' '} ${
+        class: ` ${opcoBannerItemBgImage ? 'hasBg ' : ' '} ${
           opcoBannerItems.length > 1 ? '' : 'justify-center'
         } carousel-slide p-10 flex  min-h-[650px] md:min-h-[600px] flex-col items-center w-full relative`,
         style: index === 0 ? '' : 'display: none;',
