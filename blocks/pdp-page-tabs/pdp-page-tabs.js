6589da6eef918b5a1809193c5c144e1487428de4
SanyaKasaudhan1 <sanya.kasaudhan@dhlscontractors.com>
2025-07-22 16:18:26 +0530

    PAGE TAB

--- blocks/pdp-page-tabs/pdp-page-tabs.js
+++ blocks/pdp-page-tabs/pdp-page-tabs.js
@@ -3,4 +3,14 @@
 
 export default async function decorate(block) {
     console.log("block", block);
+    block?.parentElement?.parentElement?.removeAttribute('class');
+     block?.parentElement?.parentElement?.removeAttribute('style');
+  const pdp_tab_wrapper = div({
+    class:
+      'dhls-container mx-auto flex flex-col md:flex-row gap-6 px-5 lg:px-0',
+    'PAGE TABS'
+  });
+    
+  block.innerHTML = '';
+  block.appendChild(pdp_tab_wrapper);
 }
