d98a85082d9b8af9e08948ab1552776a543b403a
SanyaKasaudhan1 <sanya.kasaudhan@dhlscontractors.com>
2025-07-22 16:19:31 +0530

    comm

--- blocks/pdp-page-tabs/pdp-page-tabs.js
+++ blocks/pdp-page-tabs/pdp-page-tabs.js
@@ -7,10 +7,9 @@ export default async function decorate(block) {
      block?.parentElement?.parentElement?.removeAttribute('style');
   const pdp_tab_wrapper = div({
     class:
-      'dhls-container mx-auto flex flex-col md:flex-row gap-6 px-5 lg:px-0',
-    'PAGE TABS'
+      'dhls-container mx-auto flex flex-col md:flex-row gap-6 px-5 lg:px-0'
   });
-    
+    pdp_tab_wrapper.classList.add('pdppp');
   block.innerHTML = '';
   block.appendChild(pdp_tab_wrapper);
 }
