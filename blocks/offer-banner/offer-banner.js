3f2a23297b54e646b6003155244350b509a64ab8
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-07-24 15:36:00 +0530

    fixing offer banner

--- blocks/offer-banner/offer-banner.js
+++ blocks/offer-banner/offer-banner.js
@@ -13,18 +13,27 @@ export default function decorate(block) {
     offerBannerLink,
     offerBannerLinkTarget,
     offerBannerLinkLabel,
+    offerBannerBgColor,
   ] = block.children;
+console.log('offerBannerTitle:  ',offerBannerTitle);
+console.log('offerBannerImage:  ',offerBannerImage);
+console.log('offerBannerLink:  ',offerBannerLink);
+console.log('offerBannerLinkTarget:  ',offerBannerLinkTarget);
+console.log('offerBannerLinkLabel:  ',offerBannerLinkLabel);
+console.log('offerBannerBgColor:  ',offerBannerBgColor);
 
-  const titleEl = offerBannerTitle.textContent.trim() || '';
-  const imgEl = offerBannerImage.querySelector('img');
-  const linkTextEl = offerBannerLink.textContent.trim() || '';
-  const linkLabelEl = offerBannerLinkTarget.textContent.trim() || '';
-  const bgColorEl = offerBannerLinkLabel.textContent.trim() || '#E5E7EB';
+  const titleEl = offerBannerTitle?.textContent.trim() || '';
+  const imgEl = offerBannerImage?.querySelector('img');
+  const linkTextEl = offerBannerLink?.textContent.trim() || '';
+  const linkTarget = offerBannerLinkTarget?.textContent?.trim() || '';
+  const linkLabelEl = offerBannerLinkLabel?.textContent.trim() || '';
+  const bgColorEl = offerBannerBgColor.textContent.trim() || '#E5E7EB';
 
   const title = titleEl;
   const imgSrc = imgEl?.getAttribute('src').replace(/<[^>]*>/g, '') || '';
   const imgAlt = imgEl?.getAttribute('alt') || 'Banner image';
   const linkText = linkTextEl;
+  const linkTargetEl = linkTarget ? '_blank' : '_self';
   const linkLabel = linkLabelEl;
   const bgColor = bgColorEl;
 
@@ -56,13 +65,14 @@ export default function decorate(block) {
         ? a(
           {
             href: linkTextEl || '#',
+            target: linkTargetEl,
             class:
                 'text-base text-danaherpurple-500 hover:text-danaherpurple-800 [&_svg>use]:hover:stroke-danaherpurple-800 font-semibold mt-4 flex items-center  md:pl-8',
           },
           linkLabel || '',
           span({
             class:
-                'icon icon-arrow-right  dhls-arrow-right-icon fill-current [&_svg>use]:stroke-danaherpurple-500 [&_svg>use]:hover:stroke-danaherpurple-800',
+                'icon icon-arrow-right dhls-arrow-right-icon fill-current [&_svg>use]:stroke-danaherpurple-500 [&_svg>use]:hover:stroke-danaherpurple-800',
           }),
         )
         : '',
