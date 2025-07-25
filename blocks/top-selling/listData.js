3ea2462378ec6de2af9f3994b082b66c9b17df2a
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-07-25 11:38:17 +0530

    added hover transition

--- blocks/top-selling/listData.js
+++ blocks/top-selling/listData.js
@@ -49,7 +49,7 @@ export default function renderListCard(item) {
   const imageUrl = item?.images?.[0] || '';
   const card = div({
     class:
-      'hover:shadow-md !duration-1000 !ease-in-out !transition-transform !transform self-stretch w-full outline outline-1 outline-gray-300 inline-flex flex-col md:flex-row justify-start items-center',
+      'hover:shadow-md transform transition duration-500 self-stretch w-full outline outline-1 outline-gray-300 inline-flex flex-col md:flex-row justify-start items-center',
   });
 
   const leftSection = div({
