ac86af0486e730500e9641c8f3d25eef86f5abca
SanyaKasaudhan1 <sanya.kasaudhan@dhlscontractors.com>
2025-07-29 15:33:39 +0530

    camel case lint

--- blocks/pdp-hero/pdp-hero.js
+++ blocks/pdp-hero/pdp-hero.js
@@ -446,9 +446,9 @@ export default async function decorate(block) {
 
   categoryLink.addEventListener('click', () => {
     const main = block.closest('main');
-    const product_Tab = main.querySelector('#products-tab');
-    if (product_Tab) {
-      product_Tab.scrollIntoView({ behavior: 'smooth' });
+    const productTab = main.querySelector('#products-tab');
+    if (productTab) {
+      productTab.scrollIntoView({ behavior: 'smooth' });
     }
   });
 
