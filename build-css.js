94b7a7ac9315cb80b1a667d0b6b140845f3cc6b7
SanyaKasaudhan1 <sanya.kasaudhan@dhlscontractors.com>
2025-07-31 12:30:50 +0530

    pdp-carousel

--- build-css.js
+++ build-css.js
@@ -337,6 +337,14 @@ const fileMappings = [
   },
   {
     content: [
+      './blocks/pdp-carousel/pdp-carousel.js',
+      './blocks/pdp-carousel/gridData.js',
+    ],
+    output: './blocks/pdp-carousel/pdp-carousel.css',
+    wrapper: 'pdp-carousel-wrapper',
+  },
+  {
+    content: [
       './blocks/top-selling-products/top-selling-products.js',
       './blocks/top-selling-products/gridData.js',
     ],
