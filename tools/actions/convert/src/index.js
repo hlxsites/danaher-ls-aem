6582a65fa6971952fc01c6cd477f12f454b09a55
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-08-01 13:44:36 +0530

    Em1 checkout payment module (#1696)
    * testing cvonverter
    
    * testing converter
    
    * testing converter
    
    * testing converter
    
    * testing converter
    
    * testing converter on ebuy
    
    * testing converter
    
    * testing converter
    
    * testing converter
    
    * testing converter
    
    * testing converter
    
    * linting
    
    * linintg
    
    * adding cl
    
    * adding cl
    
    * testing converter
    
    * testing converter
    
    * testing converter
    
    * testing converter
    
    * testing converter

--- tools/actions/convert/src/index.js
+++ tools/actions/convert/src/index.js
@@ -214,9 +214,11 @@ function skipConverter(path) {
       return false;
     }
 
-    if ((path.includes('us/en/products.html') || path.includes('us/en/products/brands') || path.includes('us/en/products/antibodies') || path.includes('us/en/products/assay-kits') || path.includes('/us/en/products-eds') || path.includes('/us/en/e-buy')) && !path.includes('/topics-jck1/')) {
+    if ((path.includes('us/en/products.html') || path.includes('us/en/products/brands') || path.includes('us/en/products/antibodies') || path.includes('us/en/products/assay-kits')) && !path.includes('/topics-jck1/')) {
       return true;
     }
+
+    if (path.includes('/us/en/products-eds') || path.includes('/us/en/e-buy')) return true;
   }
 
   if (path.includes('/us/en/blog-eds/')) return true;
