bb22086ce67619cd9cee4d845181954bf7d6453b
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-08-01 12:36:01 +0530

    testing converter

--- tools/actions/convert/src/index.js
+++ tools/actions/convert/src/index.js
@@ -209,14 +209,16 @@ function skipConverter(path) {
   //   }
   // }
 
+    if (path.includes('/us/en/products-eds') || path.includes('/us/en/e-buy')) return true;
   if (!converterCfg?.internalHost?.includes('danaher-ls-aem-prod')) {
     if ((path.includes('us/en/products/family') || path.includes('us/en/products/sku') || path.includes('us/en/products/bundle')) && !path.includes('/topics-jck1/')) {
       return false;
     }
 
-    if ((path.includes('us/en/products.html') || path.includes('us/en/products/brands') || path.includes('us/en/products/antibodies') || path.includes('us/en/products/assay-kits') || path.includes('/us/en/products-eds') || path.includes('/us/en/e-buy')) && !path.includes('/topics-jck1/')) {
+    if ((path.includes('us/en/products.html') || path.includes('us/en/products/brands') || path.includes('us/en/products/antibodies') || path.includes('us/en/products/assay-kits')) && !path.includes('/topics-jck1/')) {
       return true;
     }
+
   }
 
   if (path.includes('/us/en/blog-eds/')) return true;
