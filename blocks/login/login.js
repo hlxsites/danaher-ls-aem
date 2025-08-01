fb0a87ce519a98824a6ac11967f209cfb7bad89d
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-08-01 14:16:07 +0530

    Em1 checkout payment module (#1698)
    * testing cvonverter
    
    * testing converter
    
    * testing converter
    
    * testing converter
    
    * testing converter
    
    * testing converter on ebuy
    
    * testing converter
    
    * testing converter
    
    * testing converter
    
    * testing converter
    
    * testing converter
    
    * linting
    
    * linintg
    
    * adding cl
    
    * adding cl
    
    * testing converter
    
    * testing converter
    
    * testing converter
    
    * testing converter
    
    * testing converter
    
    * testing converter
    
    * testing converter
    
    * converter testing

--- blocks/login/login.js
+++ blocks/login/login.js
@@ -17,8 +17,8 @@ import {
 } from '../../scripts/common-utils.js';
 
 export default async function decorate(block) {
-  block?.parentElement?.parentElement?.removeAttribute('class');
-  block?.parentElement?.parentElement?.removeAttribute('style');
+  block.parentElement.parentElement.style.padding = '0';
+  block.parentElement.parentElement.style.margin = '0';
   const [
     heading,
     subHeading,
