de3a0298c86f474af4e47fe53af364b98588bd2d
SanyaKasaudhan1 <sanya.kasaudhan@dhlscontractors.com>
2025-08-06 10:28:40 +0530

    adding bold  content

--- blocks/accordion-container/accordion-container.js
+++ blocks/accordion-container/accordion-container.js
@@ -99,6 +99,17 @@ function createAccordionBlock(
     panel.querySelector('.accordion-answer').innerHTML += element;
   });
 
+  const liElements = panel.querySelectorAll('p,ul,li,a');
+    console.log(liElements);
+      liElements.forEach((liEle) => {
+        const strongElements = liEle.querySelectorAll('strong');
+        if (strongElements?.length) {
+          strongElements.forEach((strong) => {
+            strong.classList.add('font-bold');
+          });
+        }
+      });
+
   panel.querySelectorAll('a').forEach((link) => {
     link.classList.remove('btn', 'btn-outline-primary');
     link.classList.add(
