b1a0e6676b0818f9a4bf5ea1be01815fda2a0770
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-08-21 11:50:29 +0530

    fixing product categories removing blank title condition

--- blocks/product-categories/product-categories.js
+++ blocks/product-categories/product-categories.js
@@ -105,7 +105,7 @@ export default async function decorate(block) {
     const renderGrid = (list) => {
       grid.innerHTML = '';
       list.slice(0, maxCards).forEach((item) => {
-        if (item.type === 'Category' && item.title !== '' && item.brand !== '') {
+        if (item.type === 'Category' && item.brand !== '') {
           grid.appendChild(createCard(item));
           decorateIcons(grid);
         }
