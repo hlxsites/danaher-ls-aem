4b8254df2d2493967f0cee15535345c948a024ce
APRO2010 <prasad.esarapu@rankuno.com>
2025-08-28 10:17:52 +0530

    added the form logic

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -634,6 +634,7 @@ export default function decorate(block) {
 
       // Replace <p> with form block
       //pTag.replaceWith(formDiv);
+      pTag.parentNode.replaceChild(formDiv, pTag);
 
       // Decorate the form block ONLY (do NOT clear column!)
       decorateFormBlock(formDiv);
