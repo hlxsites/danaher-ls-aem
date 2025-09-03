affce2af2626a714ba2a9302de4d96d097b524c5
DeepikaYadav02 <deepika.yadav@dhlscontractors.com>
2025-09-03 14:44:31 +0530

    block fix

--- blocks/approved-quotes/approved-quotes.js
+++ blocks/approved-quotes/approved-quotes.js
@@ -4,7 +4,6 @@ import { showPreLoader, removePreLoader } from "../../scripts/common-utils.js";
 import { div } from "../../scripts/dom-builder.js";
 
 export default async function decorate(block) {
-  showPreLoader();
   block?.parentElement?.parentElement?.removeAttribute("class");
   block?.parentElement?.parentElement?.removeAttribute("style");
   document.querySelector("main").style = "background: #f4f4f4";
@@ -13,11 +12,7 @@ export default async function decorate(block) {
       class:
             'flex flex-col gap-5 md:flex-row w-full dhls-container lg:px-10 dhlsBp:py-12',
     });
-      const dashboardSideBarContent = await dashboardSidebar();
-      wrapper.append(dashboardSideBarContent);
       block.innerHTML = "";
       block.textContent = "";
       block.append(wrapper);
-      decorateIcons(wrapper);
-      removePreLoader();
 }
