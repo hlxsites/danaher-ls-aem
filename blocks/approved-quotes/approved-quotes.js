e9044f6f0507804fcf56dede104ac65a5e20aa5b
DeepikaYadav02 <deepika.yadav@dhlscontractors.com>
2025-09-03 14:39:24 +0530

    bug fix

--- blocks/approved-quotes/approved-quotes.js
+++ blocks/approved-quotes/approved-quotes.js
@@ -1,4 +1,23 @@
+import dashboardSidebar from "../dashboardSideBar/dashboardSideBar.js";
+import { decorateIcons } from "../../scripts/lib-franklin.js";
+import { showPreLoader, removePreLoader } from "../../scripts/common-utils.js";
+import { div } from "../../scripts/dom-builder.js";
 
 export default async function decorate(block) {
-  console.log("sdfbjkdgbdfhjkbv",block);
+  showPreLoader();
+  block?.parentElement?.parentElement?.removeAttribute("class");
+  block?.parentElement?.parentElement?.removeAttribute("style");
+  document.querySelector("main").style = "background: #f4f4f4";
+   const wrapper = div({
+      id: 'dashboardWrapper',
+      class:
+            'flex flex-col gap-5 md:flex-row w-full dhls-container lg:px-10 dhlsBp:py-12',
+    });
+      const dashboardSideBarContent = await dashboardSidebar();
+      wrapper.append(dashboardSideBarContent);
+      block.innerHTML = "";
+      block.textContent = "";
+      block.append(wrapper);
+      decorateIcons(wrapper);
+      removePreLoader();
 }
