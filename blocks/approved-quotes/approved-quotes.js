c87f6d136e9506ee530d0f3c64c151d8f2ee143e
DeepikaYadav02 <deepika.yadav@dhlscontractors.com>
2025-09-03 14:34:54 +0530

    bug fix

--- blocks/approved-quotes/approved-quotes.js
+++ blocks/approved-quotes/approved-quotes.js
@@ -1,9 +1,4 @@
 
 export default async function decorate(block) {
-  block?.parentElement?.parentElement?.removeAttribute("class");
-  block?.parentElement?.parentElement?.removeAttribute("style");
-  document.querySelector("main").style = "background: #f4f4f4";
-   
-      block.innerHTML = "";
-      block.textContent = "";
+  console.log("sdfbjkdgbdfhjkbv",block);
 }
