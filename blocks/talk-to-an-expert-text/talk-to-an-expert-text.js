13a39b2df7b1546cd6fbca3c8eb4de2e7e1dc827
APRO2010 <prasad.esarapu@rankuno.com>
2025-09-03 20:33:43 +0530

    added the talk to an expert block

--- blocks/talk-to-an-expert-text/talk-to-an-expert-text.js
+++ blocks/talk-to-an-expert-text/talk-to-an-expert-text.js
@@ -4,7 +4,7 @@ import { decorateIcons } from '../../scripts/lib-franklin.js';
 export default function decorate(block) {
   const talkToAnExpertText = block?.firstElementChild?.textContent;
   block.innerHTML = '';
-  block.classList.add(...'relative z-10 flex items-center justify-between mb-8 text-gray-600 pt-4 pb-0 md:pb-10'.split(' '));
+  block.classList.add(...'align-text-center w-full h-full'.split(' '));
   // eslint-disable-next-line no-script-url
   const goParentBack = a({ class: 'my-auto text-base text-danaherpurple-500 font-semibold' }, `${talkToAnExpertText}`);
   block.prepend(goParentBack);
