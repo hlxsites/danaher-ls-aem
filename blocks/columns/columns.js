cd1e481f1ac8a5d4bd0b32c7950a6ff07f193fd2
APRO2010 <prasad.esarapu@rankuno.com>
2025-09-03 15:52:17 +0530

    updated the logic for article info

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -646,7 +646,7 @@ export default function decorate(block) {
   const imageAspectRatio = 1.7778;
   block.querySelectorAll('div').forEach((ele, index) => {
     if (index === 0) {
-      if (window.location.pathname.includes('/us/en/blog/') || window.location.pathname.includes('/us/en/news/')) {
+      if (window.location.pathname.includes('/us/en/blog/') || window.location.pathname.includes('/us/en/news/') || window.location.pathname.includes('/us/en/we-see-a-way/')) {
         ele.classList.add(...'align-text-center w-full h-full'.split(' '));
       } else {
         ele.classList.add(...'align-text-top pb-7 py-0 my-0'.split(' '));
@@ -708,7 +708,7 @@ export default function decorate(block) {
           img.height = Math.floor(this.width / imageAspectRatio);
         };
       } else if (!block.className.includes('itemscenter')) {
-        if (window.location.pathname.includes('/us/en/blog/') || window.location.pathname.includes('/us/en/news/')) {
+        if (window.location.pathname.includes('/us/en/blog/') || window.location.pathname.includes('/us/en/news/') || window.location.pathname.includes('/us/en/we-see-a-way/')) {
           row.classList.add('h-full', 'lg:w-1/2', 'md:pr-16');
           row.querySelectorAll('h1').forEach((ele) => {
             ele.classList.add('pb-4');
@@ -753,7 +753,7 @@ export default function decorate(block) {
         });
       }
       if (block.className.includes('columns-2-cols')) {
-        if (window.location.pathname.includes('/us/en/blog/') || window.location.pathname.includes('/us/en/news/')) {
+        if (window.location.pathname.includes('/us/en/blog/') || window.location.pathname.includes('/us/en/news/') || window.location.pathname.includes('/us/en/we-see-a-way/')) {
           block.firstElementChild?.classList.add(...'container max-w-7xl mx-auto flex flex-col-reverse gap-x-12 lg:flex-col-reverse justify-items-center'.split(' '));
         } else {
           block.firstElementChild?.classList.add(...'container max-w-7xl mx-auto flex flex-col gap-x-12 gap-y-4 lg:flex-row justify-items-center'.split(' '));
@@ -788,7 +788,7 @@ export default function decorate(block) {
         const picWrapper = pic.closest('div');
         if (picWrapper && picWrapper.children.length === 1) {
           // picture is only content in column
-          if (window.location.pathname.includes('/us/en/blog/') || window.location.pathname.includes('/us/en/news/')) {
+          if (window.location.pathname.includes('/us/en/blog/') || window.location.pathname.includes('/us/en/news/') || window.location.pathname.includes('/us/en/we-see-a-way/')) {
             picWrapper.classList.add(...'columns-img-col order-none relative h-48 md:h-[27rem] block lg:absolute md:inset-y-0 lg:inset-y-0 lg:right-2 lg:w-1/2 lg:mt-56'.split(' '));
             pic.querySelector('img').classList.add(...'absolute bottom-0 h-full w-full object-cover'.split(' '));
           } else {
