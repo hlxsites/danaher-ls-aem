27ed8c3a2c71bf566e30c6f5d3f6cbea01088377
APRO2010 <prasad.esarapu@rankuno.com>
2025-09-08 11:04:00 +0530

    reverted the main branch files

---
+++ scripts/tabs-order.js
@@ -0,0 +1,264 @@
+export default function tabsOrder() {
+  return {
+    sciex: [
+      {
+        tabName: 'products',
+        order: 1,
+      },
+      {
+        tabName: 'overview',
+        order: 2,
+      },
+      {
+        tabName: 'specifications',
+        order: 3,
+      },
+      {
+        tabName: 'parts',
+        order: 4,
+      },
+      {
+        tabName: 'citations',
+        order: 5,
+      },
+      {
+        tabName: 'resources',
+        order: 6,
+      },
+      {
+        tabName: 'faqs',
+        order: 7,
+      },
+      {
+        tabName: 'relatedproducts',
+        order: 8,
+      },
+    ],
+    abcam: [
+      {
+        tabName: 'overview',
+        order: 1,
+      },
+      {
+        tabName: 'keyfacts',
+        order: 2,
+      },
+      {
+        tabName: 'targetdata',
+        order: 3,
+      },
+      {
+        tabName: 'reactivitydata',
+        order: 4,
+      },
+      {
+        tabName: 'storageinformation',
+        order: 5,
+      },
+      {
+        tabName: 'notes',
+        order: 6,
+      },
+      {
+        tabName: 'productprotocols',
+        order: 7,
+      },
+      {
+        tabName: 'images',
+        order: 8,
+      },
+      {
+        tabName: 'publications',
+        order: 9,
+      },
+      {
+        tabName: 'faqs',
+        order: 10,
+      },
+      {
+        tabName: 'relatedproducts',
+        order: 11,
+      },
+    ],
+    'molecular devices': [
+      {
+        tabName: 'products',
+        order: 1,
+      },
+      {
+        tabName: 'overview',
+        order: 2,
+      },
+      {
+        tabName: 'specifications',
+        order: 3,
+      },
+      {
+        tabName: 'parts',
+        order: 4,
+      },
+      {
+        tabName: 'citations',
+        order: 5,
+      },
+      {
+        tabName: 'resources',
+        order: 6,
+      },
+      {
+        tabName: 'faqs',
+        order: 7,
+      },
+      {
+        tabName: 'relatedproducts',
+        order: 8,
+      },
+    ],
+    phenomenex: [
+      {
+        tabName: 'products',
+        order: 1,
+      },
+      {
+        tabName: 'overview',
+        order: 2,
+      },
+      {
+        tabName: 'specifications',
+        order: 3,
+      },
+      {
+        tabName: 'parts',
+        order: 4,
+      },
+      {
+        tabName: 'conditions',
+        order: 5,
+      },
+      {
+        tabName: 'analysis',
+        order: 6,
+      },
+      {
+        tabName: 'citations',
+        order: 7,
+      },
+      {
+        tabName: 'resources',
+        order: 8,
+      },
+      {
+        tabName: 'faqs',
+        order: 9,
+      },
+      {
+        tabName: 'relatedproducts',
+        order: 10,
+      },
+    ],
+    'beckman coulter life sciences': [
+      {
+        tabName: 'products',
+        order: 1,
+      },
+      {
+        tabName: 'overview',
+        order: 2,
+      },
+      {
+        tabName: 'specifications',
+        order: 3,
+      },
+      {
+        tabName: 'parts',
+        order: 4,
+      },
+      {
+        tabName: 'citations',
+        order: 5,
+      },
+      {
+        tabName: 'accessories',
+        order: 6,
+      },
+      {
+        tabName: 'resources',
+        order: 7,
+      },
+      {
+        tabName: 'faqs',
+        order: 8,
+      },
+      {
+        tabName: 'relatedproducts',
+        order: 9,
+      },
+    ],
+    'leica microsystems': [
+      {
+        tabName: 'products',
+        order: 1,
+      },
+      {
+        tabName: 'overview',
+        order: 2,
+      },
+      {
+        tabName: 'specifications',
+        order: 3,
+      },
+      {
+        tabName: 'parts',
+        order: 4,
+      },
+      {
+        tabName: 'citations',
+        order: 5,
+      },
+      {
+        tabName: 'resources',
+        order: 6,
+      },
+      {
+        tabName: 'faqs',
+        order: 7,
+      },
+      {
+        tabName: 'relatedproducts',
+        order: 8,
+      },
+    ],
+    idbs: [
+      {
+        tabName: 'overview',
+        order: 1,
+      },
+      {
+        tabName: 'resources',
+        order: 2,
+      },
+      {
+        tabName: 'faqs',
+        order: 3,
+      },
+    ],
+    genedata: [
+      {
+        tabName: 'overview',
+        order: 1,
+      },
+      {
+        tabName: 'applications',
+        order: 2,
+      },
+      {
+        tabName: 'softwares',
+        order: 3,
+      },
+      {
+        tabName: 'faqs',
+        order: 4,
+      },
+    ],
+  };
+}
