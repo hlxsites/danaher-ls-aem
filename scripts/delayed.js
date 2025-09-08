e579fa1b1a6d3aeff30666ef51ca696e7ed0c313
aadidhls <aadi.ahlawat@dhlscontractors.com>
2025-09-08 15:08:25 +0530

    updating order-status endpoint

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -8,7 +8,7 @@ import { removePreLoader, showNotification } from './common-utils.js';
 import { getAuthenticationToken } from './token-utils.js';
 
 /**
- * Sets up event delegation for "Add to Cart" button clicks within the container.
+ma * Sets up event delegation for "Add to Cart" button clicks within the container.
  * Efficiently handles clicks even for dynamically added buttons.
  *
  * @param {HTMLElement} wrapper - The container element that may contain one or more buy buttons.
