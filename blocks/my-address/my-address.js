9bc1278c5abb186751ca310a10bd7851a338cc41
DeepikaYadav02 <deepika.yadav@dhlscontractors.com>
2025-09-10 13:13:23 +0530

    address page fix

--- blocks/my-address/my-address.js
+++ blocks/my-address/my-address.js
@@ -1,10 +1,10 @@
 import { div } from "../../scripts/dom-builder.js";
-import dashboardSidebar from "../dashboardSideBar/dashboardSideBar.js";
-import { removePreLoader, showPreLoader } from "../../scripts/common-utils.js";
-import { decorateIcons } from "../../scripts/lib-franklin.js";
+// import dashboardSidebar from "../dashboardSideBar/dashboardSideBar.js";
+// import { removePreLoader, showPreLoader } from "../../scripts/common-utils.js";
+// import { decorateIcons } from "../../scripts/lib-franklin.js";
 
 export default async function decorate(block) {
-  showPreLoader();
+//   showPreLoader();
   block?.parentElement?.parentElement?.removeAttribute("class");
   block?.parentElement?.parentElement?.removeAttribute("style");
   const wrapper = div({
@@ -12,14 +12,14 @@ export default async function decorate(block) {
     class:
       "flex flex-col gap-5 md:flex-row w-full dhls-container lg:px-10 dhlsBp:py-12",
   });
-  const dashboardSideBarContent = await dashboardSidebar();
-  const quoteDetailsWrapper = div({
-    class: "w-[70%] inline-flex flex-col justify-start items-start gap-5 ",
-  });
-  wrapper.append(dashboardSideBarContent, quoteDetailsWrapper);
+//   const dashboardSideBarContent = await dashboardSidebar();
+//   const quoteDetailsWrapper = div({
+//     class: "w-[70%] inline-flex flex-col justify-start items-start gap-5 ",
+//   });
+//   wrapper.append(dashboardSideBarContent, quoteDetailsWrapper);
   block.innerHTML = "";
   block.textContent = "";
   block.append(wrapper);
-  decorateIcons(wrapper);
-  removePreLoader();
+//   decorateIcons(wrapper);
+//   removePreLoader();
 }
